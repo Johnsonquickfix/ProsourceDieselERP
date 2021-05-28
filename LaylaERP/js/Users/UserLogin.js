6d187ce28d80a4e5cdc10dac608ef7c461af1278
david.quickfix1 <david.quickfix1@gmail.com>
2021-05-28 12:00:15 +0530

    changes on users 27052021

--- LaylaERP/js/Users/UserLogin.js
+++ LaylaERP/js/Users/UserLogin.js
@@ -15,6 +15,7 @@
             success: function (data) {
                 //$("#loading-div-background").hide();
                 if (data.status == true) {
+                   // alert(data.url);
                     window.open(top.window.location.origin + '/' + data.url, "_self");
                 }
                 else {

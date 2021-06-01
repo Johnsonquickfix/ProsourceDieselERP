d46a5c5e06c4368a7546e5905cf6b537597a17f8
Tom Bell <tombell.quickfix@gmail.com>
2021-06-01 20:24:55 +0530

    Add serialization class for users role

--- LaylaERP/Models/UsersRepositry.cs
+++ LaylaERP/Models/UsersRepositry.cs
@@ -8,7 +8,7 @@ using MySql.Data.MySqlClient;
 using System.Data;
 using System.Configuration;
 using Newtonsoft.Json;
-
+using System.Collections;
 
 namespace LaylaERP.Models
 {
@@ -20,21 +20,39 @@ namespace LaylaERP.Models
         {
             try
             {
+               
                 DataSet ds1 = new DataSet();
-                ds1.Clear();
                 string sqlquery = "select ID, User_Image, user_login, user_status, if(user_status=0,'Active','InActive') as status,user_email,user_pass,meta_value from wp_users, wp_usermeta WHERE  wp_usermeta.meta_key='wp_capabilities' And wp_users.ID=wp_usermeta.user_id And wp_users.ID IN (SELECT user_id FROM wp_usermeta WHERE meta_key = 'wp_capabilities' AND meta_value NOT LIKE '%customer%') ORDER BY ID ASC";
+                
                 ds1 = DAL.SQLHelper.ExecuteDataSet(sqlquery);
-
+                string result = string.Empty;
                 for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
                 {
                     clsUserDetails uobj = new clsUserDetails();
+                    //Code for role
+
+                    if (!string.IsNullOrEmpty(ds1.Tables[0].Rows[i]["meta_value"].ToString()) && ds1.Tables[0].Rows[i]["meta_value"].ToString().Length > 5 && ds1.Tables[0].Rows[i]["meta_value"].ToString().Substring(0, 2) == "a:")
+                    {
+                        if (ds1.Tables[0].Rows[i]["meta_value"].ToString().Trim() == "a:0:{}")
+                            ds1.Tables[0].Rows[i]["meta_value"] = "Unknown";
+                        else
+                            ds1.Tables[0].Rows[i]["meta_value"] = User_Role_Name(ds1.Tables[0].Rows[i]["meta_value"].ToString());
+                    }
+                    else
+                    {
+                        ds1.Tables[0].Rows[i]["meta_value"] = ds1.Tables[0].Rows[i]["meta_value"];
+                    }
+
                     uobj.ID = Convert.ToInt32(ds1.Tables[0].Rows[i]["ID"].ToString());
                     uobj.user_login = ds1.Tables[0].Rows[i]["user_login"].ToString();
-                    uobj.user_role = ds1.Tables[0].Rows[i]["meta_value"].ToString();
+                    result = ds1.Tables[0].Rows[i]["meta_value"].ToString();
+                    uobj.my = result;
                     uobj.user_email = ds1.Tables[0].Rows[i]["user_email"].ToString();
                     if ((ds1.Tables[0].Rows[i]["user_status"].ToString() == "0"))
                     { uobj.user_status = "Active"; }
                     else { uobj.user_status = "InActive"; }
+
+                    //Code For Role End
                     userslist.Add(uobj);
                 }
 
@@ -44,6 +62,55 @@ namespace LaylaERP.Models
 
             }
             
+        }
+
+        public static string User_Role_Name(string usertype)
+        {
+            string varUserType = string.Empty;
+            if (usertype != "Administrator" && usertype != "Accounting" && usertype != "Mod Squad" && usertype != "Author" && usertype != "Shop Manager" && usertype != "Subscriber" && usertype != "Supply Chain Manager" && usertype != "SEO Editor")
+            {
+
+                Models.clsSerialization vardeserilaziation = new Models.clsSerialization();
+                Hashtable ht = (Hashtable)vardeserilaziation.Deserialize(usertype);
+
+                if (ht.ContainsKey("administrator"))
+                {
+                    varUserType = "Administrator";
+                }
+                else if (ht.ContainsKey("accounting"))
+                {
+                    varUserType = "Accounting";
+                }
+                else if (ht.ContainsKey("modsquad"))
+                {
+                    varUserType = "Mod Squad";
+                }
+                else if (ht.ContainsKey("author"))
+                {
+                    varUserType = "Author";
+                }
+                else if (ht.ContainsKey("shop_manager"))
+                {
+                    varUserType = "Shop Manager";
+                }
+                else if (ht.ContainsKey("subscriber"))
+                {
+                    varUserType = "Subscriber";
+                }
+                else if (ht.ContainsKey("supplychainmanager"))
+                {
+                    varUserType = "Supply Chain Manager";
+                }
+                else if (ht.ContainsKey("wpseo_editor"))
+                {
+                    varUserType = "SEO Editor";
+                }
+                else
+                {
+                    varUserType = string.Empty;
+                }
+            }
+            return varUserType;
         }
     }
 }

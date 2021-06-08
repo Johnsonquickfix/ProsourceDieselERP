fae5ba58fb99436c62f19c236b877af681e95696
Tom Bell <tombell.quickfix@gmail.com>
2021-06-08 20:41:22 +0530

    Changes on users

--- LaylaERP/Controllers/UsersController.cs
+++ LaylaERP/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ using LaylaERP.Models;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Configuration;
-
+using LaylaERP.BAL;
 namespace LaylaERP.Controllers
 {
     public class UsersController : Controller
@@ -15,8 +15,9 @@ namespace LaylaERP.Controllers
         // GET: Users
         public ActionResult Users()
         {
-            
-               return View();
+
+            RoleCounter();
+            return View();
         }
 
         [HttpPost]
@@ -28,11 +29,15 @@ namespace LaylaERP.Controllers
             //return Json(new { data = Models.UsersRepositry.userslist }, JsonRequestBehavior.AllowGet);
             return View();
         }
+        public ActionResult AddNewUser(FormCollection dt)
+        {
+            return View();
+        }
 
         // GET: Assign Role
         public ActionResult AssignRole()
         {
-            GetRoles();
+            //GetRoles();
             return View();
         }
 
@@ -43,9 +48,24 @@ namespace LaylaERP.Controllers
             //string result = Models.UsersRepositry.userslist.ToString();
             //UsersRepositry.userslist.Clear();
 
-            Models.UsersRepositry.ShowUsersDetails();
-            return Json(new { data = Models.UsersRepositry.userslist }, JsonRequestBehavior.AllowGet);
+            UsersRepositry.ShowUsersDetails();
+            return Json(new { data = UsersRepositry.userslist }, JsonRequestBehavior.AllowGet);
         }
-        
+
+        private void RoleCounter()
+        {
+            ViewBag.admin = Convert.ToInt32(UsersRepositry.RoleCount().ToString());
+            ViewBag.modsquad = Convert.ToInt32(UsersRepositry.RoleCount(1).ToString());
+            ViewBag.customer = Convert.ToInt32(UsersRepositry.RoleCount(1, 2).ToString());
+            ViewBag.shopmanager = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3).ToString());
+            ViewBag.subscriber = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4).ToString());
+            ViewBag.supplychainmanager = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5).ToString());
+            ViewBag.seoeditor = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6).ToString());
+            ViewBag.author = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7).ToString());
+            ViewBag.norole = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8).ToString());
+            ViewBag.accounting = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8, 9).ToString());
+            ViewBag.all = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).ToString());
+
+        }
     }
 }

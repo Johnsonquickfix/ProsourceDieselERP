6cdf923b5f3cf7483be60962bc477ab7779e5338
Tom Bell <tombell.quickfix@gmail.com>
2021-06-09 11:29:12 +0530

    Users controller page

--- LaylaERP/Controllers/UsersController.cs
+++ LaylaERP/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using LaylaERP.Models;
 using MySql.Data.MySqlClient;
 using System.Data;
 using System.Configuration;
+using LaylaERP.BAL;
 
 namespace LaylaERP.Controllers
 {
@@ -63,6 +64,37 @@ namespace LaylaERP.Controllers
             UsersRepositry.ShowUsersDetails();
             return Json(new { data = UsersRepositry.userslist }, JsonRequestBehavior.AllowGet);
         }
-        
+
+        private void RoleCounter()
+        {
+            ViewBag.admin = Convert.ToInt32(UsersRepositry.RoleCount().ToString());
+            ViewBag.modsquad = Convert.ToInt32(UsersRepositry.RoleCount(1).ToString());
+            ViewBag.customer = Convert.ToInt32(UsersRepositry.RoleCount(1, 2).ToString());
+            ViewBag.shopmanager = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3).ToString());
+            ViewBag.subscriber = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4).ToString());
+            ViewBag.supplychainmanager = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5).ToString());
+            ViewBag.seoeditor = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6).ToString());
+            ViewBag.author = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7).ToString());
+            ViewBag.norole = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8).ToString());
+            ViewBag.accounting = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8, 9).ToString());
+            ViewBag.all = Convert.ToInt32(UsersRepositry.RoleCount(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).ToString());
+
+        }
+
+
+        public JsonResult GetRoles()
+        {
+            string t1 = "";
+            DataTable dt = new DataTable();
+            dt = BAL.Users.GetSystemRoles();
+            List<string> usertype = new List<string>();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                t1 = dt.Rows[i]["user_type"].ToString();
+                usertype.Add(t1);
+            }
+            return Json(usertype, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }

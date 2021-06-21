952f8b44456d045b1ca432215f6a13b10cb4b3fb
Tom Bell <tombell.quickfix@gmail.com>
2021-06-21 20:33:43 +0530

    Export

--- LaylaERP/BAL/ExportRepository.cs
+++ LaylaERP/BAL/ExportRepository.cs
@@ -14,46 +14,56 @@ namespace LaylaERP.BAL
     public class ExportRepository
     {
         public static List<Export_Details> usersexportlist = new List<Export_Details>();
-        public static void ExportUsersDetails()
+        public static void ExportUsersDetails(string from_dateusers, string to_dateusers)
         {
             try
             {
-                usersexportlist.Clear();
-                DataSet ds1 = new DataSet();
-                string sqlquery = "select ID, User_Image, user_login, user_status, if(user_status=0,'Active','InActive') as status,user_email,user_pass,meta_value from wp_users, wp_usermeta WHERE wp_users.user_registered IS NOT NULL and wp_usermeta.meta_key='wp_capabilities' And wp_users.ID=wp_usermeta.user_id And wp_users.ID IN (SELECT user_id FROM wp_usermeta WHERE meta_key = 'wp_capabilities' AND meta_value NOT LIKE '%customer%') ORDER BY ID DESC";
-                ds1 = DAL.SQLHelper.ExecuteDataSet(sqlquery);
-                string result = string.Empty;
-
-                for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
+                string sqlquery = string.Empty;
+                if (from_dateusers !="" && to_dateusers !="")
                 {
-                    Export_Details uobj = new Export_Details();
-                    
-
-                    if (!string.IsNullOrEmpty(ds1.Tables[0].Rows[i]["meta_value"].ToString()) && ds1.Tables[0].Rows[i]["meta_value"].ToString().Length > 5 && ds1.Tables[0].Rows[i]["meta_value"].ToString().Substring(0, 2) == "a:")
-                    {
-
-                        if (ds1.Tables[0].Rows[i]["meta_value"].ToString().Trim() == "a:0:{}")
-                            ds1.Tables[0].Rows[i]["meta_value"] = "Unknown";
-                        //result = ds1.Tables[0].Rows[i]["meta_value"].ToString();
-
-                        else
-                            ds1.Tables[0].Rows[i]["meta_value"] = User_Role_Name(ds1.Tables[0].Rows[i]["meta_value"].ToString());
-
-                    }
-                    else
-                    {
-                        ds1.Tables[0].Rows[i]["meta_value"] = ds1.Tables[0].Rows[i]["meta_value"];
-                    }
-
-                    uobj.UID = Convert.ToInt32(ds1.Tables[0].Rows[i]["ID"].ToString());
-                    uobj.user_login = ds1.Tables[0].Rows[i]["user_login"].ToString();
-                    result = ds1.Tables[0].Rows[i]["meta_value"].ToString();
-                    uobj.my = result;
-                    uobj.user_email = ds1.Tables[0].Rows[i]["user_email"].ToString();
-                    uobj.user_status = ds1.Tables[0].Rows[i]["status"].ToString();
-                    
-                    usersexportlist.Add(uobj);
+                    sqlquery = "select ID, User_Image, user_login, user_status, DATE(wp_users.user_registered) as created_date, if(user_status=0,'Active','InActive') as status,user_email,user_pass,meta_value from wp_users, wp_usermeta WHERE DATE(wp_users.user_registered)>='"+from_dateusers+ "' and DATE(wp_users.user_registered)<='" + to_dateusers + "' and wp_usermeta.meta_key='wp_capabilities' And wp_users.ID=wp_usermeta.user_id And wp_users.ID IN (SELECT user_id FROM wp_usermeta WHERE meta_key = 'wp_capabilities' AND meta_value NOT LIKE '%customer%') ORDER BY ID DESC";
                 }
+                else
+                {
+                    sqlquery = "select ID, User_Image, user_login, user_status, DATE(wp_users.user_registered) as created_date, if(user_status=0,'Active','InActive') as status,user_email,user_pass,meta_value from wp_users, wp_usermeta WHERE wp_users.user_registered IS NOT NULL and wp_usermeta.meta_key='wp_capabilities' And wp_users.ID=wp_usermeta.user_id And wp_users.ID IN (SELECT user_id FROM wp_usermeta WHERE meta_key = 'wp_capabilities' AND meta_value NOT LIKE '%customer%') ORDER BY ID DESC";
+                }
+                    usersexportlist.Clear();
+                    DataSet ds1 = new DataSet();
+
+                    ds1 = DAL.SQLHelper.ExecuteDataSet(sqlquery);
+                    string result = string.Empty;
+
+                    for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
+                    {
+                        Export_Details uobj = new Export_Details();
+
+
+                        if (!string.IsNullOrEmpty(ds1.Tables[0].Rows[i]["meta_value"].ToString()) && ds1.Tables[0].Rows[i]["meta_value"].ToString().Length > 5 && ds1.Tables[0].Rows[i]["meta_value"].ToString().Substring(0, 2) == "a:")
+                        {
+
+                            if (ds1.Tables[0].Rows[i]["meta_value"].ToString().Trim() == "a:0:{}")
+                                ds1.Tables[0].Rows[i]["meta_value"] = "Unknown";
+                            //result = ds1.Tables[0].Rows[i]["meta_value"].ToString();
+
+                            else
+                                ds1.Tables[0].Rows[i]["meta_value"] = User_Role_Name(ds1.Tables[0].Rows[i]["meta_value"].ToString());
+
+                        }
+                        else
+                        {
+                            ds1.Tables[0].Rows[i]["meta_value"] = ds1.Tables[0].Rows[i]["meta_value"];
+                        }
+
+                        uobj.UID = Convert.ToInt32(ds1.Tables[0].Rows[i]["ID"].ToString());
+                        uobj.user_login = ds1.Tables[0].Rows[i]["user_login"].ToString();
+                        result = ds1.Tables[0].Rows[i]["meta_value"].ToString();
+                        uobj.my = result;
+                        uobj.user_email = ds1.Tables[0].Rows[i]["user_email"].ToString();
+                        uobj.user_status = ds1.Tables[0].Rows[i]["status"].ToString();
+                        uobj.created_date = Convert.ToDateTime(ds1.Tables[0].Rows[i]["created_date"].ToString());
+                        usersexportlist.Add(uobj);
+                    }
+                
 
             }
             catch (Exception e)
@@ -119,16 +129,33 @@ namespace LaylaERP.BAL
         //"(SELECT format(meta_value, 2) FROM wp_woocommerce_order_itemmeta WHERE order_item_id = oi.order_item_id AND meta_key = '_line_subtotal') as subtotal," +
         //"(SELECT format(meta_value, 2) FROM wp_woocommerce_order_itemmeta WHERE order_item_id = oi.order_item_id AND meta_key = '_line_total') as total" +
         //" FROM wp_posts o LEFT JOIN wp_woocommerce_order_items oi ON oi.order_id = o.id LEFT JOIN wp_posts p ON p.ID = oi.order_item_id WHERE o.post_type = 'shop_order' limit 2000 ";
-        public static string ssql = "select ws.order_id as order_id, ws.date_created as order_created, substring(ws.status,4) as status,  ws.num_items_sold as qty,format(ws.total_sales, 2) as subtotal,format(ws.net_total, 2) as total, ws.customer_id as customer_id from wp_wc_order_stats ws, wp_users wu where ws.customer_id = wu.ID order by ws.order_id desc limit 6000";
+        //public static 
 
 
         public static List<Export_Details> exportorderlist = new List<Export_Details>();
 
-        public static void ExportOrderDetails()
+        public static void ExportOrderDetails(string from_date , string to_date)
         {
             try
             {
                 exportorderlist.Clear();
+                string ssql;
+                
+                if (from_date!="" && to_date!="")
+                {
+                    DateTime fromdate = DateTime.Now, todate = DateTime.Now;
+                    fromdate = DateTime.Parse(from_date);
+                    todate = DateTime.Parse(to_date);
+                    
+                    ssql = "select ws.order_id as order_id, ws.date_created as order_created, substring(ws.status,4) as status,  ws.num_items_sold as qty,format(ws.total_sales, 2) as subtotal,format(ws.net_total, 2) as total, ws.customer_id as customer_id from wp_wc_order_stats ws, wp_users wu where ws.customer_id = wu.ID and DATE(ws.date_created)>='"+ fromdate.ToString("yyyy-MM-dd") + "' and DATE(ws.date_created)<='" + todate.ToString("yyyy-MM-dd") + "' order by ws.order_id desc limit 100";
+                    
+                }
+                else
+                {
+                    ssql = "select ws.order_id as order_id, ws.date_created as order_created, substring(ws.status,4) as status,  ws.num_items_sold as qty,format(ws.total_sales, 2) as subtotal,format(ws.net_total, 2) as total, ws.customer_id as customer_id from wp_wc_order_stats ws, wp_users wu where ws.customer_id = wu.ID order by ws.order_id desc limit 1000";
+                }
+
+
                 DataSet ds1 = new DataSet();
                 ds1 = DAL.SQLHelper.ExecuteDataSet(ssql);
                 for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
@@ -147,6 +174,12 @@ namespace LaylaERP.BAL
                 }
             }
             catch (Exception ex) { throw ex; }
+        }
+
+        public static void myexport(string mydate)
+        {
+            string my = string.Empty;
+            my = mydate;
         }
 
     }

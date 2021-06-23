b6fae105b7344d0246ec9262e6321f67191d106f
david.quickfix1 <david.quickfix1@gmail.com>
2021-06-23 19:39:18 +0530

    changes FeeN Tax

--- LaylaERP/Controllers/FeeNTaxController.cs
+++ LaylaERP/Controllers/FeeNTaxController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LaylaERP.Models;
+using LaylaERP.BAL;
+using System.Data;
+using Newtonsoft.Json;
 
 namespace LaylaERP.Controllers
 {
@@ -13,6 +17,104 @@ namespace LaylaERP.Controllers
         {
             return View("StateRecycleTax");
         }
+        [HttpGet]
+        public ActionResult StateRecycleTax(long id = 0)
+        {
+            FeeNTax model = new FeeNTax();
+            ViewBag.id = id;
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult StateRecycleTax(FeeNTax model)
+        {
+            FeeNTaxRepository FNT = new FeeNTaxRepository();
+            if (ModelState.IsValid)
+            {
+                if (model.id > 0)
+                {
+
+                    FNT.EditFeeNTaxStatus(model);
+                    return Json(new { status = true, message = "Customer Record has been updated successfully!!", url = "" }, 0);
+                }
+                else
+                {
+                    // int ID = Repo.AddNewCustomer(model);
+
+                    FNT.AddFeeNTax(model);
+
+                    ModelState.Clear();
+                    return Json(new { status = true, message = "Customer Record has been saved successfully!!", url = "" }, 0);
+
+
+                }
+            }
+            return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
+        }
+
+        public JsonResult GetFeeNTaxByID(FeeNTax model)
+        {
+            string JSONresult = string.Empty;
+            try
+            {
+
+                DataTable dt = FeeNTaxRepository.FeeNTaxByID(model);
+                JSONresult = JsonConvert.SerializeObject(dt);
+            }
+            catch { }
+            return Json(JSONresult, 0);
+        }
+        public JsonResult UpdateFeeNTax(FeeNTax model)
+        {
+            FeeNTaxRepository FNT = new FeeNTaxRepository();
+
+            if (ModelState.IsValid)
+            {
+
+                FNT.EditFeeNTaxStatus(model);
+                return Json(new { status = true, message = "Customer Status has been updated successfully!!", url = "" }, 0);
+
+            }
+            return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult GetFeeNTaxList()
+        {
+            FeeNTax FNT = new FeeNTax();
+            DataTable dt = new DataTable();
+            dt = FeeNTaxRepository.FeeNTaxList();
+            List<FeeNTax> lststu = new List<FeeNTax>();
+            if (dt != null)
+            {
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+
+                        FeeNTax obj = new FeeNTax();
+                        obj.staterecyclefee = Convert.ToUInt64(dt.Rows[i]["staterecyclefee"].ToString());
+                        obj.item_name = dt.Rows[i]["item_name"].ToString();
+                        obj.state = dt.Rows[i]["state"].ToString();
+                        obj.id = Convert.ToInt32(dt.Rows[i]["id"].ToString());
+                        obj.zip = Convert.ToInt32(dt.Rows[i]["zip"].ToString());
+                        obj.city = dt.Rows[i]["city"].ToString();
+                        obj.country = dt.Rows[i]["country"].ToString();
+
+                        lststu.Add(obj);
+                    }
+
+
+                }
+
+                FNT.lst = lststu;
+
+            }
+            return Json(lststu, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
+
---
+++ LaylaERP/js/FeeNTax/FeeNTax.js
@@ -0,0 +1,118 @@
+﻿
+function AddFeeNTax() {
+    debugger
+
+    Id = $("#hfid").val();
+    Staterecyclefee = $("#StateRecycleFee").val();
+    //item_parent_id = $("#").val();
+    Item_name = $("#Item_Name").val();
+    City = $("#shipcity").val();
+    State = $("#shipstate").val();
+    Zip = $("#Ship_Zip_PostCode").val();
+    Country = $("#shipcountry").val();
+   
+
+    if (Staterecyclefee == "") {
+        swal('alert', 'Please Enter StateRecyclefee', 'error').then(function () { swal.close(); $('#StateRecycleFee').focus(); })
+    }
+    else if (Item_name == "") {
+        swal('alert', 'Please Enter item_name', 'error').then(function () { swal.close(); $('#Item_Name').focus(); })
+    }
+    else if (Zip == "") {
+        swal('alert', 'Please Enter zip', 'error').then(function () { swal.close(); $('#Ship_Zip_PostCode').focus(); })
+    }
+   
+    else {
+        var obj = {          
+            staterecyclefee: Staterecyclefee, item_name: Item_name, city: City, state: State, zip: Zip,
+            country: Country
+        }
+        $.ajax({
+            url: '/FeeNTax/StateRecycleTax/', dataType: 'json', type: 'Post',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(obj),
+            dataType: "json",
+            success: function (data) {
+                if (data.status == true) {
+
+                    swal('Alert!', data.message, 'success');
+                  
+                }
+                else {
+                    swal('Alert!', data.message, 'error')
+                }
+            },
+            complete: function () {
+                $("#loader").hide();
+            },
+            error: function (error) {
+                swal('Error!', 'something went wrong', 'error');
+            },
+        })
+
+//function EditUser(id) {
+        //    window.location.href = 'StateRecyleTax?id=' + i.id;
+        //}
+
+
+function GetFeeNTaxByID(id) {
+            debugger
+            var ID = id;
+            var obj = {};
+            $.ajax({
+
+                url: '/FeeNTax/GetFeeNTaxByID/' + ID,
+                type: 'GET',
+                contentType: "application/json; charset=utf-8",
+                dataType: 'JSON',
+                data: JSON.stringify(obj),
+                success: function (data) {
+                    var i = JSON.parse(data);
+                    $("#StateRecycleFee").val(i[0].staterecyclefee);
+                    $("#shipstate").val(i[0].state);
+                    $("#Item_Name").val(i[0].item_name);
+                    $("#shipcity").val(i[0].city);
+                    $("#Ship_Zip_PostCode").val(i[0].zip);
+                    $("#shipcountry").val(i[0].country);
+
+
+                },
+                error: function (msg) { alert(msg); }
+            });
+
+        }
+
+function UpdateFeeNTaxStatus() {
+            
+            var obj = { strVal: ID }
+            debugger
+            $.ajax({
+                url: '/FeeNTax/UpdateFeeNTax/', dataType: 'json', type: 'Post',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(obj),
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    if (data.status == true) {
+                        swal('Alert!', data.message, 'success');
+                    }
+                    else {
+                        swal('Alert!', data.message, 'error')
+                    }
+                },
+                error: function (error) {
+                    swal('Error!', 'something went wrong', 'error');
+                },
+            })
+        }
+
+        $("#StateRecycleFee").val('');
+        //$("#").val();
+        $("#Item_Name").val('');
+        $("#shipcity").val('');
+        $("#shipstate").val('');
+        $("#Ship_Zip_PostCode").val('');
+        $("#shipcountry").val('');
+    }
+
+}

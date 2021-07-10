13f298f15fc764c3043c766d125ef11b37a95de6
david.quickfix1 <david.quickfix1@gmail.com>
2021-07-10 16:45:20 +0530

    changes users html.

--- LaylaERP/BAL/UsersRepositry.cs
+++ LaylaERP/BAL/UsersRepositry.cs
@@ -473,7 +473,7 @@ namespace LaylaERP.BAL
         {
             try
             {
-                string strquery = "select count(ZipCode) from ZIPCodes1 where city = '" + model.billing_city + "' and statefullname = '" + model.billing_state + "' and ZipCode = '" + model.billing_postcode + "' ";
+                string strquery = "select count(ZipCode) from ZIPCodes1 where city = '" + model.billing_city + "' and (statefullname = '" + model.billing_state + "' Or state='" + model.billing_state + "') and ZipCode = '" + model.billing_postcode + "' ";
                 MySqlParameter[] para =
                 {
 

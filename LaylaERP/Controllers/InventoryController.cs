4d8fa1936daddc827327273c2d6118e08ac51d44
Steven Methew <steven.quickfix@gmail.com>
2021-07-19 16:18:30 +0530

    Inventory Edit

--- LaylaERP/Controllers/InventoryController.cs
+++ LaylaERP/Controllers/InventoryController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using LaylaERP.BAL;
+using LaylaERP.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +16,53 @@ namespace LaylaERP.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult GetVarientList(InventoryModel model)
+        {
+            string result = string.Empty;
+            int TotalRecord = 0;
+            try
+            {
+                string urid = "";
+                if (model.user_status != "")
+                    urid = model.user_status;
+                string searchid = model.Search;
+                DataTable dt = InventoryRepository.GetVarients(model.strValue1, urid, searchid, model.PageNo, model.PageSize, out TotalRecord, model.SortCol, model.SortDir);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(new { sEcho = model.sEcho, recordsTotal = TotalRecord, recordsFiltered = TotalRecord, iTotalRecords = TotalRecord, iTotalDisplayRecords = TotalRecord, aaData = result }, 0);
+        }
+        public JsonResult GetProductList()
+        {
+            DataSet ds = InventoryRepository.GetProducts(); ;
+            List<SelectListItem> productlist = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+
+                productlist.Add(new SelectListItem { Text = dr["post_title"].ToString(), Value = dr["id"].ToString() });
+
+            }
+            return Json(productlist, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult UpdateInventoryStock(InventoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string meta_id = model.meta_id;
+                string meta_value = model.meta_value;
+                    
+                if (meta_id != "")
+                {
+                    new InventoryRepository().EditInventoryStock(meta_id,meta_value);
+                    return Json(new { status = true, message = "Inventory Stock has been updated successfully!!", url = "" }, 0);
+                }
+                else
+                {
+                    return Json(new { status = false, message = "Something went wrong", url = "" }, 0);
+                }
+            }
+            return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
         }
     }
 }
--- LaylaERP/Models/InventoryModel.cs
+++ LaylaERP/Models/InventoryModel.cs
@@ -5,7 +5,15 @@ using System.Web;
 
 namespace LaylaERP.Models
 {
-    public class InventoryModel
+    public class InventoryModel : PaggingModel
     {
+        public int ID { get; set; }
+        public string post_title { get; set; }
+        public string Count { get; set; }
+        public string strValue1 { get; set; }
+        public string user_status { get; set; }
+        public string Search { get; set; }
+        public string meta_id { get; set; }
+        public string meta_value { get; set; }
     }
 }

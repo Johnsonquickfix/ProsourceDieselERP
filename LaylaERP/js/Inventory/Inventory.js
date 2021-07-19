8e69159cfa29588c7b5286ee820de10e08142a0b
Steven Methew <steven.quickfix@gmail.com>
2021-07-19 20:08:33 +0530

    Inventory change

--- LaylaERP/js/Inventory/Inventory.js
+++ LaylaERP/js/Inventory/Inventory.js
@@ -44,7 +44,7 @@ function ProductGrid() {
         aoColumns: [
            
           
-            { data: 'post_title', title: 'Product', sWidth: "14%" },
+            { data: 'post', title: 'Product', sWidth: "14%" },
             {
                 'data': 'Count', sWidth: "8%",
                 'render': function (id, type, full, meta) {

9daa9162ff977fefc887617dd6bd37ac7526ae98
david.quickfix1 <david.quickfix1@gmail.com>
2021-07-20 17:32:13 +0530

    changes product controllers.

---
+++ LaylaERP/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaylaERP.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add Product
+        public ActionResult AddNewProduct()
+        {
+            return View();
+        }
+
+        // Listing Products
+        public ActionResult ListProduct()
+        {
+            return View();
+        }
+
+        // Products Categories
+        public ActionResult ProductCategories()
+        {
+            return View();
+        }
+
+        // Products Attributes
+        public ActionResult ProductAttributes()
+        {
+            return View();
+        }
+    }
+}

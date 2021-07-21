3f4ee403827dcc73532a1c2db78a3133d485f04b
Steven Methew <steven.quickfix@gmail.com>
2021-07-21 20:42:30 +0530

    Vendor Add

--- LaylaERP/BAL/ThirdPartyRepository.cs
+++ LaylaERP/BAL/ThirdPartyRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LaylaERP.Controllers;
+using LaylaERP.DAL;
+using LaylaERP.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +11,50 @@ namespace LaylaERP.BAL
 {
     public class ThirdPartyRepository
     {
+        public int AddNewVendor(ThirdPartyModel model)
+        {
+            try
+            {
+                string strsql = "";
+                 strsql = "insert into wp_vendor(nom,name_alias,client,fournisseur,code_fournisseur,status,address,zip,town,fk_pays,fk_departement,phone,fax,email,url,siren,tva_assuj,tva_intra,fk_typent,fk_effectif,fk_forme_juridique,capital,location_incoterms,logo) values(@nom, @name_alias, @client, @fournisseur, @code_fournisseur, @status, @address, @zip, @town, @fk_pays, @fk_departement, @phone, @fax, @email, @url, @siren, @tva_assuj, @tva_intra, @fk_typent, @fk_effectif, @fk_forme_juridique, @capital, @location_incoterms, @logo); SELECT LAST_INSERT_ID();";
+                MySqlParameter[] para =
+                {
+                    new MySqlParameter("@nom", model.Name),
+                    new MySqlParameter("@name_alias", model.AliasName),
+                    new MySqlParameter("@client", model.Prospect),
+                    new MySqlParameter("@fournisseur", model.Vendor),
+                    new MySqlParameter("@code_fournisseur", model.VendorCode),
+                    new MySqlParameter("@status", model.Status),
+                    new MySqlParameter("@address", model.Address),
+                    new MySqlParameter("@zip", model.ZipCode),
+                    new MySqlParameter("@town", model.City),
+                    new MySqlParameter("@fk_pays", model.Country),
+                    new MySqlParameter("@fk_departement", model.State),
+                    new MySqlParameter("@fax", model.Fax),
+                    new MySqlParameter("@email", model.EMail),
+                    new MySqlParameter("@url", model.Web),
+                    new MySqlParameter("@siren", model.ProfId),
+                    new MySqlParameter("@tva_assuj", model.SalesTaxUsed),
+                    new MySqlParameter("@tva_intra", model.VATID),
+                    new MySqlParameter("@fk_typent", model.ThirdPartyType),
+                    new MySqlParameter("@fk_effectif", model.Workforce),
+                    new MySqlParameter("@fk_forme_juridique", model.BusinessEntityType),
+                    new MySqlParameter("@capital", model.Capital),
+                    new MySqlParameter("@location_incoterms", model.Incoterms),
+                    new MySqlParameter("@logo", model.Logo),
+                };
+                int result = Convert.ToInt32(SQLHelper.ExecuteScalar(strsql, para));
+                return result;
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+
+        internal int AddNewVendor(ThirdPartyController model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- LaylaERP/Controllers/ThirdPartyController.cs
+++ LaylaERP/Controllers/ThirdPartyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LaylaERP.BAL;
+using LaylaERP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +19,31 @@ namespace LaylaERP.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public JsonResult AddVendor(ThirdPartyModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.rowid > 0)
+                {
+                    //Repo.EditCustomer(model, model.row);
+                    //return Json(new { status = true, message = "Customer Record has been updated successfully!!", url = "", id = model.ID }, 0);
+                }
+                else
+                {
+                    int ID = new ThirdPartyRepository().AddNewVendor(model);
+                    if (ID > 0)
+                    {
+                        ModelState.Clear();
+                        return Json(new { status = true, message = "Customer Record has been saved successfully!!", url = "", id = ID }, 0);
+                    }
+                    else
+                    {
+                        return Json(new { status = false, message = "Invalid Details", url = "", id = 0 }, 0);
+                    }
+                }
+            }
+            return Json(new { status = false, message = "Invalid Details", url = "", id = 0 }, 0);
+        }
     }
 }

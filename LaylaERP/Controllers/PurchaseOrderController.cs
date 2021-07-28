c6742beb4a95e70ef8a1f87a842d61373378fe4a
Steven Methew <steven.quickfix@gmail.com>
2021-07-28 20:45:02 +0530

    New Purchase

--- LaylaERP/Controllers/PurchaseOrderController.cs
+++ LaylaERP/Controllers/PurchaseOrderController.cs
@@ -28,7 +28,18 @@ namespace LaylaERP.Controllers
             return Json(productlist, JsonRequestBehavior.AllowGet);
 
         }
-        public JsonResult GetIncotermByID(ThirdPartyModel model)
+        public JsonResult GetVendor(SearchModel model)
+        {
+            DataSet ds = BAL.PurchaseOrderRepository.GetVendor();
+            List<SelectListItem> productlist = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                productlist.Add(new SelectListItem { Text = dr["Name"].ToString(), Value = dr["ID"].ToString() });
+            }
+            return Json(productlist, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetIncotermByID(PurchaseOrderModel model)
         {
             int id = model.IncotermsTypeID;
             string result = string.Empty;
@@ -39,6 +50,69 @@ namespace LaylaERP.Controllers
             }
             catch (Exception ex) { throw ex; }
             return Json(result, 0);
+        }
+        public JsonResult GetVendorByID(PurchaseOrderModel model)
+        {
+            int id = model.VendorID;
+            string result = string.Empty;
+            try
+            {
+                DataTable dt = PurchaseOrderRepository.GetVendorByID(id);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(result, 0);
+        }
+        public JsonResult GetPaymentTerm(SearchModel model)
+        {
+            DataSet ds = BAL.PurchaseOrderRepository.GetPaymentTerm();
+            List<SelectListItem> productlist = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+
+                productlist.Add(new SelectListItem { Text = dr["PaymentTerm"].ToString(), Value = dr["ID"].ToString() });
+
+            }
+            return Json(productlist, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetPaymentType(SearchModel model)
+        {
+            DataSet ds = BAL.PurchaseOrderRepository.GetPaymentType();
+            List<SelectListItem> productlist = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+
+                productlist.Add(new SelectListItem { Text = dr["PaymentType"].ToString(), Value = dr["ID"].ToString() });
+
+            }
+            return Json(productlist, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult NewPurchaseOrder(PurchaseOrderModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.rowid > 0)
+                {
+                    new PurchaseOrderRepository().EditPurchase(model, model.rowid);
+                    return Json(new { status = true, message = "Purchase Record has been updated successfully!!", url = "", id = model.rowid }, 0);
+                }
+                else
+                {
+                    int ID = new PurchaseOrderRepository().AddNewPurchase(model);
+                    if (ID > 0)
+                    {
+                        ModelState.Clear();
+                        return Json(new { status = true, message = "Purchase Record has been saved successfully!!", url = "", id = ID }, 0);
+                    }
+                    else
+                    {
+                        return Json(new { status = false, message = "Invalid Details", url = "", id = 0 }, 0);
+                    }
+                }
+            }
+            return Json(new { status = false, message = "Invalid Details", url = "", id = 0 }, 0);
         }
     }
 }
--- LaylaERP/js/PurchaseOrder/NewPurchase.js
+++ LaylaERP/js/PurchaseOrder/NewPurchase.js
@@ -1,11 +1,28 @@
 ﻿$("#loader").hide();
 getIncoterm();
+getVendor();
+getPaymentTerm();
+getPaymentType();
 
+$('#ddlVendor').change(function () {
+    var VendorID = $('#ddlVendor').val();
+    var obj = { VendorID: VendorID };
+    jQuery.ajax({
+        url: "/PurchaseOrder/GetVendorByID", dataType: 'json', type: "Post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(obj),
+        success: function (data) {
+            data = JSON.parse(data);
+            $('#txtRefvendor').val(data[0].vendor);
+        },
+        error: function (jqXHR, textStatus, errorThrown) { swal('Error!', errorThrown, "error"); }
+    });
+})
 $('#ddlIncoTerms').change(function () {
     var IncotermsTypeID = $('#ddlIncoTerms').val();
     var obj = { IncotermsTypeID: IncotermsTypeID };
     jQuery.ajax({
-        url: "/ThirdParty/GetIncotermByID", dataType: 'json', type: "Post",
+        url: "/PurchaseOrder/GetIncotermByID", dataType: 'json', type: "Post",
         contentType: "application/json; charset=utf-8",
         data: JSON.stringify(obj),
         success: function (data) {
@@ -17,14 +34,58 @@ $('#ddlIncoTerms').change(function () {
 })
 function getIncoterm() {
     $.ajax({
-        url: "/ThirdParty/GetIncoterm",
+        url: "/PurchaseOrder/GetIncoterm",
         type: "Get",
         success: function (data) {
-            var opt = '<option value="-1">Please Select IncoTerm</option>';
+            var opt = '<option value="-1">Please Select IncoTerms</option>';
             for (var i = 0; i < data.length; i++) {
                 opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
             }
             $('#ddlIncoTerms').html(opt);
+        }
+
+    });
+}
+function getVendor() {
+    $.ajax({
+        url: "/PurchaseOrder/GetVendor",
+        type: "Get",
+        success: function (data) {
+            var opt = '<option value="-1">Please Select Vendor</option>';
+            for (var i = 0; i < data.length; i++) {
+                opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
+            }
+            $('#ddlVendor').html(opt);
+        }
+
+    });
+}
+function getPaymentTerm() {
+
+    $.ajax({
+        url: "/PurchaseOrder/GetPaymentTerm",
+        type: "Get",
+        success: function (data) {
+            var opt = '<option value="-1">Please Select Payment Term</option>';
+            for (var i = 0; i < data.length; i++) {
+                opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
+            }
+            $('#ddlPaymentTerms').html(opt);
+        }
+
+    });
+}
+function getPaymentType() {
+
+    $.ajax({
+        url: "/PurchaseOrder/GetPaymentType",
+        type: "Get",
+        success: function (data) {
+            var opt = '<option value="-1">Please Select Payment Type</option>';
+            for (var i = 0; i < data.length; i++) {
+                opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
+            }
+            $('#ddlPaymentType').html(opt);
         }
 
     });

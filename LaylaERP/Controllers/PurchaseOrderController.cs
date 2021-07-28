f442c36dc6d85b48593edc059c8906ac1c6219dd
Steven Methew <steven.quickfix@gmail.com>
2021-07-28 20:44:40 +0530

    New Purchase

---
+++ LaylaERP/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,44 @@
+﻿using LaylaERP.BAL;
+using LaylaERP.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaylaERP.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        // GET: PurchaseOrder
+        public ActionResult NewPurchaseOrder()
+        {
+            return View();
+        }
+        public JsonResult GetIncoterm(SearchModel model)
+        {
+            DataSet ds = BAL.PurchaseOrderRepository.GetIncoterm();
+            List<SelectListItem> productlist = new List<SelectListItem>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                productlist.Add(new SelectListItem { Text = dr["IncoTerm"].ToString(), Value = dr["ID"].ToString() });
+            }
+            return Json(productlist, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetIncotermByID(ThirdPartyModel model)
+        {
+            int id = model.IncotermsTypeID;
+            string result = string.Empty;
+            try
+            {
+                DataTable dt = PurchaseOrderRepository.GetIncotermByID(id);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(result, 0);
+        }
+    }
+}
---
+++ LaylaERP/js/PurchaseOrder/NewPurchase.js
@@ -0,0 +1,31 @@
+﻿$("#loader").hide();
+getIncoterm();
+
+$('#ddlIncoTerms').change(function () {
+    var IncotermsTypeID = $('#ddlIncoTerms').val();
+    var obj = { IncotermsTypeID: IncotermsTypeID };
+    jQuery.ajax({
+        url: "/ThirdParty/GetIncotermByID", dataType: 'json', type: "Post",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(obj),
+        success: function (data) {
+            data = JSON.parse(data);
+            $('#txtIncoTerms').val(data[0].short_description);
+        },
+        error: function (jqXHR, textStatus, errorThrown) { swal('Error!', errorThrown, "error"); }
+    });
+})
+function getIncoterm() {
+    $.ajax({
+        url: "/ThirdParty/GetIncoterm",
+        type: "Get",
+        success: function (data) {
+            var opt = '<option value="-1">Please Select IncoTerm</option>';
+            for (var i = 0; i < data.length; i++) {
+                opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
+            }
+            $('#ddlIncoTerms').html(opt);
+        }
+
+    });
+}

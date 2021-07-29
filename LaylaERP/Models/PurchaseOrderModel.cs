108e4100dedfb7a45c85beb787388cb9f340edaa
Steven Methew <steven.quickfix@gmail.com>
2021-07-29 15:57:35 +0530

    Purchase Order

--- LaylaERP/Models/PurchaseOrderModel.cs
+++ LaylaERP/Models/PurchaseOrderModel.cs
@@ -8,8 +8,14 @@ namespace LaylaERP.Models
     public class PurchaseOrderModel
     {
         public int rowid { get; set; }
-        public int IncotermsTypeID { get; set; }
         public int VendorID { get; set; }
+        public string Vendor { get; set; }
+        public string VendorCode { get; set; }
+        public int PaymentTerms { get; set; }
+        public int Balancedays { get; set; }
+        public int PaymentType { get; set; }
+        public string Planneddateofdelivery { get; set; }
+        public int IncotermsTypeID { get; set; }
         public int fk_statut { get; set; }
         public string note_private { get; set; }
         public string note_public { get; set; }

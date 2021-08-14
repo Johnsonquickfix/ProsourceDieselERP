ed40a67145126f0dd14a503c5e3d36abc87abbb7
david.quickfix1 <david.quickfix1@gmail.com>
2021-08-14 12:20:37 +0530

    chart of account

--- LaylaERP/Controllers/AccountingController.cs
+++ LaylaERP/Controllers/AccountingController.cs
@@ -13,5 +13,11 @@ namespace LaylaERP.Controllers
         {
             return View();
         }
+
+        // GET: chart of accounts
+        public ActionResult chartofaccounts()
+        {
+            return View();
+        }
     }
 }

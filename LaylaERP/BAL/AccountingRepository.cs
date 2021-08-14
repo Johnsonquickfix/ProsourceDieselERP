db697bcf3608aaf98837bfcca3d086abed4703f8
Steven Methew <steven.quickfix@gmail.com>
2021-08-14 18:37:12 +0530

    Accounting Journal

---
+++ LaylaERP/BAL/AccountingRepository.cs
@@ -0,0 +1,25 @@
+﻿using LaylaERP.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace LaylaERP.BAL
+{
+    public class AccountingRepository
+    {
+        public static DataSet GetNatureofJournal()
+        {
+            DataSet DS = new DataSet();
+            try
+            {
+                string strSQl = "Select ID,Nature from erp_NatureofJournal";
+                DS = SQLHelper.ExecuteDataSet(strSQl);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            return DS;
+        }
+    }
+}
---
+++ LaylaERP/Models/AccountingJournalModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaylaERP.Models
+{
+    public class AccountingJournalModel
+    {
+        public int rowid { get; set; }
+        public int entity { get; set; }
+        public string code { get; set; }
+        public string label { get; set; }
+        public int nature { get; set; }
+        public int active { get; set; }
+    }
+}

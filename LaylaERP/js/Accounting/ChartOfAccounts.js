7b0da44f7acd35834e1fab2548b5ca256ab968f0
Tom Bell <tombell.quickfix@gmail.com>
2021-08-16 20:44:50 +0530

    chartofaccounts

---
+++ LaylaERP/js/Accounting/ChartOfAccounts.js
@@ -0,0 +1,39 @@
+﻿PurchaseOrderGrid();
+function PurchaseOrderGrid() {
+    var urid = parseInt($("#ddlSearchStatus").val());
+    var sid = "";
+    var obj = { user_status: urid, Search: sid, PageNo: 0, PageSize: 50, sEcho: 1, SortCol: 'id', SortDir: 'desc' };
+    $('#dtdata').DataTable({
+        columnDefs: [{ "orderable": false, "targets": 0 }], order: [[1, "desc"]],
+        destroy: true, bProcessing: true, bServerSide: true,
+        sPaginationType: "full_numbers", searching: true, ordering: true, lengthChange: true, "paging": true,
+        bAutoWidth: false, scrollX: false, scrollY: false,
+        lengthMenu: [[10, 20, 50], [10, 20, 50]],
+        sAjaxSource: "/PurchaseOrder/GetPurchaseOrderList",
+        fnServerData: function (sSource, aoData, fnCallback, oSettings) {
+
+            obj.sEcho = aoData[0].value; obj.PageSize = oSettings._iDisplayLength; obj.PageNo = oSettings._iDisplayStart;
+            $.ajax({
+                type: "POST", url: sSource, async: true, contentType: "application/json; charset=utf-8", dataType: "json", data: JSON.stringify(obj),
+                success: function (data) {
+                    var dtOption = { sEcho: data.sEcho, recordsTotal: data.recordsTotal, recordsFiltered: data.recordsFiltered, iTotalRecords: data.iTotalRecords, iTotalDisplayRecords: data.iTotalDisplayRecords, aaData: JSON.parse(data.aaData) };
+                    return fnCallback(dtOption);
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) { alert(errorThrown); },
+                async: false
+            });
+        },
+        aoColumns: [
+
+            { data: 'account_number', title: 'Account Number', sWidth: "10%" },
+            { data: 'label', title: 'Label', sWidth: "10%" },
+            { data: 'account_parent', title: 'Parent Account', sWidth: "10%" },
+            { data: 'fk_pcg_version', title: 'Group', sWidth: "10%" },
+
+        ]
+    });
+}
+
+$('#btnSearch').click(function () {
+    PurchaseOrderGrid();
+})

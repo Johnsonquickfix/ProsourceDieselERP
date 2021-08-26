cf94bb54acb7dcb24f7bd80b7bdc1e0c232d7a51
Steven Methew <steven.quickfix@gmail.com>
2021-08-26 19:17:52 +0530

    Vendor Masking

---
+++ LaylaERP/Models/AccountingModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaylaERP.Models
+{
+    //AccountingModel not in use
+    public class AccountingModel
+    {
+        public int ID { get; set; }
+        public long fk_product_id { get; set; }
+        public string Productfor { get; set; }
+        public int fk_account_number { get; set; }
+    }
+    public class ProductAccountingModel
+    {
+        public int ID { get; set; }
+        public long fk_product_id { get; set; }
+        public string Productfor { get; set; }
+        public int fk_account_number { get; set; }
+    }
+}

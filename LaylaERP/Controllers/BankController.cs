394c0ef196bf7fd7027de8d10f85afa406f29900
Tom Bell <tombell.quickfix@gmail.com>
2021-08-30 20:32:25 +0530

    Bank

--- LaylaERP/Controllers/BankController.cs
+++ LaylaERP/Controllers/BankController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using LaylaERP.BAL;
@@ -120,5 +122,97 @@ namespace LaylaERP.Controllers
                 return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
             }
         }
+
+        [HttpPost]
+        public ActionResult FileUpload(int BankID, HttpPostedFileBase ImageFile)
+        {
+            try
+            {
+                BankModel model = new BankModel();
+                if (ImageFile != null)
+                {
+                    string FileName = Path.GetFileNameWithoutExtension(ImageFile.FileName);
+                    FileName = Regex.Replace(FileName, @"\s+", "");
+                    string size = (ImageFile.ContentLength / 1024).ToString();
+                    string FileExtension = Path.GetExtension(ImageFile.FileName);
+                    if (FileExtension == ".xlsx" || FileExtension == ".xls" || FileExtension == ".pdf" || FileExtension == ".doc" || FileExtension == ".docx" || FileExtension == ".png" || FileExtension == ".jpg" || FileExtension == ".jpeg")
+                    {
+                        FileName = FileName.Trim() + FileExtension;
+                        string FileNameForsave = FileName;
+                        DataTable dt = BankRepository.GetfileCountdata(BankID, FileName);
+                        if (dt.Rows.Count > 0)
+                        {
+                            return Json(new { status = false, message = "File already exist in table", url = "" }, 0);
+                        }
+                        else
+                        {
+                            string UploadPath = Path.Combine(Server.MapPath("~/Content/BankLinkedFiles"));
+                            UploadPath = UploadPath + "\\";
+                            model.ImagePath = UploadPath + FileName;
+                            var ImagePath = "~/Content/BankLinkedFiles/" + FileName;
+                            ImageFile.SaveAs(model.ImagePath);
+                            int resultOne = BankRepository.FileUpload(BankID, FileName, ImagePath, FileExtension, size);
+                            if (resultOne > 0)
+                            {
+                                return Json(new { status = true, message = "File Upload successfully!!", url = "" }, 0);
+                            }
+                            else
+                            {
+                                return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return Json(new { status = false, message = "File Type " + FileExtension + " Not allowed", url = "" }, 0);
+                    }
+                }
+                else
+                {
+                    return Json(new { status = false, message = "Please attach a document", url = "" }, 0);
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { status = false, message = "Invalid Details", url = "" }, 0);
+            }
+
+        }
+        [HttpPost]
+        public JsonResult GetBankLinkedFiles(ThirdPartyModel model)
+        {
+            string result = string.Empty;
+            int TotalRecord = 0;
+            try
+            {
+                long id = model.rowid;
+                string urid = "";
+                if (model.user_status != "")
+                    urid = model.user_status;
+                string searchid = model.Search;
+                ViewBag.AttachedFiles = "0";
+                DataTable dt = BankRepository.GetBankLinkedFiles(id, urid, searchid, model.PageNo, model.PageSize, out TotalRecord, model.SortCol, model.SortDir);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(new { sEcho = model.sEcho, recordsTotal = TotalRecord, recordsFiltered = TotalRecord, iTotalRecords = TotalRecord, iTotalDisplayRecords = TotalRecord, aaData = result }, 0);
+        }
+
+        public JsonResult DeleteBankLinkedFiles(BankModel model)
+        {
+            if (model.rowid > 0)
+            {
+                int ID = BankRepository.DeleteBankLinkedFiles(model);
+                if (ID > 0)
+                    return Json(new { status = true, message = "Bank Linked Files has been deleted successfully!!", url = "", id = ID }, 0);
+                else
+                    return Json(new { status = false, message = "Invalid Details", url = "", id = 0 }, 0);
+            }
+            else
+            {
+                return Json(new { status = false, message = "Bank info not Found", url = "", id = 0 }, 0);
+            }
+        }
+
     }
 }

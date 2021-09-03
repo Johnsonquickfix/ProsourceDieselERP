561c07c935fef0202168fd1ff5ed41108e49dbff
david.quickfix1 <david.quickfix1@gmail.com>
2021-09-03 16:43:37 +0530

    changes controller

---
+++ LaylaERP/Controllers/HrmsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaylaERP.Controllers
+{
+    public class HrmsController : Controller
+    {
+        // GET: Employee
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        // GET: Attendance
+        public ActionResult Attendance()
+        {
+            return View();
+        }
+
+        // GET: Leave
+        public ActionResult Leave()
+        {
+            return View();
+        }
+
+        // GET: Payroll
+        public ActionResult Payroll()
+        {
+            return View();
+        }
+
+        // GET: Appraisal
+        public ActionResult Appraisal()
+        {
+            return View();
+        }
+
+        // GET: Hrms
+        public ActionResult RoleMembership()
+        {
+            return View();
+        }
+    }
+}

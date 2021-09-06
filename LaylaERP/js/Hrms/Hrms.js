aeb609187b01db8e87aaf1e5270e49997c6e3315
Tom Bell <tombell.quickfix@gmail.com>
2021-09-06 20:59:57 +0530

    Hrms

---
+++ LaylaERP/js/Hrms/Hrms.js
@@ -0,0 +1,16 @@
+﻿
+
+function getVendor() {
+    $.ajax({
+        url: "/CommercialProposal/GetVendor",
+        type: "Get",
+        success: function (data) {
+            var opt = '<option value="-1">Please Select Vendor</option>';
+            for (var i = 0; i < data.length; i++) {
+                opt += '<option value="' + data[i].Value + '">' + data[i].Text + '</option>';
+            }
+            $('#ddlVendor').html(opt);
+        }
+
+    });
+}

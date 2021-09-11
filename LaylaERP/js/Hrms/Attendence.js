d2b6ac91b8104bba9c3f4ab9f2ad5db564967614
Steven Methew <steven.quickfix@gmail.com>
2021-09-11 11:38:25 +0530

    Attendence

---
+++ LaylaERP/js/Hrms/Attendence.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+    EmployeeList();
+})
+
+$('#btnSearch').click(function () {
+    EmployeeList();
+})
+
+function EmployeeList() {
+    var urid = $("#ddlSearchStatus").val();
+    ID = $("#hfid").val();
+    var table_EL = $('#EmployeeListdata').DataTable({
+        columnDefs: [{ "orderable": true, "targets": 0 }, { 'visible': false, 'targets': [0] }], order: [[0, "desc"]],
+        destroy: true, bProcessing: true, bServerSide: true, bAutoWidth: false, searching: true,
+        responsive: true, lengthMenu: [[10, 20, 50], [10, 20, 50]],
+        language: {
+            lengthMenu: "_MENU_ per page",
+            zeroRecords: "Sorry no records found",
+            info: "Showing _START_ to _END_ of _TOTAL_ entries",
+            infoFiltered: "",
+            infoEmpty: "No records found",
+            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
+        },
+        initComplete: function () {
+            $('#EmployeeListdata_filter input').unbind();
+            $('#EmployeeListdata_filter input').bind('keyup', function (e) {
+                var code = e.keyCode || e.which;
+                if (code == 13) { table_EL.search(this.value).draw(); }
+            });
+        },
+        sAjaxSource: "/Hrms/GetEmployeeAttendenceList",
+        fnServerData: function (sSource, aoData, fnCallback, oSettings) {
+            aoData.push({ name: "strValue1", value: urid });
+            var col = 'id';
+            if (oSettings.aaSorting.length >= 0) {
+                var col = oSettings.aaSorting[0][0] == 0 ? "ID" : oSettings.aaSorting[0][0] == 1 ? "name" : oSettings.aaSorting[0][0] == 2 ? "email" : oSettings.aaSorting[0][0] == 3 ? "phone" : "id";
+                aoData.push({ name: "sSortColName", value: col });
+            }
+            oSettings.jqXHR = $.ajax({
+                dataType: 'json', type: "GET", url: sSource, data: aoData,
+                "success": function (data) {
+                    var dtOption = { sEcho: data.sEcho, recordsTotal: data.recordsTotal, recordsFiltered: data.recordsFiltered, aaData: JSON.parse(data.aaData) };
+                    return fnCallback(dtOption);
+                }
+            });
+        },
+        aoColumns: [
+            { data: 'ID', title: 'ID', sWidth: "20%", class: 'text-left' },
+            { data: 'name', title: 'Name', sWidth: "20%", class: 'text-left' },
+            { data: 'designation', title: 'Designation', sWidth: "20%" },
+           
+            {
+                'data': 'ID', sWidth: "10%", title: 'Action',
+                'render': function (id, type, full, meta) {
+                    return '<a href="../Hrms/Employee/' + id + '"><i class="glyphicon glyphicon-pencil"></i></a>';
+                }
+            }
+        ]
+    });
+}

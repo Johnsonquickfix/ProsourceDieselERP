3a83a7cfa47fe54d89d3ce7182deab178e4ee597
Calvin Christison <christison.quickfix@gmail.com>
2021-09-18 19:47:58 +0530

    All files for ordershippingrule

---
+++ LaylaERP/js/Setting/OrderShippingRule.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    $.get('/Product/GetVender/' + 1, function (data) {
+        var items = "";
+        // $('#ddlvender').empty();
+        // items += "<option value=''>Please select</option>";
+        $.each(data, function (index, value) {
+            items += $('<option>').val(this['Value']).text(this['Text']).appendTo("#ddlvender");
+        })
+        //$('#ddlvender').bind(items);
+    });
+    getProducts();
+    $(".select2").select2();
+})
+function getProducts() {
+    $.ajax({
+        url: "/Inventory/GetProductList",
+        type: "Get", beforeSend: function () { $("#loader").show(); },
+        success: function (data) {
+            let dt = JSON.parse(data);
+            //Product
+            $("#ddlProduct").html('<option value="0">Select Product</option>');
+            for (i = 0; i < dt['Table'].length; i++) { $("#ddlProduct").append('<option value="' + dt['Table'][i].id + '">' + dt['Table'][i].text + '</option>'); }
+        },
+        complete: function () { $("#loader").hide(); },
+        error: function (xhr, status, err) { $("#loader").hide(); }
+    });
+}

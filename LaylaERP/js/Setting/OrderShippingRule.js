af56b4c513187eb4c61401e3a9bde6af05d20f0a
Calvin Christison <christison.quickfix@gmail.com>
2021-09-21 17:15:27 +0530

    Country Add on shipping order rule

--- LaylaERP/js/Setting/OrderShippingRule.js
+++ LaylaERP/js/Setting/OrderShippingRule.js
@@ -12,17 +12,22 @@
 
     fillrole();
     getProducts();
-    $(".select2").select2();
-    $("#ddlState").select2({
-        allowClear: true, minimumInputLength: 2, placeholder: "Search State",
-        ajax: {
-            url: '/Product/GetStateData', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
-            data: function (params) { var obj = { strValue1: params.term, strValue2: 'US' }; return JSON.stringify(obj); },
-            processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.StateFullName, name: item.StateFullName, val: item.State, id: item.State } }) }; },
-            error: function (xhr, status, err) { }, cache: true
-        }
+    $("#ddlCountry").change(function () {
+        var obj = $("#ddlCountry").val();
+        BindStateCounty(obj);
+        SelectedStateCounty();
     });
-    SelectedStateCounty();
+    $(".select2").select2();
+    //$("#ddlState").select2({
+    //    allowClear: true, minimumInputLength: 2, placeholder: "Search State",
+    //    ajax: {
+    //        url: '/Product/GetStateData', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
+    //        data: function (params) { var obj = { strValue1: params.term, strValue2: 'US' }; return JSON.stringify(obj); },
+    //        processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.StateFullName, name: item.StateFullName, val: item.State, id: item.State } }) }; },
+    //        error: function (xhr, status, err) { }, cache: true
+    //    }
+    //});
+   // SelectedStateCounty();
     dataGridLoad();
 })
 
@@ -65,16 +70,16 @@ $('#btnSave').click(function () {
 
 $('#btndelete').click(function () {
     var fk_rule = $('#ddlRuledel').val();
-    //var country = $('#ddlCountrydel').val();
+    var country = $('#ddlCountrydel').val();
     if (fk_rule == "") {
         swal("alert", "Please select Rule", "error").then(function () { swal.close(); $('#ddlRuledel').focus(); })
     }
-    //else if (country == "") {
-    //    swal("alert", "Please select Country", "error").then(function () { swal.close(); $('#ddlCountrydel').focus(); })
-    //}
+    else if (country == "") {
+        swal("alert", "Please select Country", "error").then(function () { swal.close(); $('#ddlCountrydel').focus(); })
+    }
     else {
        // var obj = { fk_ShippingID: Shipping, countrycode: country }
-        var obj = { fk_rule: fk_rule }
+        var obj = { fk_rule: fk_rule, countryshipping: country }
         $.ajax({
             url: '/Setting/deleteShippingrule/', dataType: 'json', type: 'Post',
             contentType: "application/json; charset=utf-8",
@@ -105,6 +110,45 @@ $('#btndelete').click(function () {
     }
 });
 
+
+function BindStateCounty(obj) {
+
+    $("#ddlState").select2({
+        allowClear: true, minimumInputLength: 2, placeholder: "Search State",
+        ajax: {
+            url: '/Product/GetStateData', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
+            data: function (params) { var obj = { strValue1: params.term, strValue2: $("#ddlCountry").val() }; return JSON.stringify(obj); },
+            processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.StateFullName, name: item.StateFullName, val: item.State, id: item.State } }) }; },
+            error: function (xhr, status, err) { }, cache: true
+        }
+    });
+}
+function SelectedStateCounty() {
+    $("#ddlState").empty();
+    //var strValue2: $("#ddlCountry").val();
+    var obj = { strValue1: $("#ddlCountry").val() }
+    $.ajax({
+
+        url: '/Product/SelectedStateData/' + 1,
+
+        type: 'post',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(obj),
+        success: function (data) {
+            var datalog = JSON.parse(data);
+            for (var i = 0; i < datalog.length; i++) {
+                $("#ddlState").append('<option value="' + datalog[i].State + '" selected>' + datalog[i].StateFullName + '</option>');
+            }
+
+        },
+        error: function (msg) { alert(msg); },
+        async: false
+    });
+
+
+}
+
 function getProducts() {
     $.ajax({
         url: "/Inventory/GetProductList",
@@ -125,7 +169,7 @@ function fillrole() {
         var items = "";
         $('#ddlRule').empty();
         $('#ddlRuledel').empty();
-        optionText = 'Please select Roule';
+        optionText = 'Please Select Rule';
         optionValue = "";
         $('#ddlRule').append(new Option(optionText, optionValue));
         $('#ddlRuledel').append(new Option(optionText, optionValue));
@@ -151,31 +195,7 @@ function fillrole() {
     //    //$('#txtShippingClass').bind(items);
     //});
 }
-function SelectedStateCounty() {
-    $("#ddlState").empty();
-    //var strValue2: $("#ddlCountry").val();
-    var obj = { strValue1: 'US' }
-    $.ajax({
-
-        url: '/Product/SelectedStateData/' + 1,
-
-        type: 'post',
-        contentType: "application/json; charset=utf-8",
-        dataType: 'JSON',
-        data: JSON.stringify(obj),
-        success: function (data) {
-            var datalog = JSON.parse(data);
-            for (var i = 0; i < datalog.length; i++) {
-                $("#ddlState").append('<option value="' + datalog[i].State + '" selected>' + datalog[i].StateFullName + '</option>');
-            }
-
-        },
-        error: function (msg) { alert(msg); },
-        async: false
-    });
-
-
-}
+ 
 $(document).on('click', "#btncl", function () {
     //$('#dvdetails').hide();
     location.reload();
@@ -183,85 +203,7 @@ $(document).on('click', "#btncl", function () {
 $(document).on('click', "#btnsavedetails", function () {
     Adddetails();
 });
-function Adddetails() {
-    debugger
-    ID = $("#hfshipingid").val();
-    var statearray = $('#ddlState option:selected')
-        .toArray().map(item => item.value).join();
-   // console.log(statearray);
-    locationval = statearray; 
-    fk_rule = $("#ddlRule").val();
-    services = $("#ddlService").val();
-    fk_product = $("#ddlProduct").val();
-    fk_vendor = $("#ddlvender").val();
-  
-    //taxprise = $("#txttaxprice").val();
 
-    if (fk_rule == "") {
-        swal('Alert', 'Please Select Rule', 'error').then(function () { swal.close(); $('#ddlRule').focus(); });
-    }
-    else if (fk_product == "") {
-        swal('Alert', 'Please Select Product', 'error').then(function () { swal.close(); $('#ddlProduct').focus(); });
-    }
-    else if (location == "") {
-        swal('Alert', 'Please Select State', 'error').then(function () { swal.close(); $('#ddlState').focus(); });
-    }
-    else if (fk_vendor == "") {
-        swal('Alert', 'Please Select Vendor', 'error').then(function () { swal.close(); $('#ddlvender').focus(); });
-    }
-    else {
-        var obj = {
-            ID: ID,
-            fk_rule: fk_rule,
-            location: locationval,
-            services: services,
-            fk_product: fk_product,
-            fk_vendor: fk_vendor 
-            //Shipping_taxrate: taxprise
-        }
-    
-        $.ajax({
-            url: '/Setting/CreateShiprule/', dataType: 'json', type: 'Post',
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(obj),
-            dataType: "json",
-            headers: { "Content-Type": "application/json" },
-            beforeSend: function () {
-                $("#loader").show();
-            },
-            success: function (data) {
-                if (data.status == true) {
-                    if (data.url == "Manage") {
-                        dataGridLoad('');
-                        $('#ddlState').val(null).trigger('change');
-                       // ClearControl();
-                        swal('Alert!', data.message, 'success');
-                    }
-                    else {
-                        dataGridLoad('');
-                        $('#ddlState').val(null).trigger('change');
-                        //ClearControl();
-                        // $('#fetch_results > input:text').val('');
-                        swal('Alert!', data.message, 'success');
-                    }
-                    //$('#ddlProduct').val(null).trigger('change');
-                    //clear_fetch();
-
-                }
-                else {
-                    swal('Alert!', data.message, 'error')
-                }
-            },
-            complete: function () {
-                $("#loader").hide();
-            },
-            error: function (error) {
-                swal('Error!', 'something went wrong', 'error');
-            },
-            async: false
-        })
-    }
-}
 
 function dataGridLoad(order_type) {
 
@@ -293,7 +235,7 @@ function dataGridLoad(order_type) {
             aoData.push({ name: "strValue4", value: '' });
             var col = 'rowid';
             if (oSettings.aaSorting.length > 0) {
-                var col = oSettings.aaSorting[0][0] == 0 ? "rulrname" : oSettings.aaSorting[0][0] == 1 ? "State" : oSettings.aaSorting[0][0] == 2 ? "vendrname" : oSettings.aaSorting[0][0] == 3 ? "title" : oSettings.aaSorting[0][0] == 4 ? "services" : "rowid";
+                var col = oSettings.aaSorting[0][0] == 0 ? "rulrname" : oSettings.aaSorting[0][0] == 1 ? "Country" : oSettings.aaSorting[0][0] == 2 ? "State" : oSettings.aaSorting[0][0] == 3 ? "vendrname" : oSettings.aaSorting[0][0] == 4 ? "title" : oSettings.aaSorting[0][0] == 5 ? "services" : "rowid";
                 aoData.push({ name: "sSortColName", value: col });
             }
             //console.log(aoData);
@@ -313,7 +255,8 @@ function dataGridLoad(order_type) {
             //    }
             //},
 
-            { data: 'rulrname', title: 'Rule Name', sWidth: "12%" }, 
+            { data: 'rulrname', title: 'Rule Name', sWidth: "12%" },
+            { data: 'Country', title: 'Country', sWidth: "12%" },
             { data: 'State', title: 'State', sWidth: "12%" },
             { data: 'vendrname', title: 'Vendor', sWidth: "12%" },
             { data: 'title', title: 'Product', sWidth: "12%" },
@@ -351,6 +294,7 @@ function EditData(id) {
             var i = JSON.parse(data);
             $('#ddlRule').focus();
             $('#ddlRule').val(i[0].fk_rule).trigger('change');
+            $('#ddlCountry').val(i[0].country).trigger('change');
             $('#ddlProduct').val(i[0].fk_product).trigger('change');
             $('#ddlvender').val(i[0].fk_vendor).trigger('change');
             $('#ddlService').val(i[0].services).trigger('change');
@@ -363,4 +307,87 @@ function EditData(id) {
 }
 function statedata(id, name) {
     $("#ddlState").empty().append('<option value="' + id + '" selected>' + name + '</option>');
+}
+function ClearControl() {
+    $('#ddlCountry').val('').trigger('change');
+   // $("#txtPrice").val('0');
+}
+function Adddetails() {
+    debugger
+    ID = $("#hfshipingid").val();
+    var statearray = $('#ddlState option:selected')
+        .toArray().map(item => item.value).join();
+    // console.log(statearray);
+    locationval = statearray;
+    Countryval = $('#ddlCountry').val();
+    fk_rule = $("#ddlRule").val();
+    services = $("#ddlService").val();
+    fk_product = $("#ddlProduct").val();
+    fk_vendor = $("#ddlvender").val();
+
+    //taxprise = $("#txttaxprice").val();
+
+    if (fk_rule == "") {
+        swal('Alert', 'Please Select Rule', 'error').then(function () { swal.close(); $('#ddlRule').focus(); });
+    }
+    else if (Countryval == "") {
+        swal('Alert', 'Please Select Country', 'error').then(function () { swal.close(); $('#ddlCountry').focus(); });
+    }
+    else if (fk_product == "") {
+        swal('Alert', 'Please Select Product', 'error').then(function () { swal.close(); $('#ddlProduct').focus(); });
+    }
+    else if (location == "") {
+        swal('Alert', 'Please Select State', 'error').then(function () { swal.close(); $('#ddlState').focus(); });
+    }
+    else if (fk_vendor == "") {
+        swal('Alert', 'Please Select Vendor', 'error').then(function () { swal.close(); $('#ddlvender').focus(); });
+    }
+    else {
+        var obj = {
+            ID: ID,
+            fk_rule: fk_rule,
+            location: locationval,
+            countryshipping: Countryval,
+            services: services,
+            fk_product: fk_product,
+            fk_vendor: fk_vendor
+            //Shipping_taxrate: taxprise
+        }
+
+        $.ajax({
+            url: '/Setting/CreateShiprule/', dataType: 'json', type: 'Post',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(obj),
+            dataType: "json",
+            beforeSend: function () { $("#loader").show(); },
+            success: function (data) {
+                if (data.status == true) {
+                    if (data.url == "Manage") {
+                        dataGridLoad('');
+                        $('#ddlState').val(null).trigger('change');
+                        ClearControl();
+                        swal('Alert!', data.message, 'success');
+                    }
+                    else {
+                        dataGridLoad('');
+                        $('#ddlState').val(null).trigger('change');
+                        ClearControl();
+                        // $('#fetch_results > input:text').val('');
+                        swal('Alert!', data.message, 'success');
+                    }
+                    //$('#ddlProduct').val(null).trigger('change');
+                    //clear_fetch();
+
+                }
+                else {
+                    swal('Alert!', data.message, 'error')
+                }
+            },
+            complete: function () { $("#loader").hide(); },
+            error: function (error) {
+                swal('Error!', 'something went wrong', 'error');
+            },
+             
+        })
+    }
 }

6e998861188f4dd18fe927164f9f11fd0d5f872f
Tom Bell <tombell.quickfix@gmail.com>
2021-09-25 15:29:00 +0530

    DA

--- LaylaERP/Models/DaModel.cs
+++ LaylaERP/Models/DaModel.cs
@@ -7,6 +7,10 @@ namespace LaylaERP.Models
 {
     public class DaModel
     {
-
+        public int rowid { get; set; }
+        public decimal da_rate1 { get; set; }
+        public decimal da_rate2 { get; set; }
+        public decimal da_rate_others { get; set; }
+        public DateTime from_date { get; set; }
     }
 }

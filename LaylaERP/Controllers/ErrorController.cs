16199f2fa4b5104b2647a85a2186298ad01d5fca
david.quickfix1 <david.quickfix1@gmail.com>
2021-09-29 19:27:28 +0530

    change web config

--- LaylaERP/Controllers/ErrorController.cs
+++ LaylaERP/Controllers/ErrorController.cs
@@ -11,7 +11,7 @@ namespace LaylaERP.Controllers
         // GET: Error
         public ActionResult Index()
         {
-            return View();
+            return View("Error");
         }
     }
 }

62cf22058af0673ca71f1151e955c8d0e3a4a946
Johnsonquickfix <johnson.quickfix@gmail.com>
2021-09-30 15:10:24 +0530

    Coupon Product Search

--- LaylaERP/js/Marketing/Coupons.js
+++ LaylaERP/js/Marketing/Coupons.js
@@ -2,23 +2,23 @@
 
 $(document).ready(function () {
     $("#loader").hide();
-
+    bindProduct();
     var url = window.location.pathname;
     var id = url.substring(url.lastIndexOf('/') + 1);
 
     if (id != "" && id != "Index") {
-        $("#hfid").val(id);  
-    /*    setTimeout(function () { GetDataByID(id); }, 100);*/
-       // $("#hfprodcid").val("629,632");
+        $("#hfid").val(id);
+        /*    setTimeout(function () { GetDataByID(id); }, 100);*/
+        // $("#hfprodcid").val("629,632");
         //setTimeout(function () { GetProdctByID($("#hfprodcid").val()); }, 2000);
         //setTimeout(function () { GetExProdctByID($("#hfexprodcid").val()); }, 2020);
         //setTimeout(function () { GetCategoryProdctByID($("#hfcategid").val()); }, 2040);
         //setTimeout(function () { GetExCategoryProdctByID($("#exhfcategid").val()); }, 2050);
-         GetDataByID(id); 
-       GetProdctByID($("#hfprodcid").val()); 
-      GetExProdctByID($("#hfexprodcid").val()); 
-      GetCategoryProdctByID($("#hfcategid").val()); 
-       GetExCategoryProdctByID($("#exhfcategid").val()); 
+        GetDataByID(id);
+        //GetProdctByID($("#hfprodcid").val());
+        //GetExProdctByID($("#hfexprodcid").val());
+        GetCategoryProdctByID($("#hfcategid").val());
+        GetExCategoryProdctByID($("#exhfcategid").val());
     }
 
     $("#txtCouponAmount").keyup(function () {
@@ -37,33 +37,33 @@ $(document).ready(function () {
         $this.val($this.val().substring(0, 10));
     });
 
-$('#btnGenerateCoupon').click(function () {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for (var i = 0; i < 8; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    $('#txtGenrateCouponCode').val(text);
+    $('#btnGenerateCoupon').click(function () {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        for (var i = 0; i < 8; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        $('#txtGenrateCouponCode').val(text);
 
-});
+    });
 
-    $('#ddlProduct').select2({
-        allowClear: true, minimumInputLength: 3, placeholder: "Search Product",
-        ajax: {
-            url: '/Coupons/GetProductList', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
-            data: function (params) { var obj = { strValue1: params.term }; return JSON.stringify(obj); },
-            processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.post_title, name: item.post_title, id: item.id} }) }; },
-            error: function (xhr, status, err) { }, cache: true
-        }
-    });
-    $('#ddlProductExlude').select2({
-        allowClear: true, minimumInputLength: 3, placeholder: "Search Product",
-        ajax: {
-            url: '/Coupons/GetProductList', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
-            data: function (params) { var obj = { strValue1: params.term }; return JSON.stringify(obj); },
-            processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.post_title, name: item.post_title, id: item.id } }) }; },
-            error: function (xhr, status, err) { }, cache: true
-        }
-    });
+    //$('#ddlProduct').select2({
+    //    allowClear: true, minimumInputLength: 3, placeholder: "Search Product",
+    //    ajax: {
+    //        url: '/Coupons/GetProductList', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
+    //        data: function (params) { var obj = { strValue1: params.term }; return JSON.stringify(obj); },
+    //        processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.post_title, name: item.post_title, id: item.id } }) }; },
+    //        error: function (xhr, status, err) { }, cache: true
+    //    }
+    //});
+    //$('#ddlProductExlude').select2({
+    //    allowClear: true, minimumInputLength: 3, placeholder: "Search Product",
+    //    ajax: {
+    //        url: '/Coupons/GetProductList', type: "POST", contentType: "application/json; charset=utf-8", dataType: 'json', delay: 250,
+    //        data: function (params) { var obj = { strValue1: params.term }; return JSON.stringify(obj); },
+    //        processResults: function (data) { var jobj = JSON.parse(data); return { results: $.map(jobj, function (item) { return { text: item.post_title, name: item.post_title, id: item.id } }) }; },
+    //        error: function (xhr, status, err) { }, cache: true
+    //    }
+    //});
     $('#ddlProductCategories').select2({
         allowClear: true, minimumInputLength: 3, placeholder: "Search Categories",
         ajax: {
@@ -83,7 +83,7 @@ $('#btnGenerateCoupon').click(function () {
         }
     });
     $('#txtCouponAmount').change(function () {
-       var DiscountType = $("#ddlDiscountType").val();
+        var DiscountType = $("#ddlDiscountType").val();
         var CouponAmount = $("#txtCouponAmount").val();
         if (DiscountType == "percent") {
             if (parseInt(CouponAmount) > 100) {
@@ -96,12 +96,21 @@ $('#btnGenerateCoupon').click(function () {
         AddCoupons();
     })
 });
+function bindProduct() {
+    $.ajax({ method: 'post', url: '/Coupons/GetProductList', data: JSON.stringify({ strValue1: '' }), async: false }).done(function (result, textStatus, jqXHR) {
+        result = JSON.parse(result);
+
+        $("#ddlProduct,#ddlProductExlude").select2({
+            data: result, multiple: true, allowClear: true, minimumInputLength: 3, placeholder: { id: '-1', text: 'Select Product' }
+        });
+    }).fail(function (jqXHR, textStatus, errorThrown) { console.log(jqXHR); });
+}
 
 function AddCoupons() {
     debugger
     ID = $("#hfid").val();
     var freeship = "";
-    var Individualval  = "";
+    var Individualval = "";
     var Exsaleitems = "";
     var AutoCouponval = "";
 
@@ -114,11 +123,11 @@ function AddCoupons() {
     var ExcludeCategoriesval = $('#ddlExcludeCategories option:selected')
         .toArray().map(item => item.value).join();
 
-     if ($("#free_ship").prop('checked') == true)
-         freeship = "yes";
-     else
+    if ($("#free_ship").prop('checked') == true)
+        freeship = "yes";
+    else
         freeship = "no";
-   
+
     if ($("#individual_only").prop('checked') == true)
         Individualval = "yes";
     else
@@ -137,7 +146,7 @@ function AddCoupons() {
     couponcode = $("#txtGenrateCouponCode").val();
     DiscountType = $("#ddlDiscountType").val();
     CouponAmount = $("#txtCouponAmount").val();
-    free_ship = freeship; 
+    free_ship = freeship;
     CouponExpiryDate = $("#txtCouponExpiryDate").val();
     MinimumSpend = $("#txtMinimumSpend").val();
     MaximumSpend = $("#txtMaximumSpend").val();
@@ -154,12 +163,12 @@ function AddCoupons() {
     UsageLimitPerUser = $("#txtUsageLimitPerUser").val();
     discription = $("#txtdiscription").val();
 
-     
+
     if (CouponAmount == "")
         CouponAmount = "0.00";
     else
         CouponAmount = addZeroes(CouponAmount);
-   
+
 
     if (MinimumSpend == "")
         MinimumSpend = "0";
@@ -168,80 +177,80 @@ function AddCoupons() {
     if (discription == "")
         discription = "-";
 
-   
+
     if (couponcode == "") {
         swal('Alert', 'Please Enter Coupon Code', 'error').then(function () { swal.close(); $('#txtGenrateCouponCode').focus(); });
     }
     else if (CouponExpiryDate == "") {
         swal('Alert', 'Please Enter Coupon Expiry Date', 'error').then(function () { swal.close(); $('#txtCouponExpiryDate').focus(); });
     }
-       
+
 
     else {
 
-        
-        
+
+
         var obj = {
-                ID: ID,
-                post_title: couponcode,
-                post_name: couponcode,
-                post_excerpt: discription,
-                discount_type: DiscountType,
-                coupon_amount: CouponAmount,
-                free_shipping: free_ship,
-                date_expires: CouponExpiryDate,
-                min_subtotal: MinimumSpend,
-                max_subtotal: MaximumSpend,
-                individual_use: individual,
-                exclude_sale_items: excludeitems,
-                wjecf_is_auto_coupon: autocoupon, 
-                product_ids: Productid,
-                exclude_product_ids: Productexludeid,
-                categories_ids: categories,
-                exclude_categories_ids: Excludecategories,
-                cus_email: email,
-                usage_limit: UsageLimitPerCoupon,
-                limit_usage_to_x_items: LimitUsagetoXItems,
-                usage_limit_per_user: UsageLimitPerUser
-            }
-            $.ajax({
-                url: '/Coupons/CreateCoupons/', dataType: 'json', type: 'Post',
-                contentType: "application/json; charset=utf-8",
-                data: JSON.stringify(obj),
-                dataType: "json",
-                beforeSend: function () {
-                    $("#loader").show();
-                },
-                success: function (data) {
-                    if (data.status == true) {
-                        if (data.url == "Manage") {
-                            swal('Alert!', data.message, 'success').then((result) => { location.href = '../ManageCoupons'; });
-                        }
-                        else {
-                            $('#fetch_results > input:text').val('');
-                            swal('Alert!', data.message, 'success').then((result) => { location.href = 'ManageCoupons'; });
-                        }
-                       //$('#ddlProduct').val(null).trigger('change');
-                       //clear_fetch();
-                       
+            ID: ID,
+            post_title: couponcode,
+            post_name: couponcode,
+            post_excerpt: discription,
+            discount_type: DiscountType,
+            coupon_amount: CouponAmount,
+            free_shipping: free_ship,
+            date_expires: CouponExpiryDate,
+            min_subtotal: MinimumSpend,
+            max_subtotal: MaximumSpend,
+            individual_use: individual,
+            exclude_sale_items: excludeitems,
+            wjecf_is_auto_coupon: autocoupon,
+            product_ids: Productid,
+            exclude_product_ids: Productexludeid,
+            categories_ids: categories,
+            exclude_categories_ids: Excludecategories,
+            cus_email: email,
+            usage_limit: UsageLimitPerCoupon,
+            limit_usage_to_x_items: LimitUsagetoXItems,
+            usage_limit_per_user: UsageLimitPerUser
+        }
+        $.ajax({
+            url: '/Coupons/CreateCoupons/', dataType: 'json', type: 'Post',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(obj),
+            dataType: "json",
+            beforeSend: function () {
+                $("#loader").show();
+            },
+            success: function (data) {
+                if (data.status == true) {
+                    if (data.url == "Manage") {
+                        swal('Alert!', data.message, 'success').then((result) => { location.href = '../ManageCoupons'; });
                     }
                     else {
-                        swal('Alert!', data.message, 'error')
+                        $('#fetch_results > input:text').val('');
+                        swal('Alert!', data.message, 'success').then((result) => { location.href = 'ManageCoupons'; });
                     }
-                },
-                complete: function () {
-                    $("#loader").hide();
-                    //location.href = '/Users/Users/';
-                    //window.location.href = '/Users/Users/';
+                    //$('#ddlProduct').val(null).trigger('change');
+                    //clear_fetch();
 
-                },
-                error: function (error) {
-                    swal('Error!', 'something went wrong', 'error');
-                },
-            })
-        }
-        
-     
+                }
+                else {
+                    swal('Alert!', data.message, 'error')
+                }
+            },
+            complete: function () {
+                $("#loader").hide();
+                //location.href = '/Users/Users/';
+                //window.location.href = '/Users/Users/';
+
+            },
+            error: function (error) {
+                swal('Error!', 'something went wrong', 'error');
+            },
+        })
+    }
+
+
 
 }
 
@@ -250,27 +259,16 @@ function addZeroes(num) {
     // Cast as number
     var num = Number(num);
     // If not a number, return 0
-    if (isNaN(num)) {
-        return 0;
-    }
+    if (isNaN(num)) { return 0; }
     // If there is no decimal, or the decimal is less than 2 digits, toFixed
-    if (String(num).split(".").length < 2 || String(num).split(".")[1].length <= 2) {
-        num = num.toFixed(2);
-    }
+    if (String(num).split(".").length < 2 || String(num).split(".")[1].length <= 2) { num = num.toFixed(2); }
     // Return the number
     return num;
 }
 function clear_fetch() {
-
-    $('#fetch_results').find('input:text, input:password,input:number, input:checkbox select')
-        .each(function () {           
-            $(this).val('');
-        });
-    $('input[type=checkbox]').each(function () {
-        this.checked = false;
-    });
+    $('#fetch_results').find('input:text, input:password,input:number, input:checkbox select').each(function () { $(this).val(''); });
+    $('input[type=checkbox]').each(function () { this.checked = false; });
 }
-
 
 function GetDataByID(order_id) {
     var ID = order_id;
@@ -291,22 +289,22 @@ function GetDataByID(order_id) {
             $('#ddlDiscountType').val(i[0].discount_type.trim()).trigger('change');
             $("#txtCouponAmount").val(i[0].coupon_amount);
             if (i[0].free_shipping == "yes")
-            $("#free_ship").prop("checked", true);
+                $("#free_ship").prop("checked", true);
             $("#txtCouponExpiryDate").val(i[0].date_expires);
             $("#txtMinimumSpend").val(i[0].minimum_amount);
-            $("#txtMaximumSpend").val(i[0].maximum_amount);          
+            $("#txtMaximumSpend").val(i[0].maximum_amount);
             if (i[0].individual_use == "yes")
-                $("#individual_only").prop("checked", true); 
+                $("#individual_only").prop("checked", true);
             if (i[0].exclude_sale_items == "yes")
-                 $("#exclude_items").prop("checked", true);
+                $("#exclude_items").prop("checked", true);
             if (i[0]._wjecf_is_auto_coupon == "yes")
                 $("#auto_coupon").prop("checked", true);
-         
-       
-           // $("#ddlProduct").empty().append('<option value="' + i[0].product_ids + '" selected>' + i[0].product_ids + '</option>');
-           // $("#ddlProductExlude").empty().append('<option value="' + i[0].exclude_product_ids + '" selected>' + i[0].exclude_product_ids + '</option>');
-          //  $("#ddlProductCategories").empty().append('<option value="' + i[0].product_categories + '" selected>' + i[0].product_categories + '</option>');
-          //  $("#ddlExcludeCategories").empty().append('<option value="' + i[0].exclude_product_categories + '" selected>' + i[0].exclude_product_categories + '</option>');
+
+
+            // $("#ddlProduct").empty().append('<option value="' + i[0].product_ids + '" selected>' + i[0].product_ids + '</option>');
+            // $("#ddlProductExlude").empty().append('<option value="' + i[0].exclude_product_ids + '" selected>' + i[0].exclude_product_ids + '</option>');
+            //  $("#ddlProductCategories").empty().append('<option value="' + i[0].product_categories + '" selected>' + i[0].product_categories + '</option>');
+            //  $("#ddlExcludeCategories").empty().append('<option value="' + i[0].exclude_product_categories + '" selected>' + i[0].exclude_product_categories + '</option>');
             $("#txtAllowedEmails").val(i[0].customer_email);
             $("#txtUsageLimitPerCoupon").val(i[0].usage_limit);
             $("#txtLimitUsagetoXItems").val(i[0].limit_usage_to_x_items);
@@ -315,7 +313,10 @@ function GetDataByID(order_id) {
             $("#hfexprodcid").val(i[0].exclude_product_ids);
             $("#hfcategid").val(i[0].product_categories);
             $("#exhfcategid").val(i[0].exclude_product_categories);
-            
+
+            $("#ddlProduct").select2("val", [i[0].product_ids.split(',')]);
+            $("#ddlProductExlude").select2("val", [i[0].exclude_product_ids.split(',')]);
+
             // $("#shipcountry").val(i[0].country);
             //  $('#Item_Name').val(i[0].item_name.trim()).trigger('change');
 
@@ -435,5 +436,3 @@ function GetExCategoryProdctByID(ProdctID) {
     }
 
 }
-
- 

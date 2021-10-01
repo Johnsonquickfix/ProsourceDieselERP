9cb482c032e845ac86100ba75cd44289405136cb
david.quickfix1 <david.quickfix1@gmail.com>
2021-10-01 18:49:54 +0530

    error changes.

--- LaylaERP/Controllers/ErrorController.cs
+++ LaylaERP/Controllers/ErrorController.cs
@@ -9,9 +9,15 @@ namespace LaylaERP.Controllers
     public class ErrorController : Controller
     {
         // GET: Error
-        public ActionResult Index()
+        public ViewResult Index()
         {
             return View("Error");
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View("NotFound");
         }
     }
 }

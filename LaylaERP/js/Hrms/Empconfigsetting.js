05dfeb3237f7e9897824a62796e2f010ceed747f
Tom Bell <tombell.quickfix@gmail.com>
2021-10-04 15:33:35 +0530

    Configuration

--- LaylaERP/js/Hrms/Empconfigsetting.js
+++ LaylaERP/js/Hrms/Empconfigsetting.js
@@ -19,6 +19,7 @@
     festivaladvance = $("#chkfestivaladvance").prop("checked") ? 1 : 0;
     hrapercent = $("#chkhrapercent").prop("checked") ? 1 : 0;
     hraslab = $("#chkhraslab").prop("checked") ? 1 : 0;
+    hrapercentvalue = $("#txthrapercentvalue").val();
 
     if (id == "") {
         swal('Alert', 'Please Enter id', 'error').then(function () { swal.close(); $('#ddlproduct').focus(); });
@@ -49,7 +50,7 @@
             festival_adv: festivaladvance,
             hra_percent: hrapercent,
             hra_slab: hraslab,
-
+            hra_percent_value: hrapercentvalue,
         }
         $.ajax({
             url: '/Hrms/UpdateConfigSetting/', dataType: 'json', type: 'Post',
@@ -61,6 +62,59 @@
                 if (data.status == true) {
                     swal('Alert!', data.message, 'success');
 
+                }
+                else {
+                    swal('Alert!', data.message, 'error');
+                }
+            },
+            complete: function () { $("#loader").hide(); },
+            error: function (error) { swal('Error!', 'something went wrong', 'error'); },
+        })
+
+    }
+
+}
+
+function AddDA() {
+    darate1 = $("#txtdarate1").val();
+    darate2 = $("#txtdarate2").val();
+    darateothers = $("#txtdarateothers").val();
+    fromdate = $("#txtfromdate").val();
+
+    var formattedDate = new Date(fromdate);
+    var d = formattedDate.getDate();
+    var m = ("0" + (formattedDate.getMonth() + 1)).slice(-2)
+    var y = formattedDate.getFullYear();
+    var date_created = y + "-" + m + "-" + d;
+
+    if (darate1 == "") {
+        swal('Alert', 'Please Enter DA office', 'error').then(function () { swal.close(); $('#txtdarate1').focus(); });
+    }
+    else if (darate2 == "") {
+        swal('Alert', 'Please Enter DA Field', 'error').then(function () { swal.close(); $('#txtdarate2').focus(); });
+    }
+    else if (darateothers == "") {
+        swal('Alert', 'Please Enter DA others', 'error').then(function () { swal.close(); $('#txtdarateothers').focus(); });
+    }
+    else if (fromdate == "") {
+        swal('Alert', 'Please Enter Date', 'error').then(function () { swal.close(); $('#txtfromdate').focus(); });
+    }
+    else {
+        var obj = {
+            da_rate1: darate1,
+            da_rate2: darate2,
+            da_rate_others: darateothers,
+            from_date: date_created,
+        }
+        $.ajax({
+            url: '/Hrms/AddDaDetails', dataType: 'json', type: 'Post',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(obj),
+            dataType: "json",
+            beforeSend: function () { $("#loader").show(); },
+            success: function (data) {
+                if (data.status == true) {
+                    swal('Alert!', data.message, 'success');
                 }
                 else {
                     swal('Alert!', data.message, 'error');

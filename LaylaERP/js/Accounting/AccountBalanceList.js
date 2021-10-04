b1f16e13f28cae1b9f9b49a9056394f0014d6d88
Tom Bell <tombell.quickfix@gmail.com>
2021-10-04 20:41:10 +0530

    Account Balance

---
+++ LaylaERP/js/Accounting/AccountBalanceList.js
@@ -0,0 +1,47 @@
+﻿function AccountBalanceList() {
+    var urid = $("#ddlSearchStatus").val();
+    var ID = $("#hfid").val();
+    var table_EL = $('#EmployeeListdata').DataTable({
+        columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': false, 'targets': [0] }], order: [[0, "desc"]],
+        destroy: true, bProcessing: true, bServerSide: true, bAutoWidth: false, searching: true,
+        responsive: true, lengthMenu: [[10, 20, 50], [10, 20, 50]],
+        language: {
+            lengthMenu: "_MENU_ per page",
+            zeroRecords: "Sorry no records found",
+            info: "Showing _START_ to _END_ of _TOTAL_ entries",
+            infoFiltered: "",
+            infoEmpty: "No records found",
+            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
+        },
+        initComplete: function () {
+            $('#EmployeeListdata_filter input').unbind();
+            $('#EmployeeListdata_filter input').bind('keyup', function (e) {
+                var code = e.keyCode || e.which;
+                if (code == 13) { table_EL.search(this.value).draw(); }
+            });
+        },
+        sAjaxSource: "/Accounting/AccountBalanceList",
+        fnServerData: function (sSource, aoData, fnCallback, oSettings) {
+            aoData.push({ name: "strValue1", value: urid });
+            var col = 'id';
+            if (oSettings.aaSorting.length >= 0) {
+                var col = oSettings.aaSorting[0][0] == 0 ? "id" : oSettings.aaSorting[0][0] == 1 ? "account" : oSettings.aaSorting[0][0] == 2 ? "debit" : "id";
+                aoData.push({ name: "sSortColName", value: col });
+            }
+            oSettings.jqXHR = $.ajax({
+                dataType: 'json', type: "GET", url: sSource, data: aoData,
+                "success": function (data) {
+                    var dtOption = { sEcho: data.sEcho, recordsTotal: data.recordsTotal, recordsFiltered: data.recordsFiltered, aaData: JSON.parse(data.aaData) };
+                    return fnCallback(dtOption);
+                }
+            });
+        },
+        aoColumns: [
+            { data: 'id', title: 'ID', sWidth: "5%" },
+            { data: 'account', title: 'Accounting Account', sWidth: "5%" },
+            { data: 'debit', title: 'Debit', sWidth: "10%" },
+            { data: 'credit', title: 'Credit', sWidth: "10%" },
+            { data: 'balance', title: 'Balance', sWidth: "10%" },
+        ],
+    });
+}

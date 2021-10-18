e1b4e9877447274c72eae3f8b1c1b8ca7af254e3
Calvin Christison <christison.quickfix@gmail.com>
2021-10-18 12:43:02 +0530

    Print update

--- LaylaERP/js/Reception/ReceivePrint.js
+++ LaylaERP/js/Reception/ReceivePrint.js
@@ -71,7 +71,7 @@ function getPurchaseOrderPrint1(id, is_mail) {
 }
 
 function printinvoice_Receive(id, result, is_mail, is_inv) {
-    let data = JSON.parse(result.data); console.log(data);
+    let data = JSON.parse(result.data); console.log(data['popd']);
     let inv_title = is_inv ? 'Invoice' : 'Purchase Order';
 
     let total_qty = 0, total_gm = 0.00, total_tax = 0.00, total_shamt = 0.00, total_discamt = 0.00, total_other = 0.00, paid_amt = 0.00; total_net = 0.00;
@@ -90,7 +90,7 @@ function printinvoice_Receive(id, result, is_mail, is_inv) {
     myHtml += '                                <img src="http://40.114.51.80/Images/layla1-logo.png" alt="" width="95" height="41" class="logo-size"/>';
     myHtml += '                                <p style="margin:15px 0px;font-family:sans-serif; font-size:15px; color:#4f4f4f;line-height:1.4;">';
     myHtml += '                                    ' + result.add + ', <br>' + result.city + ', ' + result.state + ' ' + result.zip + ', <br>' + (result.country == "CA" ? "Canada" : result.country == "US" ? "United States" : result.country) + '.<br>';
-    myHtml += '                                    Phone: 001 ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + result.email + '<br />' + result.website;
+    myHtml += '                                    Phone: ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + result.email + '<br />' + result.website;
     myHtml += '                                </p>';
     myHtml += '                            </td>';
     myHtml += '                            <td style="padding:0; vertical-align: top;" align="right">';
@@ -198,6 +198,7 @@ function printinvoice_Receive(id, result, is_mail, is_inv) {
     myHtml += '                                </tr>';
     myHtml += '                            </thead>';
     myHtml += '                            <tbody style="border:1px solid #ddd;">';
+  //  console.log(data['popd'][0]);
     $(data['popd']).each(function (index, trpd) {
         myHtml += '<tr style="border-bottom: 1px solid #ddd;">';
         myHtml += '    <td style="width:20%;padding:2px 5px;">' + trpd.datec + '</td>';
@@ -301,7 +302,7 @@ function printinvoice_Re(id, result, is_mail, is_inv) {
     myHtml += '                    <div id="printPreview" style="width: 41.66666667%;">';
     myHtml += '                        <div class="businessLogo"><img src="http://40.114.51.80/Images/layla1-logo.png" alt="" id="logoUrl" style="width: 90px;" /></div>';
     myHtml += '                        <div class="businfobox">' + result.add + '<br>' + result.city + ', ' + result.state + ' ' + result.zip + ', <br>' + (result.country == "CA" ? "Canada" : result.country == "US" ? "United States" : result.country) + '</div>';
-    myHtml += '                        <div class="businfobox">Phone: 001 ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br/>' + result.email + '<br />' + result.website + '<br /></div>';
+    myHtml += '                        <div class="businfobox">Phone: ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br/>' + result.email + '<br />' + result.website + '<br /></div>';
     myHtml += '                    </div>';
     myHtml += '                    <div style="width:58.33333333%; float:right;">';
     myHtml += '                        <table style="width: 100%;">';
@@ -655,7 +656,15 @@ function getInvoicePrintnew(id) {
 }
 
 function printinvoice_Bill(id, result, is_mail, is_inv) {
-    let data = JSON.parse(result.data); console.log(result);
+    let data = JSON.parse(result.data);
+    let pono = "";
+    try {
+         pono = data['podvadd'][0].pono;
+    }
+    catch (e) {
+        pono = '';
+    }
+    console.log(pono);
     let inv_title = is_inv ? 'Bill' : 'Receive Order';
     let inv_titleNew = 'Commercial Invoice';
     let total_qty = 0, total_gm = 0.00, total_tax = 0.00, total_shamt = 0.00, total_discamt = 0.00, total_other = 0.00, paid_amt = 0.00; total_net = 0.00;
@@ -692,7 +701,7 @@ function printinvoice_Bill(id, result, is_mail, is_inv) {
     myHtml += '                                        <td style="font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4; padding:0px 2.5px;">' + inv_title + ' date:</td><td style=" padding:0px 2.5px;font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4;">' + data['po'][0].date_creation + '</td>';
     myHtml += '                                    </tr>';
     myHtml += '                                    <tr>';
-    myHtml += '                                        <td style="font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4; padding:0px 2.5px;">Client PO#:</td><td style=" padding:0px 2.5px;font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4;">' + data['podvadd'][0].pono + '</td>';
+    myHtml += '                                        <td style="font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4; padding:0px 2.5px;">Client PO#:</td><td style=" padding:0px 2.5px;font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4;">' + pono + '</td>';
     myHtml += '                                    </tr>';
     myHtml += '                                    <tr>';
     myHtml += '                                        <td style="font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4; padding:0px 2.5px;">Reference:</td><td style=" padding:0px 2.5px;font-family: sans-serif;font-size: 15px;color: #4f4f4f;line-height: 1.4;">' + data['po'][0].ref_supplier + '</td>';
@@ -719,7 +728,7 @@ function printinvoice_Bill(id, result, is_mail, is_inv) {
    // myHtml += '            <h3 class="billto" style="font-family: sans-serif;font-size:20px;margin:5px 0px 5px 0px;;color:#2c2e2f;font-weight:200;">' '</h3>';
     myHtml += '                                <p style="margin:15px 0px;font-family:sans-serif; font-size:15px; color:#4f4f4f;line-height:1.4;">';
     myHtml += '                                    ' + result.name +', <br>' + result.add + ', <br>' + result.city + ', ' + result.state + ' ' + result.zip + ', <br>' + (result.country == "CA" ? "Canada" : result.country == "CN" ? "China" : result.country == "US" ? "United States" : result.country) + '.<br>';
-    myHtml += '                                    Phone: 001 ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + result.email + '<br />' + result.website;
+    myHtml += '                                    Phone: ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + result.email + '<br />' + result.website;
     myHtml += '                                </p>';
     myHtml += '        </td>';
 
@@ -731,13 +740,18 @@ function printinvoice_Bill(id, result, is_mail, is_inv) {
     //myHtml += '            <h3 class="billto" style="font-family: sans-serif;font-size:20px;margin:0px 0px 5px 0px;;color:#2c2e2f;font-weight:200;">' + data['podvadd'][0].ref + '</h3> ';
     myHtml += '                                        </td>';
     myHtml += '                                    </tr>';
+    if (pono == '') {
 
-    myHtml += '                                    <tr>';
-    myHtml += '                               <td <p style="margin:15px 0px;font-family:sans-serif; font-size:15px; color:#4f4f4f;line-height:1.4;">';
-    myHtml += '                                    ' + data['podvadd'][0].ref + ',<br>' + data['podvadd'][0].address + ',<br>' + data['podvadd'][0].address1 + data['podvadd'][0].City + ' ' + data['podvadd'][0].state + ', ' + data['podvadd'][0].zip + '<br>' + (data['podvadd'][0].Country == "CA" ? "Canada" : data['podvadd'][0].fk_country == "CN" ? "China"  : data['podvadd'][0].Country == "US" ? "United States" : data['podvadd'][0].Country) + '.<br>';
-    myHtml += '                                    Phone: 001 ' + data['podvadd'][0].phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + data['podvadd'][0].email + '<br />' ;
-    myHtml += '                                </p> </td>';
-    myHtml += '                                    </tr>';
+    }
+    else
+    {
+        myHtml += '                                    <tr>';
+        myHtml += '                               <td <p style="margin:15px 0px;font-family:sans-serif; font-size:15px; color:#4f4f4f;line-height:1.4;">';
+        myHtml += '                                    ' + data['podvadd'][0].ref + ',<br>' + data['podvadd'][0].address + ',<br>' + data['podvadd'][0].address1 + data['podvadd'][0].City + ' ' + data['podvadd'][0].state + ', ' + data['podvadd'][0].zip + '<br>' + (data['podvadd'][0].Country == "CA" ? "Canada" : data['podvadd'][0].fk_country == "CN" ? "China" : data['podvadd'][0].Country == "US" ? "United States" : data['podvadd'][0].Country) + '.<br>';
+        myHtml += '                                    Phone: ' + data['podvadd'][0].phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br />' + data['podvadd'][0].email + '<br />';
+        myHtml += '                                </p> </td>';
+        myHtml += '                                    </tr>';
+    }
     myHtml += '                                </table>';
     myHtml += '        </td>';
     myHtml += '     </tr>';
@@ -946,7 +960,7 @@ function printinvoice(id, result, is_mail, is_inv) {
 
     myHtml += '                        <div><img src="http://40.114.51.80/Images/layla1-logo.png" alt="" id="logoUrl" style="width: 60px;" /></div>';
     myHtml += '                        <div>' + result.add + '<br>' + result.city + ', ' + result.state + ' ' + result.zip + ', <br>' + (result.country == "CA" ? "Canada" : result.country == "US" ? "United States" : result.country) + '</div>';
-    myHtml += '                        <div>Phone: 001 ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br/>' + result.email + '<br />' + result.website + '<br /></div>';
+    myHtml += '                        <div>Phone: ' + result.phone.toString().replace(/(\d\d\d)(\d\d\d)(\d\d\d\d)/, "($1) $2-$3") + '<br/>' + result.email + '<br />' + result.website + '<br /></div>';
 
     myHtml += '                </div>';
     myHtml += '            </div>';

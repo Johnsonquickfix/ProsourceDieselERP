8dbbbd96044854cb5db026bb5a29175452aa72ff
Johnsonquickfix <johnson.quickfix@gmail.com>
2021-11-02 16:23:39 +0530

    Order Return Update

--- LaylaERP_v1/js/Orders/OrderRefund.js
+++ LaylaERP_v1/js/Orders/OrderRefund.js
@@ -85,6 +85,7 @@ function getOrderItemList(oid) {
     var option = { strValue1: oid };
     //let coupon_list = [];
     ajaxFunc('/Orders/GetOrderProductList', option, beforeSendFun, function (data) {
+        console.log(data);
         let itemHtml = '', recyclingfeeHtml = '', feeHtml = '', shippingHtml = '', giftCardHtml = '', refundHtml = '', couponHtml = '';
         let zQty = 0.00, zGAmt = 0.00, zTDiscount = 0.00, zTotalTax = 0.00, zShippingAmt = 0.00, zGiftCardAmt = 0.00, zStateRecyclingAmt = 0.00, zFeeAmt = 0.00, zRefundAmt = 0.00;
         for (var i = 0; i < data.length; i++) {
@@ -179,14 +180,14 @@ function getOrderItemList(oid) {
                 $("#shippingTotal").data("orderitemid", orderitemid);
                 $('#order_shipping_line_items').append(shippingHtml);
             }
-            else if (data[i].product_type == 'gift_card') {
+            else if (data[i].product_type == 'gift_card') {                
                 giftCardHtml = '<tr id="tritemId_' + orderitemid + '" data-orderitemid="' + orderitemid + '" data-pname="' + data[i].product_name + '">';
                 giftCardHtml += '<td class="text-center item-action"><i class="fa fa-gift"></i></td>';
                 giftCardHtml += '<td>' + data[i].product_name + '</td><td></td><td class="text-right row-refuntamt"></td><td class="RefundAmount text-right"></td><td class="TotalAmount text-right">' + data[i].total.toFixed(2) + '</td><td></td><td></td>';
                 giftCardHtml += '</tr>';
                 zGiftCardAmt = zGiftCardAmt + (parseFloat(data[i].total) || 0.00);
                 $("#giftCardTotal").data("orderitemid", orderitemid);
-                $('#gift_card_line_items').append(giftCardHtml);
+                $('#gift_card_line_items').append(giftCardHtml); console.log(giftCardHtml);
             }
             else if (data[i].product_type == 'refund') {
                 refundHtml = '<tr id="tritemId_' + orderitemid + '" data-orderitemid="' + orderitemid + '" data-pname="' + data[i].product_name + '">';

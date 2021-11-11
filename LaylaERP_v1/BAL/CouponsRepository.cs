a63ed693165effa7d751e1cb807b917a77d610ea
Calvin Christison <christison.quickfix@gmail.com>
2021-11-11 17:45:23 +0530

    New changes

--- LaylaERP_v1/BAL/CouponsRepository.cs
+++ LaylaERP_v1/BAL/CouponsRepository.cs
@@ -737,21 +737,21 @@ namespace LaylaERP.BAL
                 DateTime cDate = CommonDate.CurrentDate(), cUTFDate = CommonDate.UtcDate();
                 StringBuilder strSql = new StringBuilder();
 
-                //strSql.Append(string.Format("insert into wp_posts(post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,qb_sync,qb_refund_sync) (select post_author,'"+ cDate.ToString("yyyy-MM-dd HH:mm:ss") + "','"+ cDate.ToString("yyyy-MM-dd HH:mm:ss") + "',post_content, concat(post_title , '"+ code + "') post_title ,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,ID,guid,menu_order,post_type,post_mime_type,comment_count,qb_sync,qb_refund_sync from wp_posts_Coupons);"));
-                //strSql.Append(string.Format("insert into wp_postmeta (post_id,meta_key,meta_value)(select wp.id pid,meta_key,case when meta_key = 'date_expires' then UNIX_TIMESTAMP(STR_TO_DATE('"+ExpireDate+"', '%m/%d/%Y')) else meta_value end meta_value from wp_postmeta_coupons inner join wp_posts_Coupons on wp_posts_Coupons.id = wp_postmeta_coupons.post_id inner join wp_posts wp on wp.post_parent = wp_posts_Coupons.ID and wp.post_type = 'shop_coupon' and wp.post_title like '%"+code+"%'); "));
-                //strSql.Append(string.Format("insert into wp_Coupons_month (month_type) values ('{0}'); ", code));
+                strSql.Append(string.Format("insert into wp_posts(post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count,qb_sync,qb_refund_sync) (select post_author,'" + cDate.ToString("yyyy-MM-dd HH:mm:ss") + "','" + cDate.ToString("yyyy-MM-dd HH:mm:ss") + "',post_content, concat(post_title , '" + code + "') post_title ,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,ID,guid,menu_order,post_type,post_mime_type,comment_count,qb_sync,qb_refund_sync from wp_posts_Coupons);"));
+                strSql.Append(string.Format("insert into wp_postmeta (post_id,meta_key,meta_value)(select wp.id pid,meta_key, DATEDIFF(SECOND, '1970-01-01','"+ExpireDate+"')  meta_value from wp_postmeta_coupons inner join wp_posts_Coupons on wp_posts_Coupons.id = wp_postmeta_coupons.post_id inner join wp_posts wp on wp.post_parent = wp_posts_Coupons.ID and wp.post_type = 'shop_coupon' and wp.post_title like '%" + code + "%'); "));
+                strSql.Append(string.Format("insert into wp_Coupons_month (month_type) values ('{0}'); ", code));
+               result = SQLHelper.ExecuteNonQuery(strSql.ToString());
+                //string strsql = "erp_Coupon_iud";
+                //SqlParameter[] para =
+                //{
+                //    new SqlParameter("@qflag", "IAU"),                    
+                //    new SqlParameter("@code", code),
+                //    new SqlParameter("@ExpireDate", ExpireDate),
+                //    new SqlParameter("@cDate", cDate),              
+                //};
+               // int result = Convert.ToInt32(SQLHelper.ExecuteNonQuery(strsql, para));
 
-                string strsql = "erp_Coupon_iud";
-                SqlParameter[] para =
-                {
-                    new SqlParameter("@qflag", "IAU"),                    
-                    new SqlParameter("@code", code),
-                    new SqlParameter("@ExpireDate", ExpireDate),
-                    new SqlParameter("@cDate", cDate),              
-                };
-             //   int result = Convert.ToInt32(SQLHelper.ExecuteNonQuery(strsql, para));
-
-                result = SQLHelper.ExecuteNonQuery(strSql.ToString());
+               // result = SQLHelper.ExecuteNonQuery(strSql.ToString());
 
                // string query = strSql.ToString();
 

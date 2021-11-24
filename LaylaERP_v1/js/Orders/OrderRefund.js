717bb0c1b484b27902d3d8e97f4a84efd180240a
Johnsonquickfix <johnson.quickfix@gmail.com>
2021-11-24 16:57:12 +0530

    Order refund validation update

--- LaylaERP_v1/js/Orders/OrderRefund.js
+++ LaylaERP_v1/js/Orders/OrderRefund.js
@@ -529,7 +529,9 @@ function saveCO() {
     if (itemsDetails.length <= 0) { swal('Alert!', 'Please add product.', "error"); return false; }
     let obj = { order_id: oid, order_statsXML: JSON.stringify(postStatus), postmetaXML: JSON.stringify(postMeta), order_itemsXML: JSON.stringify(itemsDetails) };
     let totalPay = parseFloat(parseFloat(AvailableGiftCardAmount) + parseFloat(orderTotal)).toFixed(2);
-    //console.log(postStatus, itemsDetails); return;
+    let bal = $('#netPaymentTotal').text() || 0.00;
+    if (net_total > bal) { swal('Alert!', 'Order amount cannot refund more than ' + bal.fixed(2) + '.', "error"); return false; }
+    //console.log(postStatus, itemsDetails, totalPay, net_total); return;
     if (totalPay > net_total) {
         $.ajax({
             type: "POST", contentType: "application/json; charset=utf-8",

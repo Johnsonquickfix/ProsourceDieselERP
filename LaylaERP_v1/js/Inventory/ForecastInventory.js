c8d209883f485486317478ac9b9ffae5fc7aee85
fred <fred.quickfix1@gmail.com>
2021-12-09 15:21:03 +0530

    journal

--- LaylaERP_v1/js/Inventory/ForecastInventory.js
+++ LaylaERP_v1/js/Inventory/ForecastInventory.js
@@ -11,8 +11,24 @@
         startDate: moment(), autoUpdateInput: true, alwaysShowCalendars: true,
         locale: { format: 'MM/DD/YYYY', cancelLabel: 'Clear' }, opens: 'right', orientation: "left auto"
     }, function (start, end, label) {
-        //ForeCastreport(true);
+       
     });
+
+/*
+    $('#txtForecastDate').daterangepicker({
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment(), autoUpdateInput: true, alwaysShowCalendars: true,
+        locale: { format: 'MM/DD/YYYY', cancelLabel: 'Clear' }, opens: 'right', orientation: "left auto"
+    }, function (start, end, label) {
+        
+    }); */
 });
 
 
@@ -56,9 +72,6 @@ function getProductList() {
 }
 
 function ForeCastreport(is_date) {
-    //var d = new Date();
-    //var m = d.getMonth();
-    //var y = d.getFullYear();
     let sd = $('#txtOrderDate').data('daterangepicker').startDate.format('YYYY-MM-DD');
     let ed = $('#txtOrderDate').data('daterangepicker').endDate.format('YYYY-MM-DD');
     let dfa = is_date ? "'" + sd + "' and '" + ed + "'" : '';
@@ -96,3 +109,44 @@ function ForeCastreport(is_date) {
     });
 
 }
+/*
+//Forecast report for all products
+ForecastReportAll();
+function ForecastReportAll() {
+    let sd = $('#txtForecastDate').data('daterangepicker').startDate.format('YYYY-MM-DD');
+    let ed = $('#txtForecastDate').data('daterangepicker').endDate.format('YYYY-MM-DD');
+    //let dfa = is_date ? "'" + sd + "' and '" + ed + "'" : '';
+
+    var obj = { stockfromdate: sd, stocktodate: ed }
+    $.ajax({
+        url: '/Inventory/GetForecastReport',
+        method: 'post',
+        datatype: 'json',
+        contentType: "application/json; charset=utf-8",
+        processing: true,
+        data: JSON.stringify(obj),
+        success: function (data) {
+            $('#dtForecastReportAllList').dataTable({
+                destroy: true,
+                scrollX: false,
+                data: JSON.parse(data),
+                "columns": [
+                    { data: 'id', title: 'ID', sWidth: "5%" },
+                    { data: 'post_title', title: 'Product', sWidth: "25%" },
+                    { data: 'op_stock', title: 'In Stock', sWidth: "10%" },
+                    { data: 'prev_sale', title: 'Previous Month Sale', sWidth: "10%" },
+                    { data: 'daily_velocity', title: 'Projected Daily Velocity', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '') },
+                    { data: 'remain_days', title: 'Remaining Days', sWidth: "10%", render: $.fn.dataTable.render.number('', '', 0, '') },
+                    { data: 'run_out_date', title: 'Run Out Date', sWidth: "10%" },
+                    { data: 'week_velocity', title: 'Projected Weekly Velocity', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '') },
+                    { data: 'month_velocity', title: 'Projected Monthly Velocity', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '') },
+                ],
+                "order": [[0, 'asc']],
+            });
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.responseText);
+        }
+    });
+
+} */

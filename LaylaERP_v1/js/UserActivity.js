d7dd90be60d1153726df61157ebe4743d6a20f59
Steven Methew <steven.quickfix@gmail.com>
2021-12-13 20:37:36 +0530

    Gift card user activity

---
+++ LaylaERP_v1/js/UserActivity.js
@@ -0,0 +1,18 @@
+﻿
+function ActivityLog(ModuleName, ModuleURL) {
+    console.log(ModuleName, ModuleURL);
+    var obj = {
+        ModuleName: ModuleName, ModuleURL: ModuleURL
+    }
+    $.ajax({
+        url: '/ThirdParty/ActivityDbLog/', dataType: 'json', type: 'Post',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(obj),
+        dataType: "json",
+        success: function (data) { },
+        error: function (error) {
+            console.log(error);
+        },
+    })
+
+}

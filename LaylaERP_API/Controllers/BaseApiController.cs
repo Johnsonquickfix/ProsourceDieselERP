44a21a089379f6df9ccbbdce2e89e7e56ab78819
Johnsonquickfix <johnson.quickfix@gmail.com>
2021-12-16 14:46:30 +0530

    API ADD

---
+++ LaylaERP_API/Controllers/BaseApiController.cs
@@ -0,0 +1,21 @@
+﻿namespace LaylaERP_API.Controllers
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Web.Configuration;
+    using System.Web.Http;
+
+    public class BaseApiController : ApiController
+    {
+        public string IKEYValue = WebConfigurationManager.AppSettings["IKey"];
+
+        internal static string GetHeader(HttpRequestMessage thisReq)
+        {
+            var retVal = string.Empty;
+            IEnumerable<string> values = new List<string>();
+            thisReq.Headers.TryGetValues("userName", out values);
+            retVal = ((string[])values)[0];
+            return retVal;
+        }
+    }
+}
---
+++ LaylaERP_API/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using LaylaERP_API.Models;
+using System.Web;
+
+namespace LaylaERP_API.Providers
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly string _publicClientId;
+
+        public ApplicationOAuthProvider(string publicClientId)
+        {
+            if (publicClientId == null)
+            {
+                throw new ArgumentNullException("publicClientId");
+            }
+
+            _publicClientId = publicClientId;
+        }
+         
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var appDbContext = new ApplicationDbContext(context.UserName);
+            HttpContext.Current.GetOwinContext().Set(appDbContext);
+            HttpContext.Current.GetOwinContext().Set(new ApplicationUserManager(new Microsoft.AspNet.Identity.EntityFramework.UserStore<ApplicationUser>(appDbContext)));
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            //var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
+            var user = await userManager.FindAsync(context.UserName, context.Password);
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+            else if (user.LockoutEnabled)
+            {
+                context.SetError("invalid_grant", "Your Account is temporary deactivated.");
+                return;
+            }
+
+            ClaimsIdentity oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, DefaultAuthenticationTypes.ExternalBearer);
+            //ClaimsIdentity cookiesIdentity = await user.GenerateUserIdentityAsync(userManager, DefaultAuthenticationTypes.ExternalBearer);
+            List<Claim> roles = oAuthIdentity.Claims.Where(c => c.Type == ClaimTypes.Role).ToList();
+            AuthenticationProperties properties = CreateProperties(user.UserName, Newtonsoft.Json.JsonConvert.SerializeObject(roles.Select(x => x.Value)));
+            AuthenticationTicket ticket = new AuthenticationTicket(oAuthIdentity, properties);
+            context.Validated(ticket);
+            //context.Request.Context.Authentication.SignIn(cookiesIdentity);
+            context.Request.Context.Authentication.SignIn(oAuthIdentity);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // Resource owner password credentials does not provide a client ID.
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            if (context.ClientId == _publicClientId)
+            {
+                Uri expectedRootUri = new Uri(context.Request.Uri, "/");
+
+                if (expectedRootUri.AbsoluteUri == context.RedirectUri)
+                {
+                    context.Validated();
+                }
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public static AuthenticationProperties CreateProperties(string userName, string Roles)
+        {
+            IDictionary<string, string> data = new Dictionary<string, string>
+            {
+                { "userName", userName },
+                {"roles",Roles}
+            };
+            return new AuthenticationProperties(data);
+        }
+    }
+}

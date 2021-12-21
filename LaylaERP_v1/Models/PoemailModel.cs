080a2bb08e8c298439416c645131fa12af1d4a67
fred <fred.quickfix1@gmail.com>
2021-12-21 17:09:22 +0530

    poemail

---
+++ LaylaERP_v1/Models/PoemailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaylaERP_v1.Models
+{
+    public class PoemailModel
+    {
+        public string user_id { get; set; }
+        public string user_email { get; set; }
+        public int status { get; set; }
+        public int rowid { get; set; }
+    }
+}

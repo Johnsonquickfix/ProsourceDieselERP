787da424f855ebe59e67e27b48cb3f67cd32df7f
david.quickfix1 <david.quickfix1@gmail.com>
2022-01-03 13:56:27 +0530

    changes api

--- LaylaERP_API/Controllers/HomeController.cs
+++ LaylaERP_API/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace LaylaERP_API.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Layla ERP";
+            ViewBag.Title = "Layla ERP - API";
 
             return View();
         }

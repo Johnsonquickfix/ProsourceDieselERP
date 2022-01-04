55d174546c60ae8a33532c5de5c7a31249383f30
fred <fred.quickfix1@gmail.com>
2022-01-04 18:40:13 +0530

    Events

---
+++ LaylaERP_v1/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaylaERP_v1.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddNewEvents()
+        {
+            return View();
+        }
+    }
+}

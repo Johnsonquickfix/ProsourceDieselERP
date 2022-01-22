6023d57fa0afad76cca219825af7c7b07821ec87
johnson.quickfix <johnson.quickfix@gmail.com>
2022-01-22 12:36:57 +0530

    Account Journal date wise sorting update

--- pub/js/FeeNTax/FeeNTax.js
+++ pub/js/FeeNTax/FeeNTax.js
@@ -1,74 +1,64 @@
 ﻿
+//function Datagrid() {
+
+//    $('#dtdata').dataTable();
+//        $.ajax({
+//            type: "GET",
+//            url: "/FeeNTax/GetFeeNTaxList",
+//            contentType: "application/json; charset=utf-8",
+//            data: "{}",
+//            dataType: "json",
+//            success: function (data) {
+//                console.log(data);
+//                $.each(data, function (key, i) {
+//                    $("#dtdata").append("<tr><td>" + i.id + "</td><td>" + i.staterecyclefee + "</td><td>" + i.state + "</td><td>" + i.item_name + "</td><td>" + i.city + "</td><td>" + i.zip + "</td><td>" + i.country + "</td><td><a href='#'>Edit</a></td></tr>");
+//                });
+
+//            },
+//            error: function (result) {
+//                alert("Error");
+//            }
+//        })
+//    };
+
 function Datagrid() {
 
-    $('#dtdata').dataTable();
-
-
-
-        $.ajax({
-            type: "GET",
-            url: "/FeeNTax/GetFeeNTaxList",
+    var s = $("#ddlSearchStatus").val();
+    $('#dtdata').DataTable({
+        destroy: true,
+        scrollX: true,
+        bAutoWidth: false,
+        "ajax": {
+            "url": "/FeeNTax/GetFeeNTaxList",
+            "type": 'GET',
+            "dataType": 'json',
+            data: { status: s },
             contentType: "application/json; charset=utf-8",
-            data: "{}",
-            dataType: "json",
-            success: function (data) {
-                console.log(data);
-                $.each(data, function (key, i) {
-                    $("#dtdata").append("<tr><td>" + i.id + "</td><td>" + i.staterecyclefee + "</td><td>" + i.state + "</td><td>" + i.item_name + "</td><td>" + i.city + "</td><td>" + i.zip + "</td><td>" + i.country + "</td><td><a href='#'>Edit</a></td></tr>");
-                });
+        },
+        lengthMenu: [[10, 20, 50, 100], [10, 20, 50, 100]],
+        "columns": [
 
-            },
-            error: function (result) {
-                alert("Error");
+            { 'data': 'id', 'sWidth': "8%" },
+            { 'data': 'recyclefee', 'sWidth': "15%" },
+            { 'data': 'item_name', 'sWidth': "20%" },
+            { 'data': 'state', 'sWidth': "15%" },
+            { 'data': 'country', 'sWidth': "10%" },
+            { 'data': 'taxableshow', 'sWidth': "10%" },
+            { 'data': 'activeshow', 'sWidth': "10%" },
+
+            {
+                'data': 'id', sWidth: "12%",
+                'render': function (id, type, full, meta) {
+                    if ($("#hfEdit").val() == "1") {
+                        return '<a href="../FeeNTax/CreateNew/' + id + '" data-toggle="tooltip" title="View/Edit tax" onclick="ActivityLog(\'Edit state recycle tax\',\'/FeeNTax/Index/' + id +'\');"><i class="glyphicon glyphicon-pencil"></i></a>'
+                    }
+                    else { return "No Permission"; }
+                }
             }
-        })
-    };
-
-
-
-    //destroy: true,
-    //    bAutoWidth: false,
-    //        "ajax": {
-    //        "url": "/FeeNTax/GetFeeNTaxList",
-    //        "type": 'GET',
-    //            "dataType": 'json',
-    //                data: {},
-    //    contentType: "application/json; charset=utf-8",
-    //    },
-    //lengthMenu: [[10, 20, 50, 100], [10, 20, 50, 100]],
-    //    "columns": [
-           
-    //        { 'data': 'id', 'sWidth': "8%" },
-    //        { 'data': 'staterecyclefee', 'sWidth': "12%" },
-    //        { 'data': 'state', 'sWidth': "25%" },
-    //        { 'data': 'item_name', 'sWidth': "10%" },
-    //        { 'data': 'zip', 'sWidth': "15%" },
-    //        { 'data': 'country', 'sWidth': "30%" },
-    //        {
-    //            'data': 'my', 'sWidth': "22%",
-
-    //        },
-    //        {
-    //            'data': 'ID', sWidth: "8%",
-    //            'render': function (ID, type, full, meta) {
-    //                return '<a href="javascript:void(0);" onClick="EditUser(' + ID + ')"><i class="glyphicon glyphicon-pencil"></i></a>'
-    //            }
-    //        }
-    //    ],
-    //    "order": [[1, 'desc']],
-    //});
- 
-
-
-//function EditUser(id) {
-//    window.location.href = 'Index?id=' + id;
-//}
-
-
-
-
-
-
+        ],
+        "order": [[0, 'desc']],
+    });
+}
 //    $.ajax({
 //        type: "GET",
 //        url: "/FeeNTax/GetFeeNTaxList",
@@ -89,119 +79,134 @@ function Datagrid() {
 //}
 
 function AddFeeNTax() {
-    debugger
-
-    Id = $("#hfid").val();
+    id = $("#hfid").val();
     Staterecyclefee = $("#StateRecycleFee").val();
-    //item_parent_id = $("#").val();
-    Item_name = $("#Item_Name").val();
+    //Item_name = $("#Item_Name").text().trim();
+    var data = $("#Item_Name option:selected").text();
+    Item_name=data;
+    //alert(data);
     City = $("#shipcity").val();
     State = $("#shipstate").val();
     Zip = $("#Ship_Zip_PostCode").val();
     Country = $("#shipcountry").val();
-
+    item_parent_id = $("#Item_Name").val();
+    istaxable = $("#chktaxable").prop("checked") ? 1 : 0;
+    isactive = $("#chkactive").prop("checked") ? 1 : 0;
 
     if (Staterecyclefee == "") {
-        swal('alert', 'Please Enter StateRecyclefee', 'error').then(function () { swal.close(); $('#StateRecycleFee').focus(); })
+        swal('alert', 'Please enter state recycle fee', 'error').then(function () { swal.close(); $('#StateRecycleFee').focus(); })
     }
     else if (Item_name == "") {
-        swal('alert', 'Please Enter item_name', 'error').then(function () { swal.close(); $('#Item_Name').focus(); })
+        swal('alert', 'Please enter product name', 'error').then(function () { swal.close(); $('#Item_Name').focus(); })
+    }
+    else if (City == "") {
+        swal('alert', 'Please enter ship city', 'error').then(function () { swal.close(); $('#shipcity').focus(); })
+    }
+    else if (State == "") {
+        swal('alert', 'Please enter ship state', 'error').then(function () { swal.close(); $('#shipstate').focus(); })
     }
     else if (Zip == "") {
-        swal('alert', 'Please Enter zip', 'error').then(function () { swal.close(); $('#Ship_Zip_PostCode').focus(); })
+        swal('alert', 'Please enter ship zip code', 'error').then(function () { swal.close(); $('#Ship_Zip_PostCode').focus(); })
     }
 
     else {
-        var obj = {
-            staterecyclefee: Staterecyclefee, item_name: Item_name, city: City, state: State, zip: Zip,
-            country: Country
-        }
-        $.ajax({
-            url: '/FeeNTax/StateRecycleTax/', dataType: 'json', type: 'Post',
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(obj),
-            dataType: "json",
-            success: function (data) {
-                if (data.status == true) {
-
-                    swal('Alert!', data.message, 'success');
-
-                }
-                else {
-                    swal('Alert!', data.message, 'error')
-                }
-            },
-            complete: function () {
-                $("#loader").hide();
-            },
-            error: function (error) {
-                swal('Error!', 'something went wrong', 'error');
-            },
-        })
-
-        //function EditUser(id) {
-        //    window.location.href = 'StateRecyleTax?id=' + i.id;
-        //}
-    }
-}
-
-function GetFeeNTaxByID(id) {
-            debugger
-            var ID = id;
-            var obj = {};
+        $("#Ship_Zip_PostCode").change();
+        if ($("#hfzipstatus").val() == "true") {
+            var obj = {
+                id: id, staterecyclefee: Staterecyclefee, item_name: Item_name, city: City, state: State, zip: Zip,
+                country: Country, item_parent_id: item_parent_id, is_taxable: istaxable, is_active: isactive,
+            }
             $.ajax({
-
-                url: '/FeeNTax/GetFeeNTaxByID/' + ID,
-                type: 'GET',
-                contentType: "application/json; charset=utf-8",
-                dataType: 'JSON',
-                data: JSON.stringify(obj),
-                success: function (data) {
-                    var i = JSON.parse(data);
-                    $("#StateRecycleFee").val(i[0].staterecyclefee);
-                    $("#shipstate").val(i[0].state);
-                    $("#Item_Name").val(i[0].item_name);
-                    $("#shipcity").val(i[0].city);
-                    $("#Ship_Zip_PostCode").val(i[0].zip);
-                    $("#shipcountry").val(i[0].country);
-
-
-                },
-                error: function (msg) { alert(msg); }
-            });
-
-        }
-
-function UpdateFeeNTaxStatus() {
-            
-            var obj = { strVal: ID }
-            debugger
-            $.ajax({
-                url: '/FeeNTax/UpdateFeeNTax/', dataType: 'json', type: 'Post',
+                url: '/FeeNTax/StateRecycleTax/', dataType: 'json', type: 'Post',
                 contentType: "application/json; charset=utf-8",
                 data: JSON.stringify(obj),
                 dataType: "json",
+                beforeSend: function () {
+                    $("#loader").show();
+                },
                 success: function (data) {
-                    console.log(data);
                     if (data.status == true) {
-                        swal('Alert!', data.message, 'success');
+                        $('#parent > input:text').val('');
+                        swal('Success!', data.message, 'success').then((result) => { location.href = '../Index'; });
                     }
                     else {
                         swal('Alert!', data.message, 'error')
                     }
                 },
+                complete: function () {
+                    $("#loader").hide();
+                    isEdit(false);
+                },
                 error: function (error) {
                     swal('Error!', 'something went wrong', 'error');
                 },
             })
-        
 
-        $("#StateRecycleFee").val('');
-        //$("#").val();
-        $("#Item_Name").val('');
-        $("#shipcity").val('');
-        $("#shipstate").val('');
-        $("#Ship_Zip_PostCode").val('');
-        $("#shipcountry").val('');
+            //function EditUser(id) {
+            //    window.location.href = 'StateRecyleTax?id=' + i.id;
+            //}
+        }
     }
+}
+
+function GetFeeNTaxByID(id) {
+
+    var ID = id;
+    var obj = {};
+    $.ajax({
+
+        url: '/FeeNTax/GetFeeNTaxByID/' + ID,
+        type: 'GET',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'JSON',
+        data: JSON.stringify(obj),
+        success: function (data) {
+            var i = JSON.parse(data);
+            console.log(i);
+            $("#shipcountry").val(i[0].country).trigger('change');
+            $("#StateRecycleFee").val(i[0].staterecyclefee);
+            $("#shipcity").empty().append('<option value="' + i[0].city + '" selected>' + i[0].city + '</option>');
+            $("#shipstate").empty().append('<option value="' + i[0].state + '" selected>' + i[0].state + '</option>');
+            $("#Item_Name").empty().append('<option value="' + i[0].item_parent_id + '" selected>' + i[0].item_name + '</option>');
+            $("#Ship_Zip_PostCode").val(i[0].zip);
+            i[0].is_taxable == true ? $("#chktaxable").prop("checked", true) : $("#chktaxable").prop("checked", false);
+            i[0].is_active == true ? $("#chkactive").prop("checked", true) : $("#chkactive").prop("checked", false);
+            $('#Item_Name').val(i[0].item_parent_id).trigger('change');
+        },
+        error: function (msg) { alert(msg); }
+    });
+}
+
+function UpdateFeeNTaxStatus() {
+
+    var obj = { strVal: ID }
+    debugger
+    $.ajax({
+        url: '/FeeNTax/UpdateFeeNTax/', dataType: 'json', type: 'Post',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(obj),
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            if (data.status == true) {
+                swal('Success!', data.message, 'success');
+            }
+            else {
+                swal('Alert!', data.message, 'error')
+            }
+        },
+        error: function (error) {
+            swal('Error!', 'something went wrong', 'error');
+        },
+    })
+
+
+    $("#StateRecycleFee").val('');
+    //$("#").val();
+    $("#Item_Name").val('');
+    $("#shipcity").val('');
+    $("#shipstate").val('');
+    $("#Ship_Zip_PostCode").val('');
+    $("#shipcountry").val('');
+}
 
--- pub/js/Users/UserLogin.js
+++ pub/js/Users/UserLogin.js
@@ -2,8 +2,9 @@
     var uID = $('#txtUserId').val();
     var uPass = $('#txturPassword').val();
     if (uID == "") { swal('Alert!', 'Please enter user name!', "error"); }
-    else if (uPass == "") { swal('Alert!', "Enter enter Password.", "error"); }
+    else if (uPass == "") { swal('Alert!', "Please enter password!", "error"); }
     else {
+        $('#a-autoid-0-announce').text("Waiting...");
         var obj = { UserName: uID, PassWord: uPass }
         $.ajax({
             url: '/AccountAPI/Login', dataType: 'json', type: "Post",
@@ -18,8 +19,52 @@
                     window.open(top.window.location.origin + '/' + data.url, "_self");
                 }
                 else {
-                    swal('Alert!', data.message, "error");
+                    swal('Error!', data.message, "error");
+                    $('#a-autoid-0-announce').text("Login");
+                  
                 }
+            },
+            error: function (errormessage) {
+                //$("#loading-div-background").hide();
+                swal('Error!', "Something went wrong .", "error");
+                $('#a-autoid-0-announce').text("Login");
+            
+            }
+        });
+    }
+
+}
+
+$('#sendquery').click(function () {
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var content = $('#content').val();
+    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (name == "") { swal('Alert', 'Please enter name', 'error').then(function () { swal.close(); $('#name').focus(); }); }
+    else if (email == "") { swal('Alert', 'Please enter email', 'error').then(function () { swal.close(); $('#email').focus(); }); }
+    else if (!pattern.test(email)) { swal('Alert', 'Please enter valid email', 'error').then(function () { swal.close(); $('#email').focus(); }); }
+    else if (subject == 0) { swal('Alert', 'Please enter subject', 'error').then(function () { swal.close(); $('#subject').focus(); }); }
+
+
+    else {
+
+        var obj = { name: name, email: email, subject: subject, content: content }
+        $.ajax({
+            url: '/AccountAPI/ContactUs', dataType: 'json', type: "Post",
+
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(obj),
+            success: function (data) {
+                //$("#loading-div-background").hide();
+
+                swal('success!', "Message send Successfully", "success");
+                $('#name').val('');
+                $('#email').val('');
+                $('#subject').val('');
+                $('#content').val(0);
+
             },
             error: function (errormessage) {
                 //$("#loading-div-background").hide();
@@ -27,5 +72,4 @@
             }
         });
     }
-
-}
+});

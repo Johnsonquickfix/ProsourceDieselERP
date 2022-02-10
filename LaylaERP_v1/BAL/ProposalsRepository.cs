da28e440a5c4aea1aba4c13f11795d985b8ef022
johnson.quickfix <johnson.quickfix@gmail.com>
2022-02-10 16:04:49 +0530

    Supplier Proposals (DTC) Update

---
+++ LaylaERP_v1/BAL/ProposalsRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace LaylaERP.BAL
+{
+    using DAL;
+    using Models;
+    using UTILITIES;
+    using System.Data.SqlClient;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Web;
+    using System.Xml;
+
+    public class ProposalsRepository
+    {
+        public static DataTable GetProposals(DateTime? fromdate, DateTime? todate, long supplierid, string search, int pageno, int pagesize, out int totalrows, string SortCol = "id", string SortDir = "DESC")
+        {
+            DataTable dt = new DataTable();
+            totalrows = 0;
+            try
+            {
+                SqlParameter[] parameters =
+                {
+                    fromdate.HasValue ? new SqlParameter("@fromdate", fromdate.Value) : new SqlParameter("@fromdate", DBNull.Value),
+                    todate.HasValue ? new SqlParameter("@todate", todate.Value) : new SqlParameter("@todate", DBNull.Value),
+                    new SqlParameter("@flag", "SERCH"),
+                    //!CommanUtilities.Provider.GetCurrent().UserType.ToLower().Contains("administrator") ? new SqlParameter("@userid", CommanUtilities.Provider.GetCurrent().UserID) : new SqlParameter("@userid",DBNull.Value),
+                    new SqlParameter("@supplierid", supplierid),
+                    new SqlParameter("@searchcriteria", search),
+                    new SqlParameter("@pageno", pageno),
+                    new SqlParameter("@pagesize", pagesize),
+                    new SqlParameter("@sortcol", SortCol),
+                    new SqlParameter("@sortdir", SortDir)
+                };
+                DataSet ds = SQLHelper.ExecuteDataSet("erp_Proposals_search", parameters);
+                dt = ds.Tables[0];
+                if (ds.Tables[1].Rows.Count > 0)
+                    totalrows = Convert.ToInt32(ds.Tables[1].Rows[0]["TotalRecord"].ToString());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dt;
+        }
+
+    }
+}
---
+++ LaylaERP_v1/Controllers/ProposalsController.cs
@@ -0,0 +1,48 @@
+﻿namespace LaylaERP.Controllers
+{
+    using BAL;
+    using Models;
+    using UTILITIES;
+    using Microsoft.Ajax.Utilities;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+
+    public class ProposalsController : Controller
+    {
+        // GET: SupplierProposals
+        [Route("proposals/supplierproposals")]
+        public ActionResult SupplierProposalsList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("proposals/proposals-list")]
+        public JsonResult GetProposalsList(JqDataTableModel model)
+        {
+            string result = string.Empty;
+            int TotalRecord = 0;
+            try
+            {
+                long supplierid = 0;
+                DateTime? fromdate = null, todate = null;
+                if (!string.IsNullOrEmpty(model.strValue1))
+                    fromdate = Convert.ToDateTime(model.strValue1);
+                if (!string.IsNullOrEmpty(model.strValue2))
+                    todate = Convert.ToDateTime(model.strValue2);
+                if (!string.IsNullOrEmpty(model.strValue3))
+                    supplierid = Convert.ToInt32(model.strValue3);
+                DataTable dt = ProposalsRepository.GetProposals(fromdate, todate, supplierid, model.sSearch, model.iDisplayStart, model.iDisplayLength, out TotalRecord, model.sSortColName, model.sSortDir_0);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(new { sEcho = model.sEcho, recordsTotal = TotalRecord, recordsFiltered = TotalRecord, iTotalRecords = TotalRecord, iTotalDisplayRecords = TotalRecord, aaData = result }, 0);
+        }
+    }
+}

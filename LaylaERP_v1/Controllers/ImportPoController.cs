c50487aedf1d5a7d85f929bafcc6419b01e7957a
Tom Bell <tombell.quickfix@gmail.com>
2022-02-28 17:59:48 +0530

    export

---
+++ LaylaERP_v1/Controllers/ImportPoController.cs
@@ -0,0 +1,149 @@
+﻿using ExcelDataReader;
+using LaylaERP.DAL;
+using LaylaERP.Models;
+using LaylaERP_v1.BAL;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace LaylaERP_v1.Controllers
+{
+    public class ImportPoController : Controller
+    {
+        // GET: ImportPo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ImportPo()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("uploadpofile")]
+        public ActionResult ImportPoFile(HttpPostedFileBase importFile)
+        {
+            if (importFile == null) return Json(new { Status = 0, Message = "No File Selected" });
+            long size = importFile.ContentLength;
+            string fileType = Path.GetExtension(importFile.FileName);
+            if (size < 2097152)
+            {
+                try
+                {
+                    var fileData = GetDataFromPoExcelFile(importFile.InputStream);
+                    XmlDocument xmlDoc = CommonClass.ToXml(fileData);
+                    var dt = UploadXMLData("IDEST", xmlDoc);
+                    if (dt.Rows.Count > 0)
+                    {
+                        if (dt.Rows[0]["Response"].ToString().Contains("Success"))
+                            return Json(new { Status = 1, Message = "Data Imported Successfully. " });
+                        else
+                            return Json(new { Status = 0, Message = "Excel invalid data format. " });
+                    }
+                    return Json(new { Status = 0, Message = "Excel invalid data format. " });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { Status = 0, Message = ex.Message });
+                }
+            }
+            else { return Json(new { Status = 0, Message = "Not excel file or size of file is larger than 2MB" }); }
+        }
+
+        private List<ImportPoModel> GetDataFromPoExcelFile(Stream stream)
+        {
+            var poList = new List<ImportPoModel>();
+            try
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration
+                    {
+                        UseColumnDataType = false,
+                        ConfigureDataTable = delegate { return new ExcelDataTableConfiguration { UseHeaderRow = true }; }
+                    });
+
+                    if (dataSet.Tables.Count > 0)
+                    {
+                        var dataTable = dataSet.Tables[0];
+                        foreach (DataRow objDataRow in dataTable.Rows)
+                        {
+                            if (objDataRow.ItemArray.All(x => string.IsNullOrEmpty(x?.ToString()))) continue;
+                            poList.Add(new BAL.ImportPoModel()
+                            {
+                                po_date = objDataRow["Date"].ToString(),
+                                po_invoice_number = objDataRow["Invoice"].ToString(),
+                                customer_requisition = objDataRow["Customer requisition"].ToString(),
+                                po_due_date = objDataRow["Due date"].ToString(),
+                                po_currency = objDataRow["Currency"].ToString(),
+                                original_invoice_amt = objDataRow["Original Invoice Amount"] != DBNull.Value ? (!string.IsNullOrEmpty(objDataRow["Original Invoice Amount"].ToString()) ? Convert.ToDecimal(objDataRow["Original Invoice Amount"].ToString()) : 0) : 0,
+                                remain_invoice_amt = objDataRow["Remaining Invoice Amount"] != DBNull.Value ? (!string.IsNullOrEmpty(objDataRow["Remaining Invoice Amount"].ToString()) ? Convert.ToDecimal(objDataRow["Remaining Invoice Amount"].ToString()) : 0) : 0,
+                            }); 
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return poList;
+        }
+
+        public class CommonClass
+        {
+            public static XmlDocument ToXml<T>(List<T> _ListObj)
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                XPathNavigator nav = xmlDoc.CreateNavigator();
+                using (XmlWriter writer = nav.AppendChild())
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(List<T>), new XmlRootAttribute("BulkData"));
+                    ser.Serialize(writer, _ListObj);
+                }
+                return xmlDoc;
+            }
+        }
+        public static DataTable UploadXMLData(string qFlag, XmlDocument xmlList)
+        {
+            var dt = new DataTable();
+            try
+            {
+                SqlParameter[] parameters =
+                {
+                    new SqlParameter("@detailsXML ",xmlList.OuterXml),
+                    new SqlParameter("@QFlag",qFlag)
+                };
+
+                dt = SQLHelper.ExecuteDataTable("erp_commerce_purchase_order_invoice_import", parameters);
+            }
+            catch (SqlException ex)
+            { throw ex; }
+            return dt;
+        }
+
+        public JsonResult GetImportPolList(JqDataTableModel model)
+        {
+            string result = string.Empty;
+            int TotalRecord = 0;
+            try
+            {
+                DataTable dt = ImportPoRepository.GetImportPoList(model.strValue1, model.sSearch, model.iDisplayStart, model.iDisplayLength, out TotalRecord, model.sSortColName, model.sSortDir_0);
+                result = JsonConvert.SerializeObject(dt);
+            }
+            catch (Exception ex) { throw ex; }
+            return Json(new { sEcho = model.sEcho, recordsTotal = TotalRecord, recordsFiltered = TotalRecord, iTotalRecords = TotalRecord, iTotalDisplayRecords = TotalRecord, aaData = result }, 0);
+        }
+    }
+}
---
+++ LaylaERP_v1/js/ImportPo/ImportPo.js
@@ -0,0 +1,51 @@
+﻿function ImportPoList() {
+    var urid = $("#ddlSearchStatus").val();
+    ID = $("#hfid").val();
+    var table_EL = $('#EmailListdata').DataTable({
+        columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': true, 'targets': [0] }], order: [[0, "desc"]],
+        destroy: true, bProcessing: true, bServerSide: true, bAutoWidth: false, searching: true,
+        responsive: true, lengthMenu: [[10, 20, 50], [10, 20, 50]],
+        language: {
+            lengthMenu: "_MENU_ per page",
+            zeroRecords: "Sorry no records found",
+            info: "Showing _START_ to _END_ of _TOTAL_ entries",
+            infoFiltered: "",
+            infoEmpty: "No records found",
+            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
+        },
+        initComplete: function () {
+            $('#EmailListdata_filter input').unbind();
+            $('#EmailListdata_filter input').bind('keyup', function (e) {
+                var code = e.keyCode || e.which;
+                if (code == 13) { table_EL.search(this.value).draw(); }
+            });
+        },
+        sAjaxSource: "/ImportPo/GetImportPolList",
+        fnServerData: function (sSource, aoData, fnCallback, oSettings) {
+            aoData.push({ name: "strValue1", value: urid });
+            var col = 'id';
+            if (oSettings.aaSorting.length > 0) { aoData.push({ name: "sSortColName", value: oSettings.aoColumns[oSettings.aaSorting[0][0]].data }); }
+            //if (oSettings.aaSorting.length >= 0) {
+            //    var col = oSettings.aaSorting[0][0] == 0 ? "user_id" : oSettings.aaSorting[0][0] == 1 ? "user_email" : oSettings.aaSorting[0][0] == 2 ? "status" : "id";
+            //    aoData.push({ name: "sSortColName", value: col });
+            //}
+            oSettings.jqXHR = $.ajax({
+                dataType: 'json', type: "GET", url: sSource, data: aoData,
+                "success": function (data) {
+                    var dtOption = { sEcho: data.sEcho, recordsTotal: data.recordsTotal, recordsFiltered: data.recordsFiltered, aaData: JSON.parse(data.aaData) };
+                    return fnCallback(dtOption);
+                }
+            });
+        },
+        aoColumns: [
+
+            { data: 'podatesort', title: 'Date', sWidth: "5%", render: function (inv_num, type, full, meta) { return full.po_date; } },
+            { data: 'po_invoice_number', title: 'Invoice', sWidth: "10%" },
+            { data: 'customer_requisition', title: 'Requisition', sWidth: "10%" },
+            { data: 'poduedatesort', title: 'Due date', sWidth: "10%", render: function (inv_num, type, full, meta) { return full.po_due_date; } },
+            { data: 'po_currency', title: 'Currency', sWidth: "10%" },
+            { data: 'original_invoice_amt', title: 'Original Amount', sWidth: "10%", render: $.fn.dataTable.render.number(',', '.', 2, '') },
+            { data: 'remain_invoice_amt', title: 'Remain Amount', sWidth: "10%", render: $.fn.dataTable.render.number(',', '.', 2, '') },
+        ]
+    });
+}

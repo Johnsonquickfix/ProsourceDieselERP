746f05b2808fcbc7d5e876d35bbca07e83bea5cd
Johnsonquickfix <johnson.quickfix@gmail.com>
2022-03-22 12:27:24 +0530

    Podium Payment update

--- LaylaERP_v1/Controllers/PodiumController.cs
+++ LaylaERP_v1/Controllers/PodiumController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 using LaylaERP.Models;
+using System.Text;
 
 namespace LaylaERP.Controllers
 {
@@ -18,6 +19,7 @@ namespace LaylaERP.Controllers
         {
             try
             {
+                string _DBType = "MYSQL";
                 DataTable dt = OrderRepository.GetPodiumOrdersList();
                 string access_token = clsPodium.GetToken();
                 foreach (DataRow dr in dt.Rows)
@@ -33,22 +35,44 @@ namespace LaylaERP.Controllers
                             {
                                 long id = Convert.ToInt64(dr["id"].ToString());
                                 string str_note = obj.data.customerName;
-                                //OrderPodiumDetailsModel model = new OrderPodiumDetailsModel();
-                                //model.post_id = Convert.ToInt64(dr["id"].ToString());
-                                //model.payment_uid = obj.data.payments[0].uid; model.location_uid = obj.data.location.uid; model.invoice_number = obj.data.invoiceNumber;
-                                //model.order_note = "Payment completed through Podium by " + obj.data.customerName + " on ";
-                                //OrderRepository.UpdatePodiumStatus(model);
-                                string str = "[{ post_id: " + id.ToString() + ", meta_key: '_podium_payment_uid', meta_value: '" + obj.data.payments[0].uid + "' }, { post_id: " + id.ToString() + ", meta_key: '_podium_location_uid', meta_value: '" + obj.data.location.uid + "' },"
+
+                                if (_DBType == "MSSQL")
+                                {
+                                    string str = "[{ post_id: " + id.ToString() + ", meta_key: '_podium_payment_uid', meta_value: '" + obj.data.payments[0].uid + "' }, { post_id: " + id.ToString() + ", meta_key: '_podium_location_uid', meta_value: '" + obj.data.location.uid + "' },"
                                             + "{ post_id: " + id.ToString() + ", meta_key: '_podium_invoice_number', meta_value: '" + obj.data.invoiceNumber + "' }, { post_id: " + id.ToString() + ", meta_key: '_podium_status', meta_value: 'PAID' }]";
 
-                                System.Xml.XmlDocument postsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
-                                System.Xml.XmlDocument order_statsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
-                                System.Xml.XmlDocument postmetaXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
-                                System.Xml.XmlDocument order_itemsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
-                                System.Xml.XmlDocument order_itemmetaXML = JsonConvert.DeserializeXmlNode("{\"Data\":" + str + "}", "Items");
+                                    System.Xml.XmlDocument postsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
+                                    System.Xml.XmlDocument order_statsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
+                                    System.Xml.XmlDocument postmetaXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
+                                    System.Xml.XmlDocument order_itemsXML = JsonConvert.DeserializeXmlNode("{\"Data\":[]}", "Items");
+                                    System.Xml.XmlDocument order_itemmetaXML = JsonConvert.DeserializeXmlNode("{\"Data\":" + str + "}", "Items");
 
-                                OrderRepository.AddOrdersPost(id, "UPP", 0, str_note, postsXML, order_statsXML, postmetaXML, order_itemsXML, order_itemmetaXML);
+                                    OrderRepository.AddOrdersPost(id, "UPP", 0, str_note, postsXML, order_statsXML, postmetaXML, order_itemsXML, order_itemmetaXML);
+                                }
+                                else if (_DBType == "MYSQL")
+                                {
+                                    DateTime cDate = CommonDate.CurrentDate(), cUTFDate = CommonDate.UtcDate();
+                                    string strSql_insert = string.Empty;
+                                    StringBuilder strSql = new StringBuilder(string.Format("update wp_posts set post_modified='{0}',post_modified_gmt='{1}' where id = {2};", cDate.ToString("yyyy/MM/dd HH:mm:ss"), cUTFDate.ToString("yyyy/MM/dd HH:mm:ss"), id));
+                                    strSql_insert += string.Format(" select '{0}' post_id,'{1}' meta_key,'{2}' meta_value", id, "_podium_payment_uid", obj.data.payments[0].uid);
+                                    strSql_insert += string.Format(" union all select '{0}' post_id,'{1}' meta_key,'{2}' meta_value", id, "_podium_location_uid", obj.data.location.uid);
+                                    strSql_insert += string.Format(" union all select '{0}' post_id,'{1}' meta_key,'{2}' meta_value", id, "_podium_invoice_number", obj.data.invoiceNumber);
+                                    strSql_insert += string.Format(" union all select '{0}' post_id,'{1}' meta_key,'{2}' meta_value", id, "_podium_status", "PAID");
 
+                                    strSql.Append(string.Format("update wp_postmeta set meta_value = '{0}' where post_id = '{1}' and meta_key = '{2}'; ", obj.data.payments[0].uid, id, "_podium_payment_uid"));
+                                    strSql.Append(string.Format("update wp_postmeta set meta_value = '{0}' where post_id = '{1}' and meta_key = '{2}'; ", obj.data.location.uid, id, "_podium_location_uid"));
+                                    strSql.Append(string.Format("update wp_postmeta set meta_value = '{0}' where post_id = '{1}' and meta_key = '{2}'; ", obj.data.invoiceNumber, id, "_podium_invoice_number"));
+                                    strSql.Append(string.Format("update wp_postmeta set meta_value = '{0}' where post_id = '{1}' and meta_key = '{2}'; ", "PAID", id, "_podium_status"));
+
+                                    strSql_insert = "insert into wp_postmeta (post_id,meta_key,meta_value) select * from (" + strSql_insert + ") as tmp where tmp.meta_key not in (select meta_key from wp_postmeta where post_id = " + id + ");";
+                                    strSql.Append(strSql_insert);
+
+                                    strSql.Append(string.Format("update wp_posts set post_status = '{0}',post_modified = '{1}', post_modified_gmt = '{2}' where id = {3};", "wc-processing", cDate.ToString("yyyy/MM/dd HH:mm:ss"), cUTFDate.ToString("yyyy/MM/dd HH:mm:ss"), id));
+                                    strSql.Append("insert into wp_comments(comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_date_gmt, comment_content, comment_karma, comment_approved, comment_agent, comment_type, comment_parent, user_id) ");
+                                    strSql.Append(string.Format("values ({0}, 'WooCommerce', 'woocommerce@laylasleep.com', '', '', '{1}', '{2}', '{3}', '0', '1', 'WooCommerce', 'order_note', '0', '0');", id, cDate.ToString("yyyy/MM/dd HH:mm:ss"), cUTFDate.ToString("yyyy/MM/dd HH:mm:ss"), "Payment completed through Podium by " + obj.data.customerName + " on " + cDate.ToString("MM-dd-yyyy HH:mm:ss")));
+
+                                    DAL.MYSQLHelper.ExecuteNonQueryWithTrans(strSql.ToString());
+                                }
                                 if (id > 0)
                                 {
                                     OrderRepository.OrderInvoiceMail(id);

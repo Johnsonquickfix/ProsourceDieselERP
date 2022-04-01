e241ae23d1396eae9293a7a2bbec387d2640827b
david.quickfix1 <david.quickfix1@gmail.com>
2022-04-01 17:02:48 +0530

    affirm model

---
+++ LaylaERP_v1/Models/ImportOrderaffirmModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaylaERP_v1.BAL
+{
+    public class ImportOrderaffirmModel
+    {
+        public string order_id { get; set; }
+        public string transaction_id { get; set; }
+        public string charge_created_date { get; set; }
+        public string event_type { get; set; }
+        public string charge_id { get; set; }        
+        public decimal total_settled { get; set; }
+        public decimal fees { get; set; }
+    }
+}

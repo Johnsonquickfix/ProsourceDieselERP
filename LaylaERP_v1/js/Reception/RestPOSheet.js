58f41ced986edea1ea0cd0f7e7e440e54ab002c1
christison.quickfix <christison.quickfix@gmail.com>
2022-04-29 19:32:36 +0530

    restpo updation

--- LaylaERP_v1/js/Reception/RestPOSheet.js
+++ LaylaERP_v1/js/Reception/RestPOSheet.js
@@ -7,7 +7,7 @@ function FillGrid(is_date) {
     var obj = { strValue1: 'SERCH', strValue2: '', strValue3: '', strValue4: '' }
     var table_EL = $('#dtdata').DataTable({
         //columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': false, 'targets': [9] }], order: [[9, "desc"]],
-        order: [[0, "asc"], [1, "asc"]], ordering: true, destroy: true, bProcessing: true, bServerSide: false, bAutoWidth: false, searching: true, responsive: true, lengthMenu: [[20, 50, 100], [20, 50, 100]],
+        order: [[0, "desc"], [1, "desc"]], ordering: true, destroy: true, bProcessing: true, bServerSide: false, bAutoWidth: false, searching: true, responsive: true, lengthMenu: [[20, 50, 100], [20, 50, 100]],
         language: {
             lengthMenu: "_MENU_ per page", zeroRecords: "Sorry no records found", info: "Showing _START_ to _END_ of _TOTAL_ entries",
             infoFiltered: "", infoEmpty: "No records found", processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
@@ -27,10 +27,17 @@ function FillGrid(is_date) {
 
         aoColumns: [
             /*{ data: 'fk_purchase', title: 'ID', sWidth: "8%" }, */
-            { data: 'ref', title: 'PO', sWidth: "25%", class: 'text-left' },
-            { data: 'POqty', title: 'PO. Qty', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') },
-            { data: 'recqty', title: 'Received Qty', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') },
-            { data: 'balqty', title: 'Balance Qty', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') }
+            {
+                'data': 'ref', sWidth: "25%", title: 'PO No', class: 'text-left',
+                'render': function (id, type, full, meta) {
+                 
+                    return '<a >' + id + '</a> <a href="#" class: "text-left" onclick="getPurchaseOrderPrint(' + full.fk_purchase + ', false);"><i class="fas fa-search-plus"></i></a>';
+                }
+            },
+            //{ data: 'ref', title: 'PO', sWidth: "25%", class: 'text-left' },
+            { data: 'POqty', title: 'PO. Qty .', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') },
+            { data: 'recqty', title: 'Received Qty .', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') },
+            { data: 'balqty', title: 'Balance Qty .', class: 'text-right', sWidth: "25%", render: $.fn.dataTable.render.number('', '.', 0, '') }
 
 
         ],

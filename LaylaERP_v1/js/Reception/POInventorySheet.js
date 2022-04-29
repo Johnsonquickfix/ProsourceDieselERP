6d998a151b73431ddb0e3af6d16e2b0ab4dc242a
christison.quickfix <christison.quickfix@gmail.com>
2022-04-29 17:10:19 +0530

    js update

--- LaylaERP_v1/js/Reception/POInventorySheet.js
+++ LaylaERP_v1/js/Reception/POInventorySheet.js
@@ -7,7 +7,7 @@ function FillGrid(is_date) {
     var obj = { strValue1: 'SERCH', strValue2: '', strValue3: '', strValue4: '' }
     var table_EL = $('#dtdata').DataTable({
         //columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': false, 'targets': [9] }], order: [[9, "desc"]],
-        order: [[0, "asc"], [1, "asc"]], ordering: true, destroy: true, bProcessing: true, bServerSide: false, bAutoWidth: false, searching: true, responsive: true, lengthMenu: [[10, 20, 50], [10, 20, 50]],
+        order: [[0, "asc"], [1, "asc"]], ordering: true, destroy: true, bProcessing: true, bServerSide: false, bAutoWidth: false, searching: true, responsive: true, lengthMenu: [[20, 50, 100], [20, 50, 100]],
         language: {
             lengthMenu: "_MENU_ per page", zeroRecords: "Sorry no records found", info: "Showing _START_ to _END_ of _TOTAL_ entries",
             infoFiltered: "", infoEmpty: "No records found", processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
@@ -29,9 +29,9 @@ function FillGrid(is_date) {
             { data: 'fk_product', title: 'ID', sWidth: "8%" },
             { data: 'product', title: 'Product', sWidth: "40%" },
             { data: 'Sku', title: 'SKU', sWidth: "10%" },
-            { data: 'POqty', title: 'Po. Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '$') },
-            { data: 'recqty', title: 'Received Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '$') },
-            { data: 'balqty', title: 'Balance Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 2, '$') }
+            { data: 'POqty', title: 'Po. Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 0, '') },
+            { data: 'recqty', title: 'Received Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 0, '') },
+            { data: 'balqty', title: 'Balance Qty', class: 'text-right', sWidth: "10%", render: $.fn.dataTable.render.number('', '.', 0, '') }
             
              
         ],
@@ -58,7 +58,7 @@ function FillGrid(is_date) {
                     let d = new Date(); let e = (d.getMonth() + 1) + '-' + d.getDate() + '-' + d.getFullYear();
                     return 'PO_Inventory_Sheet' + e;
                 },
-                messageTop: function () { return '<h3 style = "text-align:center"> Layla Sleep Inc.</h3 ><br /><h3 style="text-align:left">Check Register</h3>'; },
+                messageTop: function () { return '<h3 style = "text-align:center"> Layla Sleep Inc.</h3 ><br /><h3 style="text-align:left">PO Inventory Sheet</h3>'; },
             }
         ],
     });

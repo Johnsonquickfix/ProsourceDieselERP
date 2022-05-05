3572484537d3fc644e3f49c440a1c7d6cfa7b244
David Glimour <david.quickfix1@gmail.com>
2022-05-05 17:12:48 +0530

    changes on product list js

--- LaylaERP_v1/js/Product/ProductList.js
+++ LaylaERP_v1/js/Product/ProductList.js
@@ -260,7 +260,7 @@ function dataGridLoad(order_type) {
             url: '/Product/GetList', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8", data: obj,
             dataSrc: function (data) { return JSON.parse(data); }
         },
-        lengthMenu: [[10, 20, 50, 100], [10, 20, 50, 100]],
+        lengthMenu: [[20, 50, 100], [20, 50, 100]],
         columns: [
             { data: 'p_id', title: 'Parent ID', sWidth: "3%" },
             //{

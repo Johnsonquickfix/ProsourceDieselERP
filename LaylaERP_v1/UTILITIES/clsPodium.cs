17a4615ae9219fad0e85ba5bc88e4f42bd42c5d5
Johnsonquickfix <johnson.quickfix@gmail.com>
2022-05-06 12:38:03 +0530

    Podim invoice cancel

--- LaylaERP_v1/UTILITIES/clsPodium.cs
+++ LaylaERP_v1/UTILITIES/clsPodium.cs
@@ -101,7 +101,7 @@ namespace LaylaERP.UTILITIES
             {
                 if (!string.IsNullOrEmpty(pay_id))
                 {
-                    var client_cancel = new RestClient("https://api.podium.com/v4/invoices" + pay_id + "/cancel");
+                    var client_cancel = new RestClient("https://api.podium.com/v4/invoices/" + pay_id + "/cancel");
                     var request_cancel = new RestRequest(Method.POST);
                     request_cancel.AddHeader("Accept", "application/json");
                     request_cancel.AddHeader("Content-Type", "application/json");
@@ -122,6 +122,55 @@ namespace LaylaERP.UTILITIES
                 if (response_rest.StatusCode == HttpStatusCode.OK)
                 {
                     invoice_info = response_rest.Content;
+                }
+            }
+            return invoice_info;
+        }
+        public static string CancelPodiumInvoice(string pay_id)
+        {
+            string invoice_info = string.Empty;
+            // string client_id = "2f936404-dabc-4cf7-a61b-80e6bef42f66", client_secret = "fd4b08be31c507860517f1f411b216d2beea25076348fe4fb0311073080e94df", refresh_token = "703ddfb54f8f811bdfdc77356a6a9d66168ce97865ab029557dbbf7e15ded85f";
+            string client_id = string.Empty, client_secret = string.Empty, refresh_token = string.Empty;
+            System.Data.DataTable dt = BAL.Users.AppSystemSetting();
+            if (dt.Rows.Count > 0)
+            {
+                client_id = (dt.Rows[0]["podiumAPIKey"] != Convert.DBNull) ? dt.Rows[0]["podiumAPIKey"].ToString().Trim() : string.Empty;
+                client_secret = (dt.Rows[0]["podiumSecretKey"] != Convert.DBNull) ? dt.Rows[0]["podiumSecretKey"].ToString().Trim() : string.Empty;
+                refresh_token = (dt.Rows[0]["podium_refresh_code"] != Convert.DBNull) ? dt.Rows[0]["podium_refresh_code"].ToString().Trim() : string.Empty;
+            }
+
+            List<KeyValuePair<string, string>> tokenServerPairs = new List<KeyValuePair<string, string>>();
+            tokenServerPairs.Add(new KeyValuePair<string, string>("client_id", client_id));
+            tokenServerPairs.Add(new KeyValuePair<string, string>("client_secret", client_secret));
+            tokenServerPairs.Add(new KeyValuePair<string, string>("refresh_token", refresh_token));
+            tokenServerPairs.Add(new KeyValuePair<string, string>("grant_type", "refresh_token"));
+            var content = new FormUrlEncodedContent(tokenServerPairs);
+            clsPodiumAccessToken result = new clsPodiumAccessToken();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://accounts.podium.com/oauth/token");
+            client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en_US"));
+
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            var response = client.PostAsync("", content).Result;
+
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                result = JsonConvert.DeserializeObject<clsPodiumAccessToken>(response.Content.ReadAsStringAsync().Result);
+            }
+            else { result.access_token = string.Empty; }
+
+            if (!string.IsNullOrEmpty(result.access_token))
+            {
+                var client_cancel = new RestClient("https://api.podium.com/v4/invoices/" + pay_id + "/cancel");
+                var request_cancel = new RestRequest(Method.POST);
+                request_cancel.AddHeader("Accept", "application/json");
+                request_cancel.AddHeader("Content-Type", "application/json");
+                request_cancel.AddHeader("Authorization", "Bearer " + result.access_token);
+                request_cancel.AddParameter("application/json", "{\"locationUid\":\"" + locationUid + "\",\"note\":\"Invoice has been canceled.\"}", ParameterType.RequestBody);
+                IRestResponse response_cancel = client_cancel.Execute(request_cancel);
+                if (response_cancel.StatusCode == HttpStatusCode.OK)
+                {
+                    invoice_info = response_cancel.Content;
                 }
             }
             return invoice_info;

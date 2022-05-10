580cc707be89251693818234da7d995a0e62fb5f
Johnsonquickfix <johnson.quickfix@gmail.com>
2022-05-10 18:19:47 +0530

    Auto Bill Configuration

---
+++ LaylaERP_v1/BAL/MiscellaneousBillRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace LaylaERP.BAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class MiscellaneousBillRepository
+    {
+    }
+}
---
+++ LaylaERP_v1/Controllers/MiscellaneousBillController.cs
@@ -0,0 +1,45 @@
+﻿namespace LaylaERP.Controllers
+{
+    using LaylaERP.BAL;
+    using LaylaERP.Models;
+    using LaylaERP.UTILITIES;
+    using Newtonsoft.Json;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+
+    public class MiscellaneousBillController : Controller
+    {
+        public ActionResult AutoBillConfigList()
+        {
+            return View();
+        }
+
+        // GET: Miscellaneous Auto Bill Configuration
+        public ActionResult AutoBillConfig()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("miscellaneousbill/autobillconfig-save")]
+        public JsonResult AutoBillConfigSave(SearchModel model)
+        {
+            string JSONresult = string.Empty;
+            try
+            {
+                long id = 0, userid = CommanUtilities.Provider.GetCurrent().UserID;
+                if (!string.IsNullOrEmpty(model.strValue1)) id = Convert.ToInt64(model.strValue1);
+                System.Xml.XmlDocument orderXML = JsonConvert.DeserializeXmlNode("{\"Data\":" + model.strValue2 + "}", "Items");
+                System.Xml.XmlDocument orderdetailsXML = JsonConvert.DeserializeXmlNode("{\"Data\":" + model.strValue3 + "}", "Items");
+                JSONresult = JsonConvert.SerializeObject(MiscellaneousBillRepository.AutoBillConfigSave(id, userid, "I", orderXML, orderdetailsXML));
+
+            }
+            catch { }
+            return Json(JSONresult, JsonRequestBehavior.AllowGet);
+        }
+    }
+}

d06caffecb71791bbffcb7f2198cb07f3de3743f
Johnsonquickfix <johnson.quickfix@gmail.com>
2022-05-17 11:27:17 +0530

    Shipped Track update

--- LaylaERP_v1/Controllers/PaymentSettlementController.cs
+++ LaylaERP_v1/Controllers/PaymentSettlementController.cs
@@ -264,5 +264,32 @@ namespace LaylaERP.Controllers
             catch (Exception ex) { return Json(new { status = ex.Message }, 0); }
             return Json(new { status = derivedCode }, 0);
         }
+
+        // GET: OrderUnpaidList
+        public ActionResult OrderUnpaidList()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("paymentsettlement/unsettled-transactions")]
+        public JsonResult GetUnsettledOrders(JqDataTableModel model)
+        {
+            string result = string.Empty;
+            int TotalRecord = 0;
+            try
+            {
+                DateTime? fromdate = null, todate = null;
+                if (!string.IsNullOrEmpty(model.strValue1))
+                    fromdate = Convert.ToDateTime(model.strValue1);
+                if (!string.IsNullOrEmpty(model.strValue2))
+                    todate = Convert.ToDateTime(model.strValue2);
+
+                //DataTable dt = OrderRepository.OrderList(model.strValue1, model.strValue2, model.strValue3, model.sSearch, model.iDisplayStart, model.iDisplayLength, out TotalRecord, model.sSortColName, model.sSortDir_0);
+                DataTable dt = OrderRepository.AuthorizeNetOrderList(fromdate, todate, "amazon_payments_advanced", model.sSearch, model.iDisplayStart, model.iDisplayLength, out TotalRecord, model.sSortColName, model.sSortDir_0);
+                result = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            }
+            catch { }
+            return Json(new { sEcho = model.sEcho, recordsTotal = TotalRecord, recordsFiltered = TotalRecord, aaData = result }, 0);
+        }
     }
 }

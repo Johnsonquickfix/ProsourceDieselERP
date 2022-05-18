4a49651e70d4f2a275f184ddb263ba34940c8601
David Glimour <david.quickfix1@gmail.com>
2022-05-18 16:36:34 +0530

    change on chart of accounts.

--- LaylaERP_v1/js/Accounting/ChartOfAccounts.js
+++ LaylaERP_v1/js/Accounting/ChartOfAccounts.js
@@ -24,8 +24,7 @@ function ChartOfAccountGrid() {
                     { data: 'label', title: 'Name', sWidth: "16%" },
                     { data: 'pcg_type', title: 'Type', sWidth: "10%" },
                     { data: 'labelshort', title: 'Detail Type', sWidth: "10%" },
-                    { data: 'ac_type', title: 'Account tag', sWidth: "10%" },
-                    { data: 'extraparams', title: 'Chart Tag', sWidth: "10%" },
+                    { data: 'ac_type', title: 'Account tag', sWidth: "10%" }, /*{ data: 'extraparams', title: 'Chart Tag', sWidth: "10%" },*/
                     { data: 'bs_type', title: 'BS Tag', sWidth: "6%" },
                     { data: 'account_category', title: 'Account category', sWidth: "6%" },
                     {

360d8f66099e65ebcfbb117428d3fcd6e66b8671
David Glimour <david.quickfix1@gmail.com>
2022-05-18 17:10:40 +0530

    changes

--- LaylaERP_v1/js/Accounting/ChartOfAccounts.js
+++ LaylaERP_v1/js/Accounting/ChartOfAccounts.js
@@ -23,10 +23,10 @@ function ChartOfAccountGrid() {
                     { data: 'account_number', title: 'Account Number', sWidth: "10%", sType: 'numeric' },
                     { data: 'label', title: 'Name', sWidth: "16%" },
                     { data: 'pcg_type', title: 'Type', sWidth: "10%" },
-                    { data: 'labelshort', title: 'Detail Type', sWidth: "18%" },
-                    { data: 'ac_type', title: 'Account tag', sWidth: "18%" }, /*{ data: 'extraparams', title: 'Chart Tag', sWidth: "10%" },*/
+                    { data: 'labelshort', title: 'Detail Type', sWidth: "15%" },
+                    { data: 'ac_type', title: 'Account tag', sWidth: "15%" }, /*{ data: 'extraparams', title: 'Chart Tag', sWidth: "10%" },*/
                     /*{ data: 'bs_type', title: 'BS Tag', sWidth: "6%" },*/
-                    { data: 'account_category', title: 'Account category', sWidth: "6%" },
+                    { data: 'account_category', title: 'Account category', sWidth: "12%" },
                     {
                         'data': 'active', sWidth: "4%", 'sClass': 'ws_nowrap text-center',
                         'render': function (id, type, full, meta) {

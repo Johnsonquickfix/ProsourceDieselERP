b244b25744c8e736299dfebb6b9c476e7eb7ceb6
johnson <johnson.quickfix@gmail.com>
2022-06-08 13:38:26 +0530

    Bank Accounts Entry update

--- LaylaERP_v1/js/Bank/BankEntries.js
+++ LaylaERP_v1/js/Bank/BankEntries.js
@@ -9,113 +9,90 @@
             'This Month': [moment().startOf('month'), moment().endOf('month')],
             'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
         },
-        startDate: moment().subtract(1, 'month'), autoUpdateInput: true, alwaysShowCalendars: true,
+        startDate: moment().startOf('year'), autoUpdateInput: true, alwaysShowCalendars: true,
         locale: { format: 'MM/DD/YYYY', cancelLabel: 'Clear' }, opens: 'right', orientation: "left auto"
     }, function (start, end, label) {
-        BankEntriesList(true);
+        BankEntriesList();
         FundTransferList(true);
     });
-    BankEntriesList(false);
+    BankEntriesList();
     setTimeout(function () { FundTransferList(true) }, 2000);//product dropdown
-    PendingEntriesBalance();
+    
 });
-
-function BankEntriesList(is_date) {
-    var urid = $("#ddlVendor").val();
-
+function BankEntriesList() {
     let sd = $('#txtOrderDate').data('daterangepicker').startDate.format('YYYY-MM-DD');
     let ed = $('#txtOrderDate').data('daterangepicker').endDate.format('YYYY-MM-DD');
-    let dfa = is_date ? "'" + sd + "' and '" + ed + "'" : '';
-
-    var ID = $("#hfid").val();
-    var table_EL = $('#EmployeeListdata').DataTable({
-        columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': true, 'targets': [0] }], order: [[0, "desc"]],
+    let id = $("#hfid").val();
+    var table_EL = $('#tbBankEntry').DataTable({
+        //columnDefs: [{ "orderable": true, "targets": 1 }, { 'visible': true, 'targets': [0] }], order: [[0, "desc"]],
         destroy: true, bProcessing: true, bServerSide: true, bAutoWidth: false, searching: true,
         responsive: true, lengthMenu: [[10, 20, 50], [10, 20, 50]],
         language: {
-            lengthMenu: "_MENU_ per page",
-            zeroRecords: "Sorry no records found",
-            info: "Showing _START_ to _END_ of _TOTAL_ entries",
-            infoFiltered: "",
-            infoEmpty: "No records found",
-            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
+            lengthMenu: "_MENU_ per page", zeroRecords: "Sorry no records found",
+            info: "Showing _START_ to _END_ of _TOTAL_ entries", infoFiltered: "",
+            infoEmpty: "No records found", processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>'
         },
         initComplete: function () {
-            $('#EmployeeListdata_filter input').unbind();
-            $('#EmployeeListdata_filter input').bind('keyup', function (e) {
+            $('#tbBankEntry_filter input').unbind();
+            $('#tbBankEntry_filter input').bind('keyup', function (e) {
                 var code = e.keyCode || e.which;
                 if (code == 13) { table_EL.search(this.value).draw(); }
             });
         },
-        footerCallback: function (row, data, start, end, display) {
-            var api = this.api(), data;
-            console.log(data);
-            var intVal = function (i) {
-                return typeof i === 'string' ?
-                    i.replace(/[\$,]/g, '') * 1 :
-                    typeof i === 'number' ?
-                        i : 0;
-            };
+        //footerCallback: function (row, data, start, end, display) {
+        //    var api = this.api(), data;
+        //    console.log(data);
+        //    var intVal = function (i) { return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0; };
 
-            var DebitTotal = api.column(7).data().reduce(function (a, b) {
-                return intVal(a) + intVal(b);
-            }, 0);
+        //    var DebitTotal = api.column(7).data().reduce(function (a, b) { return intVal(a) + intVal(b); }, 0);
 
-            var CreditTotal = api.column(8).data().reduce(function (a, b) {
-                return intVal(a) + intVal(b);
-            }, 0);
+        //    var CreditTotal = api.column(8).data().reduce(function (a, b) { return intVal(a) + intVal(b); }, 0);
 
-            $(api.column(0).footer()).html('Page Total');
-            $(api.column(7).footer()).html('$' + parseFloat(DebitTotal).toFixed(2));
-            $(api.column(8).footer()).html('$' + parseFloat(CreditTotal).toFixed(2));
-            console.log(DebitTotal);
-            console.log(CreditTotal);
-        },
+        //    $(api.column(0).footer()).html('Page Total');
+        //    $(api.column(7).footer()).html('$' + parseFloat(DebitTotal).toFixed(2));
+        //    $(api.column(8).footer()).html('$' + parseFloat(CreditTotal).toFixed(2));
+        //    console.log(DebitTotal);
+        //    console.log(CreditTotal);
+        //},
 
-        sAjaxSource: "/Bank/BankEntriesList",
+        sAjaxSource: "/bank/bank-transactions",
         fnServerData: function (sSource, aoData, fnCallback, oSettings) {
-            aoData.push({ name: "strValue1", value: urid });
-            aoData.push({ name: "strValue2", value: ID });
-            aoData.push({ name: "strValue3", value: dfa });
-            var col = 'id';
-            if (oSettings.aaSorting.length >= 0) {
-                var col = oSettings.aaSorting[0][0] == 0 ? "id" : oSettings.aaSorting[0][0] == 1 ? "operation_date" : oSettings.aaSorting[0][0] == 2 ? "value_date" : oSettings.aaSorting[0][0] == 3 ? "paymenttype" : oSettings.aaSorting[0][0] == 4 ? "num_payment" : oSettings.aaSorting[0][0] == 5 ? "vendor" : oSettings.aaSorting[0][0] == 6 ? "bankaccount" : oSettings.aaSorting[0][0] == 7 ? "debit" : oSettings.aaSorting[0][0] == 8 ? "credit" : "id";
-                aoData.push({ name: "sSortColName", value: col });
-            }
+            aoData.push({ name: "strValue1", value: id });
+            aoData.push({ name: "strValue2", value: sd });
+            aoData.push({ name: "strValue3", value: ed });
+            if (oSettings.aaSorting.length > 0) { aoData.push({ name: "sSortColName", value: oSettings.aoColumns[oSettings.aaSorting[0][0]].data }); }
             oSettings.jqXHR = $.ajax({
                 dataType: 'json', type: "GET", url: sSource, data: aoData,
-                "success": function (data) {
+                success: function (data) {
+                    console.log(data);
                     var dtOption = { sEcho: data.sEcho, recordsTotal: data.recordsTotal, recordsFiltered: data.recordsFiltered, aaData: JSON.parse(data.aaData) };
+                    $("#txtinitialamount").text(CurrencyFormat(data.opening_balance)); $("#txtbalance").text(CurrencyFormat(data.closing_balance)); $("#txtunclearbalance").text(CurrencyFormat(data.uncleared_balance));
                     return fnCallback(dtOption);
                 }
             });
         },
         aoColumns: [
-            { data: 'id', title: 'Ref', sWidth: "10%" },
-            { data: 'operation_date', title: 'Operation Date', sWidth: "10%" },
-            { data: 'value_date', title: 'Value Date', sWidth: "10%" },
-            { data: 'paymenttype', title: 'Payment Type', sWidth: "10%" },
-            { data: 'num_payment', title: 'Number', sWidth: "10%" },
-            { data: 'vendor', title: 'Vendor Name', sWidth: "10%" },
-            { data: 'fk_invoceso', title: 'Invoice', sWidth: "10%" },
-            { data: 'debit', title: 'Debit', sWidth: "10%", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
-            { data: 'credit', title: 'Credit', sWidth: "10%", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
-            { data: 'balance', title: 'Balance', sWidth: "10%", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
-
+            { data: 'date', title: 'Date', sWidth: "15%" },
+            { data: 'doc_type', title: 'Transaction Type', sWidth: "15%" },
+            { data: 'subledger_label', title: 'Name', sWidth: "15%" },
+            { data: 'label_operation', title: 'Memo/Description', sWidth: "20%" },
+            { data: 'debit', title: 'Debit', sWidth: "15%", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
+            { data: 'credit', title: 'Credit', sWidth: "15%", render: $.fn.dataTable.render.number(',', '.', 2, '$') },
+            { data: 'balance', title: 'Balance', sWidth: "15%", render: $.fn.dataTable.render.number(',', '.', 2, '$') }
         ],
-        "dom": 'lBftipr',
-        "buttons": [
-            {
-                extend: 'csv',
-                className: 'button',
-                text: '<i class="fas fa-file-csv"></i> CSV',
-                filename: function () {
-                    var d = new Date();
-                    var e = (d.getMonth() + 1) + '-' + d.getDate() + '-' + d.getFullYear();
-                    return 'Bank_Entries' + e;
-                },
-            },
-        ],
+        //"dom": 'lBftipr',
+        //"buttons": [
+        //    {
+        //        extend: 'csv',
+        //        className: 'button',
+        //        text: '<i class="fas fa-file-csv"></i> CSV',
+        //        filename: function () {
+        //            var d = new Date();
+        //            var e = (d.getMonth() + 1) + '-' + d.getDate() + '-' + d.getFullYear();
+        //            return 'Bank_Entries' + e;
+        //        },
+        //    },
+        //],
     });
 }
 
@@ -134,7 +111,7 @@ function EntriesBalanceForSpecificBank() {
             if (d.length > 0) {
                 $("#txtentriesdebit").text('$' + parseFloat(d[0].debit).toFixed(2));
                 $("#txtentriescredit").text('$' + parseFloat(d[0].credit).toFixed(2));
-                $("#txtbalance").text('$' + (d[0].balance));
+                //$("#txtbalance").text('$' + (d[0].balance));
             }
         },
         error: function (msg) {
@@ -401,7 +378,7 @@ function Bankfundtransfer() {
             success: function (data) {
                 swal('Success!', data.message, 'success');
                 FundTransferList(true);
-                BankEntriesList(false);
+                BankEntriesList();
             },
             error: function (msg) {
 

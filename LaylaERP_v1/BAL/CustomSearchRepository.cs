eaa8a06b793983fbfe36b70929972a9f72e0ee74
johnson <johnson.quickfix@gmail.com>
2022-08-09 17:24:43 +0530

    Journals Custom Report update

--- LaylaERP_v1/BAL/CustomSearchRepository.cs
+++ LaylaERP_v1/BAL/CustomSearchRepository.cs
@@ -202,5 +202,87 @@ namespace LaylaERP.BAL
             { throw ex; }
             return dt;
         }
+
+        public static DataTable GetJournalsList(CustomSearchModel model)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = string.Empty, _select = string.Empty, _joins = string.Empty, _where = string.Empty;
+                foreach (CustomDisplayFieldModel o in model.display_field)
+                {
+                    if (o.strType.ToLower() == "erp_accounting_bookkeeping")
+                    {
+                        _select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("{0} as [{1}]", o.strValue, o.strKey);
+                    }
+                    else if (o.strType.ToLower() == "erp_accounting_account")
+                    {
+                        _select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("account.{0} as [{1}]", o.strValue, o.strKey);
+                    }
+                    else if (o.strType.ToLower() == "erp_account_process_label")
+                    {
+                        _select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("tran_type.{0} as [{1}]", o.strValue, o.strKey);
+                    }
+                }
+                string _tb = string.Empty;
+                foreach (CustomWhereFieldModel o in model.where_field)
+                {
+                    switch (o.strOperator.ToLower())
+                    {
+                        case "erp_accounting_bookkeeping":
+                            _tb = "bookkeeping";
+                            break;
+                        case "erp_accounting_account":
+                            _tb = "account";
+                            break;
+                        case "tran_type":
+                            _tb = "account";
+                            break;
+                    }
+                    switch (o.strOperator.ToLower())
+                    {
+                        case "in":
+                            _where += string.Format(" AND {0}.{1} in ({2}) ", _tb, o.strKey, o.strValue);
+                            break;
+                        case "equal to":
+                            _where += string.Format(" AND {0}.{1} = '{2}' ", _tb, o.strKey, o.strValue);
+                            break;
+                        case "start with":
+                            _where += string.Format(" AND {0}.{1} like '{2}%' ", _tb, o.strKey, o.strValue);
+                            break;
+                        case "end with":
+                            _where += string.Format(" AND {0}.{1} like '%{2}' ", _tb, o.strKey, o.strValue);
+                            break;
+                        case "any where":
+                            _where += string.Format(" AND {0}.{1} like '%{2}%' ", _tb, o.strKey, o.strValue);
+                            break;
+                    }
+                }
+                _joins = " inner join erp_accounting_account account on account.account_number = bookkeeping.subledger_account left outer join erp_account_process_label tran_type on tran_type.label_code = bookkeeping.doc_type";
+
+                sql = "SELECT COUNT(*) OVER() total_count," + _select + " FROM erp_accounting_bookkeeping AS bookkeeping" + _joins;
+                sql += " WHERE 1 = 1";
+                if (!string.IsNullOrEmpty(model.order_status))
+                {
+                    sql += " AND bookkeeping.doc_type in ( " + model.order_status + " )";
+                }
+                if (model.start_date.HasValue)
+                {
+                    sql += " AND bookkeeping.doc_date >= '" + model.start_date.Value.ToString("yyyy-MM-dd HH:mm:ss") + "' ";
+                }
+                if (model.end_date.HasValue)
+                {
+                    sql += " AND bookkeeping.doc_date < '" + model.end_date.Value.AddDays(1).ToString("yyyy-MM-dd HH:mm:ss") + "' ";
+                }
+
+                sql += _where + " order by " + model.sSortColName + " " + model.sSortDir_0 + " OFFSET " + model.iDisplayStart + " ROWS FETCH NEXT " + model.iDisplayLength + " ROWS ONLY;";
+
+                SqlParameter[] parameters = { };
+                dt = SQLHelper.ExecuteDataTable(sql, parameters);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            return dt;
+        }
     }
 }
--- LaylaERP_v1/Controllers/CustomSearchController.cs
+++ LaylaERP_v1/Controllers/CustomSearchController.cs
@@ -30,6 +30,7 @@ namespace LaylaERP.Controllers
                 string flag = "MASTER";
                 if (model.strValue1 == "ORDER") flag = "ORDERMASTER";
                 else if (model.strValue1 == "QUOTE") flag = "QUOTEMASTER";
+                else if (model.strValue1 == "JOURNAL") flag = "JOURNALMASTER";
                 result = JsonConvert.SerializeObject(CustomSearchRepository.GetFilterMasters(flag), Formatting.Indented);
             }
             catch { }
@@ -100,6 +101,53 @@ namespace LaylaERP.Controllers
             {
                 DataTable dt = CustomSearchRepository.GetQuoteOrderList(model);
                 dt.TableName = "QuoteOrder_List";
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    dt.Columns.Remove("total_count");
+                    //Add DataTable in worksheet  
+                    //wb.Worksheets.Add(dt);
+                    //var ws = wb.Worksheets.Add("Orders");
+                    var ws = wb.Worksheets.Add(dt);
+                    ws.Columns().AdjustToContents();  // Adjust column width
+                    ws.Rows().AdjustToContents();     // Adjust row heights
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+                        return File(stream.ToArray(), "application/ms-excel", fileName);
+                    }
+                }
+                //result = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            }
+            catch (Exception ex) { throw ex; }
+            //return Json(robj, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+
+        #region [Account Journals custom reports]
+        [Route("customsearch/journalscustomreport")]
+        public ActionResult JournalsCustomReport()
+        {
+            return View();
+        }
+        [HttpPost, Route("customsearch/journals-list")]
+        public JsonResult GetJournalsList(CustomSearchModel model)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = JsonConvert.SerializeObject(CustomSearchRepository.GetJournalsList(model), Formatting.Indented);
+            }
+            catch { }
+            return Json(result, 0);
+        }
+        [HttpPost, Route("customsearch/journals-list-export")]
+        public ActionResult ExportQuotejournalsList(CustomSearchModel model)
+        {
+            string fileName = String.Format("Journal_{0}.xlsx", DateTime.Now.ToString("dd_MMMM_yyyy_hh_mm_tt"));
+            try
+            {
+                DataTable dt = CustomSearchRepository.GetJournalsList(model);
+                dt.TableName = "Journal";
                 using (XLWorkbook wb = new XLWorkbook())
                 {
                     dt.Columns.Remove("total_count");

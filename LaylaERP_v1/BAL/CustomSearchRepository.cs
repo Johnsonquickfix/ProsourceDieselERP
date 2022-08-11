a9720ae3db70f168e741b1f7063b14e7fcfbabbd
johnson <johnson.quickfix@gmail.com>
2022-08-11 13:42:11 +0530

    Product custom reports

--- LaylaERP_v1/BAL/CustomSearchRepository.cs
+++ LaylaERP_v1/BAL/CustomSearchRepository.cs
@@ -284,5 +284,100 @@ namespace LaylaERP.BAL
             { throw ex; }
             return dt;
         }
+
+        public static DataTable GetProductList(CustomSearchModel model)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = string.Empty, _select = string.Empty, _joins = string.Empty, _where = string.Empty;
+                foreach (CustomDisplayFieldModel o in model.display_field)
+                {
+                    if (o.strType.ToLower() == "wp_posts")
+                    {
+                        _select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("{0} as [{1}]", o.strValue, o.strKey);
+                    }
+                    else if (o.strType.ToLower() == "wp_postmeta")
+                    {
+                        _select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("meta_{0}.meta_value as [{1}]", o.strValue, o.strKey);
+                        _joins += string.Format(" INNER JOIN wp_postmeta AS meta_{0} ON ( posts.ID = meta_{0}.post_id AND meta_{0}.meta_key = '{0}' )", o.strValue);
+                    }
+                }
+
+                foreach (CustomWhereFieldModel o in model.where_field)
+                {
+                    if (o.strType.ToLower() == "wp_posts")
+                    {
+                        switch (o.strOperator.ToLower())
+                        {
+                            case "in":
+                                _where += string.Format(" AND posts.{0} in ({1}) ", o.strKey, o.strValue);
+                                break;
+                            case "equal to":
+                                _where += string.Format(" AND posts.{0} = '{1}' ", o.strKey, o.strValue);
+                                break;
+                            case "start with":
+                                _where += string.Format(" AND posts.{0} like '{1}%' ", o.strKey, o.strValue);
+                                break;
+                            case "end with":
+                                _where += string.Format(" AND posts.{0} like '%{1}' ", o.strKey, o.strValue);
+                                break;
+                            case "any where":
+                                _where += string.Format(" AND posts.{0} like '%{1}%' ", o.strKey, o.strValue);
+                                break;
+                        }
+                    }
+                    else if (o.strType.ToLower() == "wp_postmeta")
+                    {
+                        switch (o.strOperator.ToLower())
+                        {
+                            case "equal to":
+                                _where += string.Format(" AND meta_{0}.meta_value = '{1}' ", o.strKey, o.strValue);
+                                break;
+                            case "start with":
+                                _where += string.Format(" AND meta_{0}.meta_value like '{1}%' ", o.strKey, o.strValue);
+                                break;
+                            case "end with":
+                                _where += string.Format(" AND meta_{0}.meta_value like '%{1}' ", o.strKey, o.strValue);
+                                break;
+                            case "any where":
+                                _where += string.Format(" AND meta_{0}.meta_value like '%{1}%' ", o.strKey, o.strValue);
+                                break;
+                        }
+                        if (model.display_field.FindIndex(item => o.strKey.ToLower() == item.strValue.ToLower()) < 0)
+                            _joins += string.Format(" INNER JOIN wp_postmeta AS meta_{0} ON ( posts.ID = meta_{0}.post_id AND meta_{0}.meta_key = '{0}' )", o.strKey);
+                        //_select += (!string.IsNullOrEmpty(_select) ? ", " : "") + string.Format("meta_{0}.meta_value as [{1}]", o.strValue, o.strKey);
+                        //_joins += string.Format(" INNER JOIN wp_postmeta AS meta_{0} ON ( posts.ID = meta_{0}.post_id AND meta_{0}.meta_key = '{0}' )", o.strValue);
+                    }
+                }
+
+                sql = "SELECT COUNT(*) OVER() total_count," + _select + " FROM wp_posts AS posts" + _joins;
+                sql += " WHERE posts.post_type IN ( 'product','product_variation' ) AND posts.post_status != 'draft'";
+                if (!string.IsNullOrEmpty(model.order_status))
+                {
+                    sql += " AND posts.post_status in ( " + model.order_status + " )";
+                }
+                if (!string.IsNullOrEmpty(model.order_types))
+                {
+                    sql += " AND (case when posts.post_parent = 0 then posts.id else posts.post_parent end) in (select object_id from wp_term_relationships ttr where ttr.term_taxonomy_id in (" + model.order_types + "))";
+                }
+                if (model.start_date.HasValue)
+                {
+                    sql += " AND posts.post_date >= '" + model.start_date.Value.ToString("yyyy-MM-dd HH:mm:ss") + "' ";
+                }
+                if (model.end_date.HasValue)
+                {
+                    sql += " AND posts.post_date < '" + model.end_date.Value.AddDays(1).ToString("yyyy-MM-dd HH:mm:ss") + "' ";
+                }
+
+                sql += _where + " order by " + model.sSortColName + " " + model.sSortDir_0 + " OFFSET " + model.iDisplayStart + " ROWS FETCH NEXT " + model.iDisplayLength + " ROWS ONLY;";
+
+                SqlParameter[] parameters = { };
+                dt = SQLHelper.ExecuteDataTable(sql, parameters);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            return dt;
+        }
     }
 }

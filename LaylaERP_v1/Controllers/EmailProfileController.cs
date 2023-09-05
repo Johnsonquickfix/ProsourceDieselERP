f888cc526cc1cd4602877fe7d81a559158fb6888
David Glimour <david.quickfix1@gmail.com>
2023-09-05 20:50:49 +0530

    change emailprofile

---
+++ LaylaERP_v1/Controllers/EmailProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaylaERP_v1.Controllers
+{
+    public class EmailProfileController : Controller
+    {
+        // GET: EmailProfile
+        public ActionResult Inboxprofile()
+        {
+            return View();
+        }
+        public ActionResult Sentprofile()
+        {
+            return View();
+        }
+        public ActionResult Draftprofile()
+        {
+            return View();
+        }
+    }
+}

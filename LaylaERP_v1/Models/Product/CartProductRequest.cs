3933cdaa74cc4cdb045cd1851a4aeb2b6495a8d2
johnson <johnson.quickfix@gmail.com>
2023-09-13 18:53:02 +0530

    Update API

---
+++ LaylaERP_v1/Models/Product/CartProductRequest.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace LaylaERP_v1.Models.Product
+{
+    public class CartProductRequest
+    {
+        [JsonProperty("id")]
+        public long id { get; set; }
+        [JsonProperty("variation_id")]
+        public long variation_id { get; set; }
+        [JsonProperty("quantity")]
+        public int quantity { get; set; }
+    }
+}

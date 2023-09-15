b6ce0d1a99a516259a788365439e11aeeb94bcd2
johnson <johnson.quickfix@gmail.com>
2023-09-15 15:20:55 +0530

    Update product and order sync

--- LaylaERP_v1/Controllers/DataImportController.cs
+++ LaylaERP_v1/Controllers/DataImportController.cs
@@ -28,7 +28,8 @@ namespace LaylaERP.Controllers
                 using (var client = new HttpClient())
                 {
                     //client.BaseAddress = new Uri("https://quickfixtest2.com/exportdata.php");
-                    client.BaseAddress = new Uri("https://prosourcediesel.com/proexportdataorders.php");
+                    //client.BaseAddress = new Uri("https://prosourcediesel.com/proexportdataorders.php");
+                    client.BaseAddress = new Uri("https://new.prosourcediesel.com/proexportdataorders.php");
                     client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en_US"));
 
                     ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;

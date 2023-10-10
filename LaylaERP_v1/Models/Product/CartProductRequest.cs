d32f1717a6fa66795c8cad20ddf1361d616dad6c
johnson <johnson.quickfix@gmail.com>
2023-10-10 16:44:28 +0530

    Update Cart APIs

--- LaylaERP_v1/Models/Product/CartProductRequest.cs
+++ LaylaERP_v1/Models/Product/CartProductRequest.cs
@@ -38,6 +38,7 @@ namespace LaylaERP_v1.Models.Product
             public string brand { get; set; }
             public string name { get; set; }
             public string sku { get; set; }
+            public string stock_status { get; set; }
             public bool? is_category { get; set; }
             public List<long> categories { get; set; }
             public decimal? price { get; set; }

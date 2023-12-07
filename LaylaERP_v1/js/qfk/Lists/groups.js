365f77d28a363a32151794bdb887693a3c8931b8
johnson <johnson.quickfix@gmail.com>
2023-12-07 20:13:07 +0530

    Update Segment List page and members

--- LaylaERP_v1/js/qfk/Lists/groups.js
+++ LaylaERP_v1/js/qfk/Lists/groups.js
@@ -1,6 +1,12 @@
 ﻿import Http from '../../http/index.js';
 import Cache from '../../http/cache.js';
 
+function getCriteria() { return Http.get('/api/lists/criteria/type').then(response => response.json()); }
+function getStatistic() { return Http.get('/api/lists/metrics').then(response => response.json()); }
+function getProperties() { return Http.get('/api/lists/people/property').then(response => response.json()); }
+function getGroups() { return Http.get('/api/lists/static-group').then(response => response.json()); }
+function getCountry() { return Http.get(`/api/lists/countries`).then(response => response.json()); }
+const criteriaData = new Cache(getCriteria), statisticData = new Cache(getStatistic), propertyData = new Cache(getProperties), groupData = new Cache(getGroups), countryData = new Cache(getCountry);
 
 !(function () {
     $(document).ready(function () {
@@ -42,25 +48,23 @@ import Cache from '../../http/cache.js';
             let row = $('#datatable').DataTable().row(tr);
             if (row.child.isShown()) {
                 // This row is already open - close it
-                tr.find('.details-control').empty().append('<i class="fas fa-angle-right"></i> View definition');
+                tr.find('.details-control').empty().append('<i class="fas fa-angle-right fa-2x"></i>');
                 row.child.hide();
                 tr.removeClass('shown');
             } else {
                 // Open this row
-                tr.find('.details-control').empty().append('<i class="fas fa-angle-down"></i> View definition');
+                tr.find('.details-control').empty().append('<i class="fas fa-angle-down fa-2x"></i>');
                 row.child(format(row.data())).show();
                 tr.addClass('shown');
             }
         });
     });
-
-    
 })();
 
 var $tb = $('#datatable'), bm = 'modalEdit',
     loaddata = function () {
         $tb.DataTable({
-            lengthMenu: [[25, 50, 100], [25, 50, 100]], order: [[2, "desc"]],
+            lengthMenu: [[25, 50, 100], [25, 50, 100]], order: [[4, "desc"]],
             destroy: true, bProcessing: true, responsive: false, bServerSide: true, bAutoWidth: true, scrollX: false,
             language: {
                 lengthMenu: "_MENU_ per page", zeroRecords: "Sorry no records found", info: "Showing <b>_START_ to _END_</b> of _TOTAL_",
@@ -79,14 +83,19 @@ var $tb = $('#datatable'), bm = 'modalEdit',
             },
             columns: [
                 {
-                    data: 'name', title: 'List', sWidth: "60%", orderable: false, render: function (data, type, row, meta) {
-                        return `<div class="mb-2"><a data-add="false" data-url='/lists/listmembers/${row.group_id}' href="/list/${row.group_id}/members">${data}</a> <span class="bg-primary bg-soft text-primary badge font-size-12 me-2"><i class="fas ${row.group_type_id === 1 ? 'fa-list-ul' : 'fa-bolt'}"></i> ${row.group_type_name}</span> <a href="#"><i class="fas fa-star ${row.is_flagged === !1 ? 'text-light' : 'text-warning'}"></i></a></div>`
-                            + `${row.group_type_id === 2 ? '<div><a href="javascript:void(0);" class="details-control" data-toggle="tooltip" title="View definition."><i class="fas fa-angle-right"></i> View definition</a></div>' : ''} `
+                    data: 'group_id', title: '', sWidth: "2%", orderable: false, render: function (data, type, row, meta) {
+                        return `${row.group_type_id === 2 ? '<a href="javascript:void(0);" class="details-control" data-toggle="tooltip" title="View definition."><i class="fas fa-angle-right fa-2x"></i></a>' : ''}`;
                     }
                 },
+                {
+                    data: 'name', title: 'List', sWidth: "58%", orderable: false, render: function (data, type, row, meta) {
+                        return `<div class=""><a href="#" class="me-2"><i class="fas fa-star ${row.is_flagged === !1 ? 'text-light' : 'text-warning'}"></i></a> <a data-add="false" data-url='/lists/listmembers/${row.group_id}' href="/list/${row.group_id}/members">${data}</a></div>`;
+                    }
+                },
+                { data: 'group_type_name', title: 'Type', sWidth: "10%", class: '', orderable: false, render: function (data, type, row, meta) { return `<span class="bg-primary bg-soft text-primary badge font-size-12 me-2"><i class="fas ${row.group_type_id === 1 ? 'fa-list-ul' : 'fa-bolt'}"></i> ${row.group_type_name}</span>`; } },
                 { data: 'slug', title: 'Members', sWidth: "10%", class: 'text-end', orderable: false, render: function (data, type, row, meta) { return `<a href="/lists/listmembers/${row.slug}" id="${row.group_id}" data-member-count="true">0</a>`; } },
-                { data: 'created', title: 'Created', sWidth: "20%", render: function (data, type, full, meta) { return moment(data).format('MMMM Do YYYY, h:mm A'); } },
-                { data: 'created', title: 'Action', sWidth: "10%", orderable: false, render: function (data, type, row, meta) { return _template('action', row); } }
+                { data: 'created', title: 'Created', sWidth: "15%", render: function (data, type, full, meta) { return moment(data).format('MMMM Do YYYY, h:mm A'); } },
+                { data: 'created', title: 'Action', sWidth: "5%", orderable: false, render: function (data, type, row, meta) { return _template('action', row); } }
             ],
             initComplete: function (settings, json) {
                 countMember();
@@ -113,45 +122,98 @@ var $tb = $('#datatable'), bm = 'modalEdit',
         });
     },
     format = function (d) {
-        r = document, j = JSON.parse(d.definition);
+        let r = document, j = JSON.parse(d.definition);
         var d = r.createElement('ul', { class: 'mb-2', });
         let g = null, c = null;
 
-        $.each(j, function (i, row) {
-            let _ = r.createElement('li', { class: '' });
-            $.each(row.criteria, function (si, s) {
-                console.log('customer-attribute ', s)
-                if (s.type === 'customer-statistic-value') {
-                    //(g === null ? getGroup().done(function (data) { g = data }) : void (0));
-                    //let _n = g.find(el => el.group_id === s.group); _n = _n ? _n.name : '';
-                    let _w = s.statistic_filters ? ` where <b>${s.statistic_filters.dimension}</b> ${s.statistic_filters.operator} <b>${s.statistic_filters.value.join(', ')}</b>` : '';
-                    _.append(r.createElement('span', { class: '' }, `${s.statistic} ${s.operator} ${s.timeframe} ${_w}`
-                        //(si > 0 ? 'OR ' : '') + (s.operator === 'eq' ? 'Is' : 'Is not') + ' in <b>' + _n + '</b> and was added ' + info(s)
-                    ));
-                }
-                else if (s.type === 'customer-attribute') {
-                    (g === null ? getGroup().done(function (data) { g = data }) : void (0));
-                    let _n = g.find(el => el.group_id === s.group); _n = _n ? _n.name : '';
-                    _.append(r.createElement('span', { class: '' },
-                        (si > 0 ? 'OR ' : '') + (s.operator === 'eq' ? 'Is' : 'Is not') + ' in <b>' + _n + '</b> and was added ' + info(s)
-                    ));
-                }
-                else if (s.type === 'customer-exclusion') {
-                    _.append(r.createElement('span', { class: '' },
-                        'Person <b>' + (s.operator === 'eq' ? 'Is' : 'Is not') + '</b> suppressed for email'
-                    ));
-                }
-                else if (s.type === 'customer-location') {
-                    (c === null ? getCountries().done(function (data) { c = data }) : void (0));
-                    let _n = c.find(el => el.code === s.country_id); _n = _n ? _n.name : '';
-                    _.append(r.createElement('span', { class: '' },
-                        'Location <b>' + (s.operator === 'eq' ? 'Is' : 'Is not') + '</b> within <b>' + _n + '</b>'
-                    ));
-                }
+        j.forEach((container, iContainer) => {
+            //$.each(j, function (i, row) {
+            let _li = r.createElement('li', { class: '' });
+            container.criteria.forEach((row, iRow) => {
+                //$.each(row.criteria, function (si, s) {
+                console.log(row.type, row)
+                let $span = r.createElement('span', { class: '' });
+                iRow > 0 && $span.appendChild(r.createElement('span', {}, `OR `));
+                if (row.type === 'customer-statistic-value') {
+                    $span.appendChild(r.createElement('span', {}, `<b>${row.metric_name}</b> `));
+                    if (row.operator === 'gt-zero') $span.appendChild(r.createElement('span', {}, `at least once `));
+                    else if (row.operator === 'eq-zero') $span.appendChild(r.createElement('span', {}, `zero times `));
+                    else if (row.operator === 'eq') $span.appendChild(r.createElement('span', {}, `equals `));
+                    else if (row.operator === 'neq') $span.appendChild(r.createElement('span', {}, `doesn't equal `));
+                    else if (row.operator === 'gte') $span.appendChild(r.createElement('span', {}, `is at least `));
+                    else if (row.operator === 'gt') $span.appendChild(r.createElement('span', {}, `is greater than `));
+                    else if (row.operator === 'lt') $span.appendChild(r.createElement('span', {}, `is less than `));
+                    else if (row.operator === 'lte') $span.appendChild(r.createElement('span', {}, `is at most `));
 
-                _.append(r.createElement('br'));
+                    if (row.timeframe === 'alltime') {
+                        $span.appendChild(r.createElement('span', {}, `over all time `));
+                    }
+                    else if (row.timeframe === 'in-the-last') {
+                        $span.appendChild(r.createElement('span', {}, `in the last `));
+                        row.timeframe_options && $span.appendChild(r.createElement('span', {}, `<b>${row.timeframe_options.quantity}</b> <b>${row.timeframe_options.units}s</b>`));
+                    }
+                    else if (row.timeframe === 'between') {
+                        $span.appendChild(r.createElement('span', {}, `between `));
+                        row.timeframe_options && $span.appendChild(r.createElement('span', {}, `<b>${row.timeframe_options.start}</b> and <b>${row.timeframe_options.end}</b>`));
+                    }
+                    else if (row.timeframe === 'before') {
+                        $span.appendChild(r.createElement('span', {}, `before `));
+                        row.timeframe_options && $span.appendChild(r.createElement('span', {}, `<b>${(new Date(row.timeframe_options.value)).toLocaleDateString()}</b>`));
+                    }
+                    else if (row.timeframe === 'after') {
+                        $span.appendChild(r.createElement('span', {}, `after `));
+                        row.timeframe_options && $span.appendChild(r.createElement('span', {}, `<b>${(new Date(row.timeframe_options.value)).toLocaleDateString()}</b>`));
+                    }
+                    else if (row.timeframe === 'between-static') {
+                        $span.appendChild(r.createElement('span', {}, `between dates `));
+                        row.timeframe_options && $span.appendChild(r.createElement('span', {}, `<b>${(new Date(row.timeframe_options.start)).toLocaleDateString()}</b> and <b>${(new Date(row.timeframe_options.end)).toLocaleDateString()}</b>`));
+                    }
+                    row.statistic_filters && $span.appendChild(r.createElement('span', {}, ` where <b>${row.statistic_filters.dimension}</b> ${row.statistic_filters.operator} <b>${row.statistic_filters.value.join(', ')}</b>`));
+                }
+                else if (row.type === 'customer-attribute') {
+                    $span.appendChild(r.createElement('span', {}, `<b>${row.key}</b> `));
+                    if (row.operator === 'eq') $span.appendChild(r.createElement('span', {}, `equals `));
+                    else if (row.operator === 'neq') $span.appendChild(r.createElement('span', {}, `doesn't equal `));
+                    else if (row.operator === 'contains') $span.appendChild(r.createElement('span', {}, `contains `));
+                    else if (row.operator === 'ncontains') $span.appendChild(r.createElement('span', {}, `doesn't contain `));
+                    else if (row.operator === 'starts-with') $span.appendChild(r.createElement('span', {}, `start with `));
+                    else if (row.operator === 'nstarts-with') $span.appendChild(r.createElement('span', {}, `doesn't start with `));
+                    else if (row.operator === 'ends-with') $span.appendChild(r.createElement('span', {}, `ends with `));
+                    else if (row.operator === 'nends-with') $span.appendChild(r.createElement('span', {}, `doesn't ends with `));
+                    else if (row.operator === 'exists') $span.appendChild(r.createElement('span', {}, `is set `));
+                    else if (row.operator === 'nexists') $span.appendChild(r.createElement('span', {}, `is not set `));
+                    $span.appendChild(r.createElement('span', {}, `<b>${row.value}</b> `));
+                }
+                else if (row.type === 'customer-location') {
+                    $span.appendChild(r.createElement('span', {}, 'Location <b>' + (row.operator === 'eq' ? 'Is ' : 'Is not ') + '</b> within'));
+                    if (row.region_id === 'EUROPEAN_UNION') $span.appendChild(r.createElement('span', {}, `<b>European Union</b>`));
+                    else if (row.region_id === 'UNITED_STATES') $span.appendChild(r.createElement('span', {}, `<b>United States</b>`));
+                }
+                else if (row.type === 'customer-distance') {
+                    $span.appendChild(r.createElement('span', {}, 'Person ' + (row.operator === 'eq' ? 'Is ' : 'Is not ')));
+                    $span.appendChild(r.createElement('span', {}, `within ${row.distance} `));
+                    $span.appendChild(r.createElement('span', {}, `within ${row.units} `));
+                    $span.appendChild(r.createElement('span', {}, `of ${row.postal_code} `));
+                    $span.appendChild(r.createElement('span', {}, `in ${row.country_code} `));
+                }
+                else if (row.type === 'customer-group-membership') {
+                    $span.appendChild(r.createElement('span', {}, (row.operator === 'eq' ? 'Is ' : 'Is not ')));
+                    $span.appendChild(r.createElement('span', {}, `in <b>${row.group_name}</b> `));
+                    if (row.timeframe === 'in-the-last') $span.appendChild(r.createElement('span', {}, `and was added in the last <b>${row.timeframe_options.quantity} ${row.timeframe_options.units}s</b>`));
+                    else if (row.timeframe === 'more-than') $span.appendChild(r.createElement('span', {}, `and was added more than <b>${row.timeframe_options.quantity} ${row.timeframe_options.units}s</b> ago`));
+                    else if (row.timeframe === 'at-least') $span.appendChild(r.createElement('span', {}, `and was added at least <b>${row.timeframe_options.quantity} ${row.timeframe_options.units}s</b> ago`));
+                    else if (row.timeframe === 'between') $span.appendChild(r.createElement('span', {}, `and was added between <b>${row.timeframe_options.start}</b> and <b>${row.timeframe_options.end} ${row.timeframe_options.units}s</b>`));
+                    else if (row.timeframe === 'before') $span.appendChild(r.createElement('span', {}, `and was added before <b>${(new Date(row.timeframe_options.value)).toLocaleDateString()}</b>`));
+                    else if (row.timeframe === 'after') $span.appendChild(r.createElement('span', {}, `and was added after <b>${(new Date(row.timeframe_options.value)).toLocaleDateString()}</b>`));
+                    else if (row.timeframe === 'between-static') $span.appendChild(r.createElement('span', {}, `and was added between dates <b>${(new Date(row.timeframe_options.start)).toLocaleDateString()}</b> and <b>${(new Date(row.timeframe_options.end)).toLocaleDateString()}</b>`));
+                }
+                else if (row.type === 'customer-exclusion') {
+                    $span.appendChild(r.createElement('span', {}, 'Person <b>' + (row.operator === 'eq' ? 'Is ' : 'Is not ') + '</b> suppressed for email'));
+                }
+                _li.appendChild($span);
+                _li.append(r.createElement('br'));
             });
-            d.append(_);
+            d.append(_li);
         })
         return d;
     },
@@ -172,7 +234,7 @@ var $tb = $('#datatable'), bm = 'modalEdit',
             case 'between-static':
                 return `between dates <b>${s.timeframeOptions.start}</b> and <b>${s.timeframeOptions.end}</b>`;
             default:
-                return _d = '';
+                return '';
         }
     },
     getGroup = function () {
